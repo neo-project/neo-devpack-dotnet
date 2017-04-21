f8a1eb2e4af30f65f3b59d7d5f679cd3b35213ac
Erik Zhang <erik@vcage.com>
2017-04-21 19:32:48 +0800

    synchronize with AntShares 1.5.0

---
+++ src/AntShares.SmartContract.Framework/OpCodeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using AntShares.VM;
+using System;
+
+namespace AntShares.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class OpCodeAttribute : Attribute
+    {
+        public OpCode OpCode { get; }
+
+        public OpCodeAttribute(OpCode opcode)
+        {
+            this.OpCode = opcode;
+        }
+    }
+}
---
+++ src/AntShares.SmartContract.Framework/Services/AntShares/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace AntShares.SmartContract.Framework.Services.AntShares
+{
+    public class Account
+    {
+        public extern byte[] ScriptHash
+        {
+            [Syscall("AntShares.Account.GetScriptHash")]
+            get;
+        }
+
+        public extern byte[][] Votes
+        {
+            [Syscall("AntShares.Account.GetVotes")]
+            get;
+        }
+
+        [Syscall("AntShares.Account.GetBalance")]
+        public extern long GetBalance(byte[] asset_id);
+    }
+}
