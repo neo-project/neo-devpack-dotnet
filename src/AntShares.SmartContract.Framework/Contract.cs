266b6d17e6b13b88fba4f26b5f6054d8321da82f
Erik Zhang <erik@vcage.com>
2017-05-02 12:55:45 +0800

    synchronize with AntShares 1.6.0-preview3

--- src/AntShares.SmartContract.Framework/Contract.cs
+++ src/AntShares.SmartContract.Framework/Contract.cs
@@ -1,10 +1,23 @@
-﻿using AntShares.VM;
+﻿using AntShares.SmartContract.Framework.Services.System;
+using AntShares.VM;
 
 namespace AntShares.SmartContract.Framework
 {
     public class Contract
     {
         protected static ScriptEngine ScriptEngine { get; } = new ScriptEngine();
+
+        [OpCode(OpCode.SHA1)]
+        protected extern static byte[] Sha1(byte[] data);
+
+        [OpCode(OpCode.SHA256)]
+        protected extern static byte[] Sha256(byte[] data);
+
+        [OpCode(OpCode.HASH160)]
+        protected extern static byte[] Hash160(byte[] data);
+
+        [OpCode(OpCode.HASH256)]
+        protected extern static byte[] Hash256(byte[] data);
 
         [OpCode(OpCode.CHECKSIG)]
         protected extern static bool VerifySignature(byte[] signature, byte[] pubkey);
