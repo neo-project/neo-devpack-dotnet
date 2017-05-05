3ce798d13cb1b11513e01bf326cfeee59a051a07
lights li <lightsever@hotmail.com>
2017-05-05 21:18:53 +0800

    模板基本上搞好了

--- src/AntShares.ILConvertPlugin/ConvertTask.cs
+++ src/AntShares.ILConvertPlugin/ConvertTask.cs
@@ -3,6 +3,9 @@
 
     using Microsoft.Build.Framework;
     using Microsoft.Build.Utilities;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
     /// <summary>
     /// this class generate config / xml file by template
     /// </summary>
@@ -17,10 +20,43 @@
         /// <returns>ture successful, false failure</returns>
         public override bool Execute()
         {
-            this.Log.LogWarning("come on baby.");
-            this.Log.LogWarning("src=" + this.DataSource.ToString());
+            var srcdll = this.DataSource.ToString();
 
-            return true;
+            this.Log.LogMessage("LogMessage AntShares IL Convert 0.01.");
+            this.Log.LogMessageFromText("LogMessageFromText AntShares IL Convert 0.01.", MessageImportance.High);
+            this.Log.LogWarning("LogWarning AntShares IL Convert 0.01.", MessageImportance.High);
+
+            this.Log.LogWarning("warn", "warn code", "", "abc.cs", 123, 0, 123, 0, "LogWarning with code: AntShares IL Convert 0.01.");
+
+            this.Log.LogMessageFromText("src=" + this.DataSource.ToString(), MessageImportance.High);
+
+            ProcessStartInfo pinfo = new ProcessStartInfo();
+            pinfo.FileName = "cmd.exe";
+            pinfo.WorkingDirectory = System.IO.Path.GetDirectoryName(srcdll);
+            pinfo.UseShellExecute = false;
+            pinfo.RedirectStandardInput = true;
+            pinfo.RedirectStandardOutput = true;
+            pinfo.CreateNoWindow = true;
+            Process p = Process.Start(pinfo);
+            p.StandardInput.AutoFlush = true;
+            p.StandardInput.WriteLine("tsc");
+            p.StandardInput.WriteLine("exit");
+
+            List<string> lines = new List<string>();
+            while(p.StandardOutput.EndOfStream==false)
+            {
+                lines.Add(p.StandardOutput.ReadLine());
+            }
+            string all=           p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            try
+            {
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 
