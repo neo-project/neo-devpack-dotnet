36df84059fb26a23d1610a9effa6daa468b9355e
lights li <lightsever@hotmail.com>
2017-05-05 23:06:06 +0800

    final

--- src/AntShares.ILConvertPlugin/ConvertTask.cs
+++ src/AntShares.ILConvertPlugin/ConvertTask.cs
@@ -35,37 +35,45 @@
             pinfo.RedirectStandardInput = true;
             pinfo.RedirectStandardOutput = true;
             pinfo.CreateNoWindow = true;
+            pinfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
+
             Process p = Process.Start(pinfo);
             p.StandardInput.AutoFlush = true;
             p.StandardInput.WriteLine("antshares.compiler.msil " + dllname);
             p.StandardInput.WriteLine("exit");
-            
+
             //前四行后一行不要
             string lastline = null;
             int count = 0;
             bool bSucc = false;
             while (p.StandardOutput.EndOfStream == false)
             {
+                var line = p.StandardOutput.ReadLine();
+                count++;
+                if (count <= 4) continue;
+
                 if (lastline != null && lastline.Length > 0)
                 {
                     if (lastline[0] == '<')
                     {
                         if (lastline.IndexOf("<WARN>") == 0)
                         {
-                            this.Log.LogWarning("", "", "", "", 0, 0, 0, 0, lastline.Substring(6));
+                            this.Log.LogWarning(lastline.Substring(6));
+                            lastline = line;
                             continue;
                         }
                         else if (lastline.IndexOf("<ERR>") == 0)
                         {
-                            this.Log.LogError("", "", "", "", 0, 0, 0, 0, lastline.Substring(5));
+                            this.Log.LogError(lastline.Substring(5));
+                            lastline = line;
                             continue;
                         }
-                        else if (lastline.IndexOf("<WRANCODE|") == 0)
+                        else if (lastline.IndexOf("<WARN|") == 0)
                         {
-                            var l = lastline.Substring(10);
+                            var l = lastline.Substring(6);
                             var ine = lastline.IndexOf(">");
                             var text = lastline.Substring(ine + 1);
-                            var file = lastline.Substring(10, ine - 10);
+                            var file = lastline.Substring(6, ine - 6);
                             var lines = file.Split(new char[] { '(', ')' });
                             int _line = 0;
                             if (lines.Length > 1)
@@ -73,14 +81,15 @@
                                 int.TryParse(lines[1], out _line);
                             }
                             this.Log.LogWarning("", "", "", lines[0], _line, 0, 0, 0, text);
+                            lastline = line;
                             continue;
                         }
-                        else if (lastline.IndexOf("<ERRCODE|") == 0)
+                        else if (lastline.IndexOf("<ERR|") == 0)
                         {
-                            var l = lastline.Substring(9);
+                            var l = lastline.Substring(5);
                             var ine = lastline.IndexOf(">");
                             var text = lastline.Substring(ine + 1);
-                            var file = lastline.Substring(9, ine - 9);
+                            var file = lastline.Substring(5, ine - 5);
                             var lines = file.Split(new char[] { '(', ')' });
                             int _line = 0;
                             if (lines.Length > 1)
@@ -88,19 +97,18 @@
                                 int.TryParse(lines[1], out _line);
                             }
                             this.Log.LogWarning("", "", "", lines[0], _line, 0, 0, 0, text);
+                            lastline = line;
                             continue;
                         }
                     }
-                    if(lastline.IndexOf("SUCC")==0)
+                    if (lastline.IndexOf("SUCC") == 0)
                     {
                         bSucc = true;
                     }
                     this.Log.LogMessageFromText(lastline, MessageImportance.High);
 
                 }
-                var line = p.StandardOutput.ReadLine();
-                count++;
-                if (count <= 4) continue;
+
                 lastline = line;
                 //lines.Add(line);
             }
