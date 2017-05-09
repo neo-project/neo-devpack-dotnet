b47c22c3adbd43041b8e6af215977a9af26a3b54
lights li <lightsever@hotmail.com>
2017-05-10 00:47:03 +0800

    fix

--- src/AntShares.SmartContract.Framework/Contract.cs
+++ src/AntShares.SmartContract.Framework/Contract.cs
@@ -1,4 +1,5 @@
-﻿using AntShares.VM;
+﻿using AntShares.SmartContract.Framework.Services.System;
+using AntShares.VM;
 
 namespace AntShares.SmartContract.Framework
 {
@@ -6,6 +7,14 @@ namespace AntShares.SmartContract.Framework
     {
         protected static ScriptEngine ScriptEngine { get; } = new ScriptEngine();
 
+        [OpCode(OpCode.SHA1)]
+        protected extern static byte[] Sha1(byte[] data);
+        [OpCode(OpCode.SHA256)]
+        protected extern static byte[] Sha256(byte[] data);
+        [OpCode(OpCode.HASH160)]
+        protected extern static byte[] Hash160(byte[] data);
+        [OpCode(OpCode.HASH256)]
+        protected extern static byte[] Hash256(byte[] data);
         [OpCode(OpCode.CHECKSIG)]
         protected extern static bool VerifySignature(byte[] signature, byte[] pubkey);
     }
