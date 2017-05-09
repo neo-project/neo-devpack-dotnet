1e657139abef80afd28310abd4134253dca057f4
Erik Zhang <erik@vcage.com>
2017-05-10 03:06:06 +0800

    add build.tasks to vs plugin

---
+++ src/AntShares.ILConvertPlugin/ConvertTask.cs
@@ -0,0 +1,123 @@
+﻿namespace AntShares
+{
+
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// this class generate config / xml file by template
+    /// </summary>
+    public class ConvertTask : Task
+    {
+        [Required]
+        public ITaskItem DataSource { get; set; }
+
+        /// <summary>
+        /// execute replace logic
+        /// </summary>
+        /// <returns>ture successful, false failure</returns>
+        public override bool Execute()
+        {
+            var srcdll = this.DataSource.ToString();
+
+            //this.Log.LogMessage("LogMessage AntShares IL Convert 0.01.");//can't show
+            this.Log.LogMessageFromText("LogMessageFromText AntShares IL Convert 0.01.", MessageImportance.High);
+
+
+
+            string dllname = System.IO.Path.GetFileName(srcdll);
+            ProcessStartInfo pinfo = new ProcessStartInfo();
+            pinfo.FileName = "cmd.exe";
+            pinfo.WorkingDirectory = System.IO.Path.GetDirectoryName(srcdll);
+            pinfo.UseShellExecute = false;
+            pinfo.RedirectStandardInput = true;
+            pinfo.RedirectStandardOutput = true;
+            pinfo.CreateNoWindow = true;
+            pinfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
+
+            Process p = Process.Start(pinfo);
+            p.StandardInput.AutoFlush = true;
+            p.StandardInput.WriteLine("antshares.compiler.msil " + dllname);
+            p.StandardInput.WriteLine("exit");
+
+            //前四行后一行不要
+            string lastline = null;
+            int count = 0;
+            bool bSucc = false;
+            while (p.StandardOutput.EndOfStream == false)
+            {
+                var line = p.StandardOutput.ReadLine();
+                count++;
+                if (count <= 4) continue;
+
+                if (lastline != null && lastline.Length > 0)
+                {
+                    if (lastline[0] == '<')
+                    {
+                        if (lastline.IndexOf("<WARN>") == 0)
+                        {
+                            this.Log.LogWarning(lastline.Substring(6));
+                            lastline = line;
+                            continue;
+                        }
+                        else if (lastline.IndexOf("<ERR>") == 0)
+                        {
+                            this.Log.LogError(lastline.Substring(5));
+                            lastline = line;
+                            continue;
+                        }
+                        else if (lastline.IndexOf("<WARN|") == 0)
+                        {
+                            var l = lastline.Substring(6);
+                            var ine = lastline.IndexOf(">");
+                            var text = lastline.Substring(ine + 1);
+                            var file = lastline.Substring(6, ine - 6);
+                            var lines = file.Split(new char[] { '(', ')' });
+                            int _line = 0;
+                            if (lines.Length > 1)
+                            {
+                                int.TryParse(lines[1], out _line);
+                            }
+                            this.Log.LogWarning("", "", "", lines[0], _line, 0, 0, 0, text);
+                            lastline = line;
+                            continue;
+                        }
+                        else if (lastline.IndexOf("<ERR|") == 0)
+                        {
+                            var l = lastline.Substring(5);
+                            var ine = lastline.IndexOf(">");
+                            var text = lastline.Substring(ine + 1);
+                            var file = lastline.Substring(5, ine - 5);
+                            var lines = file.Split(new char[] { '(', ')' });
+                            int _line = 0;
+                            if (lines.Length > 1)
+                            {
+                                int.TryParse(lines[1], out _line);
+                            }
+                            this.Log.LogWarning("", "", "", lines[0], _line, 0, 0, 0, text);
+                            lastline = line;
+                            continue;
+                        }
+                    }
+                    if (lastline.IndexOf("SUCC") == 0)
+                    {
+                        bSucc = true;
+                    }
+                    this.Log.LogMessageFromText(lastline, MessageImportance.High);
+
+                }
+
+                lastline = line;
+                //lines.Add(line);
+            }
+
+            //this.Log.LogMessageFromText(lastline, MessageImportance.High);
+
+            p.WaitForExit();
+            return bSucc;
+        }
+    }
+
+}
