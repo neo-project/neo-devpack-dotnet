a56d4422b7a9b86bfb0aaee75f4530b83f0c4145
lights li <lightsever@hotmail.com>
2017-05-13 23:55:14 +0800

    add more overloads to storage api (#3)

--- src/AntShares.SmartContract.Framework/Services/AntShares/Storage.cs
+++ src/AntShares.SmartContract.Framework/Services/AntShares/Storage.cs
@@ -5,10 +5,25 @@
         [Syscall("AntShares.Storage.Get")]
         public extern byte[] Get(StorageContext context, byte[] key);
 
+        [Syscall("AntShares.Storage.Get")]
+        public extern byte[] Get(StorageContext context, string key);
+
         [Syscall("AntShares.Storage.Put")]
         public extern void Put(StorageContext context, byte[] key, byte[] value);
 
+        [Syscall("AntShares.Storage.Put")]
+        public extern void Put(StorageContext context, byte[] key, string value);
+
+        [Syscall("AntShares.Storage.Put")]
+        public extern void Put(StorageContext context, string key, byte[] value);
+
+        [Syscall("AntShares.Storage.Put")]
+        public extern void Put(StorageContext context, string key, string value);
+
         [Syscall("AntShares.Storage.Delete")]
         public extern void Delete(StorageContext context, byte[] key);
+
+        [Syscall("AntShares.Storage.Delete")]
+        public extern void Delete(StorageContext context, string key);
     }
 }
