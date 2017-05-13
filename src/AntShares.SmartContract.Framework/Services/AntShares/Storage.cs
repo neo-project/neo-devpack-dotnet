86601949e009f7db589d098226a29c962fa9d439
lights li <lightsever@hotmail.com>
2017-05-13 21:50:13 +0800

    在模板中添加切换byte[] 参数 to string的重载，测试

--- src/AntShares.SmartContract.Framework/Services/AntShares/Storage.cs
+++ src/AntShares.SmartContract.Framework/Services/AntShares/Storage.cs
@@ -4,11 +4,18 @@
     {
         [Syscall("AntShares.Storage.Get")]
         public extern byte[] Get(StorageContext context, byte[] key);
+        [Syscall("AntShares.Storage.Get")]
+        public extern byte[] Get(StorageContext context, string key);
 
         [Syscall("AntShares.Storage.Put")]
         public extern void Put(StorageContext context, byte[] key, byte[] value);
+        [Syscall("AntShares.Storage.Put")]
+        public extern void Put(StorageContext context, string key, string value);
 
         [Syscall("AntShares.Storage.Delete")]
         public extern void Delete(StorageContext context, byte[] key);
+
+        [Syscall("AntShares.Storage.Delete")]
+        public extern void Delete(StorageContext context, string key);
     }
 }
