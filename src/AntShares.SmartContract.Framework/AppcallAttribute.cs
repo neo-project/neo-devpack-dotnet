809c2d566ece091c648bfc5721a5de3b36df0533
lights li <lightsever@hotmail.com>
2017-06-14 18:18:47 +0800

    add AppcallAttribute.cs

---
+++ src/AntShares.SmartContract.Framework/AppcallAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AntShares.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class AppcallAttribute : Attribute
+    {
+        public byte[] hash { get; }
+
+        public AppcallAttribute(byte[] hash)
+        {
+            this.hash = new byte[20];
+            for (var i = 0; i < 20; i++)
+            {
+                this.hash[i] = hash[i];
+            }
+        }
+        public AppcallAttribute(string hexstring)
+        {
+            this.hash = new byte[20];
+
+            for (var i = 0; i < 20; i++)
+            {
+                this.hash[i] = byte.Parse(hexstring.Substring(i * 2, 2));
+            }
+        }
+    }
+}
