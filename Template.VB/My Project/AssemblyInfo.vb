caaf9cb7aa375c2a431db25d4af2860d539143aa
Erik Zhang <erik@vcage.com>
2017-08-16 16:26:20 +0800

    upgrade to 2.1.0

--- Template.VB/My Project/AssemblyInfo.vb
+++ Template.VB/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.1.1")>
+<Assembly: AssemblyVersion("2.1.0")>
 '<Assembly: AssemblyFileVersion("1.0.0.0")>
