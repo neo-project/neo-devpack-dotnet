67d5eb6709df94e9cf3a51f90253dea50abcde19
lights <lightsever@hotmail.com>
2017-10-17 10:43:42 +0800

    add EntryPoint Attribute

---
+++ Neo.SmartContract.Framework/EntryPointAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+using System;
+
+namespace Neo.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method )]
+    public class EntryPointAttribute : Attribute
+    {
+        public TriggerType type { get; }
+
+        public EntryPointAttribute(TriggerType type)
+        {
+            this.type = type;
+        }
+    }
+}
