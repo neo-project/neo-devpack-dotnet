6df3c26aeb48b08b24c3bc3a02c90bbd97aebd62
lights <lightsever@hotmail.com>
2017-12-07 09:26:06 +0800

    为StorageContext 添加[]方法 为编译器支持以下语法做准备
    可以用Storage.CurrentContext[xxx]=yyy
    替代Storage.Put
    
    用 xxx =Storage.CurrentContext[yyy]
    替代Storage.Get

--- Neo.SmartContract.Framework/Services/Neo/StorageContext.cs
+++ Neo.SmartContract.Framework/Services/Neo/StorageContext.cs
@@ -2,5 +2,28 @@
 {
     public class StorageContext
     {
+        public byte[] this[byte[] key]
+        {
+            get
+            {
+                return Storage.Get(this,key);
+            }
+            set
+            {
+                Storage.Put(this, key, value);
+            }
+        }
+
+        public byte[] this[string key]
+        {
+            get
+            {
+                return Storage.Get(this, key);
+            }
+            set
+            {
+                Storage.Put(this, key, value);
+            }
+        }
     }
 }
