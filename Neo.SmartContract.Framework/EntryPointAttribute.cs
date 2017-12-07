b2525c42d9c53e9f4d4a2e5b8dfed0dd41218a4a
lights li <lightsever@hotmail.com>
2017-12-07 01:26:30 -0600

    add EntryPoint Attribute (#6) * add EntryPoint Attribute * uppercase a name

---
+++ Neo.SmartContract.Framework/EntryPointAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+using System;
+
+namespace Neo.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method )]
+    public class EntryPointAttribute : Attribute
+    {
+        public TriggerType Type { get; }
+
+        public EntryPointAttribute(TriggerType type)
+        {
+            this.Type = type;
+        }
+    }
+}
