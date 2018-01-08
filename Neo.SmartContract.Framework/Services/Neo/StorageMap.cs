0bde0fb91cc11c79703155b0b8d2336caa6a22ab
Erik Zhang <erik@vcage.com>
2018-01-08 15:58:15 +0800

    upgrade to v2.5.4 1. Remove old APIs: `Neo.Account.SetVotes` and `Neo.Validator.Register` 2. Add `StorageMap`

---
+++ Neo.SmartContract.Framework/Services/Neo/StorageMap.cs
@@ -0,0 +1,8 @@
+﻿namespace Neo.SmartContract.Framework.Services.Neo
+{
+    public class StorageMap
+    {
+        internal StorageContext Context;
+        internal string Prefix;
+    }
+}
--- Template.VB/My Project/AssemblyInfo.vb
+++ Template.VB/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.5.2")>
+<Assembly: AssemblyVersion("2.5.4")>
 '<Assembly: AssemblyFileVersion("1.0.0.0")>
