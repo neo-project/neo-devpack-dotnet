0812fac5473714110ae9f157d64d42d052a92f75
Ivan Poon <ravenxce@gmail.com>
2018-02-08 00:10:46 +0800

    Add InvocationTransaction to SmartContract framework
    This allows checking that a transaction is an invocation to the
    same smart contract during verification phase.
    
    Example usage: https://github.com/neo-project/proposals/pull/18#issuecomment-353839621

---
+++ Neo.SmartContract.Framework/Services/Neo/InvocationTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace Neo.SmartContract.Framework.Services.Neo
+{
+    public class InvocationTransaction : Transaction
+    {
+        public extern byte[] Script
+        {
+            [Syscall("Neo.InvocationTransaction.GetScript")]
+            get;
+        }
+    }
+}
