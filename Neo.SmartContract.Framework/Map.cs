27d5db3717a07aa4e775f7e3062f96b923306012
lights <lightsever@hotmail.com>
2018-03-16 17:17:59 +0800

    调整接口，增加重载

--- Neo.SmartContract.Framework/Map.cs
+++ Neo.SmartContract.Framework/Map.cs
@@ -1,4 +1,5 @@
 ﻿using Neo.VM;
+using System.Numerics;
 
 namespace Neo.SmartContract.Framework
 {
@@ -9,26 +10,47 @@ namespace Neo.SmartContract.Framework
     }
     public static class MapHelper
     {
-        [OpCode(OpCode.NEWMAP)]
-        public extern static Map New();
-
         [OpCode(OpCode.SETITEM)]
         public extern static void Put(this Map dict, string key, byte[] value);
+
+        [OpCode(OpCode.SETITEM)]
+        public extern static void Put(this Map dict, byte[] key, byte[] value);
+
+        [OpCode(OpCode.SETITEM)]
+        public extern static void Put(this Map dict, BigInteger key, byte[] value);
 
         [OpCode(OpCode.PICKITEM)]
         public extern static byte[] Get(this Map dict, string key);
 
+        [OpCode(OpCode.PICKITEM)]
+        public extern static byte[] Get(this Map dict, byte[] key);
+
+        [OpCode(OpCode.PICKITEM)]
+        public extern static byte[] Get(this Map dict, BigInteger key);
+
         [OpCode(OpCode.REMOVE)]
-        public extern static byte[] Remove(this Map dict, string key);
+        public extern static void Remove(this Map dict, string key);
+
+        [OpCode(OpCode.REMOVE)]
+        public extern static void Remove(this Map dict, byte[] key);
+
+        [OpCode(OpCode.REMOVE)]
+        public extern static void Remove(this Map dict, BigInteger key);
 
         [OpCode(OpCode.HASKEY)]
-        public extern static byte[] HasKey(this Map dict, string key);
+        public extern static bool HasKey(this Map dict, string key);
+
+        [OpCode(OpCode.HASKEY)]
+        public extern static bool HasKey(this Map dict, byte[] key);
+
+        [OpCode(OpCode.HASKEY)]
+        public extern static bool HasKey(this Map dict, BigInteger key);
 
         [OpCode(OpCode.KEYS)]
-        public extern static System.Collections.ICollection Keys(this Map dict);
+        public extern static byte[][] Keys(this Map dict);
 
         [OpCode(OpCode.VALUES)]
-        public extern static System.Collections.ICollection Values(this Map dict);
+        public extern static byte[][] Values(this Map dict);
     }
 
 }
