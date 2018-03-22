ea920fa8e63942cd6cb2fee8652838b054babac9
Erik Zhang <erik@vcage.com>
2018-03-22 17:11:24 +0800

    Update Map.cs

--- Neo.SmartContract.Framework/Map.cs
+++ Neo.SmartContract.Framework/Map.cs
@@ -1,14 +1,8 @@
 ﻿using Neo.VM;
-using System.Numerics;
 
 namespace Neo.SmartContract.Framework
 {
-    public class Map
-    {
-        [OpCode(OpCode.NEWMAP)]
-        public extern Map();
-    }
-    public class Map<TKEY, TVALUE>
+    public class Map<TKey, TValue>
     {
         [OpCode(OpCode.NEWMAP)]
         public extern Map();
@@ -17,60 +11,21 @@ namespace Neo.SmartContract.Framework
     public static class MapHelper
     {
         [OpCode(OpCode.SETITEM)]
-        public extern static void Put<TKEY,TVALUE>(this Map<TKEY,TVALUE> dict, TKEY key, TVALUE value);
-        [OpCode(OpCode.PICKITEM)]
-        public extern static TVALUE Get<TKEY, TVALUE>(this Map<TKEY, TVALUE> dict, TKEY key);
-        [OpCode(OpCode.REMOVE)]
-        public extern static void Remove<TKEY, TVALUE>(this Map<TKEY, TVALUE> dict, TKEY key);
-        [OpCode(OpCode.HASKEY)]
-        public extern static bool HasKey<TKEY, TVALUE>(this Map<TKEY, TVALUE> dict, TKEY key);
-        [OpCode(OpCode.KEYS)]
-        public extern static TKEY[] Keys<TKEY, TVALUE>(this Map<TKEY, TVALUE> dict);
-        [OpCode(OpCode.VALUES)]
-        public extern static TVALUE[] Values<TKEY, TVALUE>(this Map<TKEY, TVALUE> dict);
-
-
-        [OpCode(OpCode.SETITEM)]
-        public extern static void Put(this Map dict, string key, byte[] value);
-
-        [OpCode(OpCode.SETITEM)]
-        public extern static void Put(this Map dict, byte[] key, byte[] value);
-
-        [OpCode(OpCode.SETITEM)]
-        public extern static void Put(this Map dict, BigInteger key, byte[] value);
+        public extern static void Put<TKey, TValue>(this Map<TKey, TValue> map, TKey key, TValue value);
 
         [OpCode(OpCode.PICKITEM)]
-        public extern static byte[] Get(this Map dict, string key);
-
-        [OpCode(OpCode.PICKITEM)]
-        public extern static byte[] Get(this Map dict, byte[] key);
-
-        [OpCode(OpCode.PICKITEM)]
-        public extern static byte[] Get(this Map dict, BigInteger key);
+        public extern static TValue Get<TKey, TValue>(this Map<TKey, TValue> map, TKey key);
 
         [OpCode(OpCode.REMOVE)]
-        public extern static void Remove(this Map dict, string key);
-
-        [OpCode(OpCode.REMOVE)]
-        public extern static void Remove(this Map dict, byte[] key);
-
-        [OpCode(OpCode.REMOVE)]
-        public extern static void Remove(this Map dict, BigInteger key);
+        public extern static void Remove<TKey, TValue>(this Map<TKey, TValue> map, TKey key);
 
         [OpCode(OpCode.HASKEY)]
-        public extern static bool HasKey(this Map dict, string key);
-
-        [OpCode(OpCode.HASKEY)]
-        public extern static bool HasKey(this Map dict, byte[] key);
-
-        [OpCode(OpCode.HASKEY)]
-        public extern static bool HasKey(this Map dict, BigInteger key);
+        public extern static bool HasKey<TKey, TValue>(this Map<TKey, TValue> map, TKey key);
 
         [OpCode(OpCode.KEYS)]
-        public extern static byte[][] Keys(this Map dict);
+        public extern static TKey[] Keys<TKey, TValue>(this Map<TKey, TValue> map);
 
         [OpCode(OpCode.VALUES)]
-        public extern static byte[][] Values(this Map dict);
+        public extern static TValue[] Values<TKey, TValue>(this Map<TKey, TValue> map);
     }
-
 }
