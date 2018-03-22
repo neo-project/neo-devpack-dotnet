1e096533ec0ba3dbc4325f3c5566d68b6be79611
lights li <lightsever@hotmail.com>
2018-03-22 17:13:53 +0800

    Add Serialize and Dictionary (#18)

---
+++ Neo.SmartContract.Framework/Map.cs
@@ -0,0 +1,31 @@
+﻿using Neo.VM;
+
+namespace Neo.SmartContract.Framework
+{
+    public class Map<TKey, TValue>
+    {
+        [OpCode(OpCode.NEWMAP)]
+        public extern Map();
+    }
+
+    public static class MapHelper
+    {
+        [OpCode(OpCode.SETITEM)]
+        public extern static void Put<TKey, TValue>(this Map<TKey, TValue> map, TKey key, TValue value);
+
+        [OpCode(OpCode.PICKITEM)]
+        public extern static TValue Get<TKey, TValue>(this Map<TKey, TValue> map, TKey key);
+
+        [OpCode(OpCode.REMOVE)]
+        public extern static void Remove<TKey, TValue>(this Map<TKey, TValue> map, TKey key);
+
+        [OpCode(OpCode.HASKEY)]
+        public extern static bool HasKey<TKey, TValue>(this Map<TKey, TValue> map, TKey key);
+
+        [OpCode(OpCode.KEYS)]
+        public extern static TKey[] Keys<TKey, TValue>(this Map<TKey, TValue> map);
+
+        [OpCode(OpCode.VALUES)]
+        public extern static TValue[] Values<TKey, TValue>(this Map<TKey, TValue> map);
+    }
+}
--- Neo.SmartContract.Framework/OpCodeAttribute.cs
+++ Neo.SmartContract.Framework/OpCodeAttribute.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Neo.SmartContract.Framework
 {
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor)]
     public class OpCodeAttribute : Attribute
     {
         public OpCode OpCode { get; }
