39f6617a87ee176ac0b65f34b363ee6462c2df1d
陈志同 <chenzhitong12@hotmail.com>
2018-08-15 10:34:35 +0800

    update

---
+++ Neo.SmartContract.Framework/Services/Neo/ContractPropertyState.cs
@@ -0,0 +1,11 @@
+﻿namespace Neo.SmartContract.Framework.Services.Neo
+{
+    public enum ContractPropertyState : byte
+    {
+        NoProperty = 0,
+
+        HasStorage = 1 << 0,
+        HasDynamicInvoke = 1 << 1,
+        Payable = 1 << 2
+    }
+}
