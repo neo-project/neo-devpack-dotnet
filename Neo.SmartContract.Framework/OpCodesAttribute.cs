e11cc11da2a8cfad4d4d90ca91850bf38382900f
Igor Machado <igor.machado@gmail.com>
2018-11-28 18:10:17 -0200

    attribute to issue multiple opcodes

---
+++ Neo.SmartContract.Framework/OpCodesAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Neo.VM;
+using System;
+
+namespace Neo.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor)]
+    public class OpCodesAttribute : Attribute
+    {
+        public OpCode[] OpCodes { get; }
+
+        public OpCodesAttribute(params OpCode[] opcodes)
+        {
+            this.OpCodes = opcodes;
+        }
+    }
+}
