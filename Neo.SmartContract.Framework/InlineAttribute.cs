49f693c46fc39c027aebc5b9c2ddf26938ceaa92
Igor Machado <igor.machado@gmail.com>
2018-12-02 16:00:03 -0200

    created Inline attribute

---
+++ Neo.SmartContract.Framework/InlineAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Neo.VM;
+using System;
+
+namespace Neo.SmartContract.Framework
+{
+    // OpCode with Extension parameters
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor, AllowMultiple = true)]
+    public sealed class InlineAttribute : Attribute
+    {
+        // opcode id
+        public OpCode op { get; }
+        // extension of opcode
+        public string ext { get; }
+        // if extension is Hex or Char
+        public bool isHex { get; }
+
+        public InlineAttribute(OpCode op, string extension = "", bool isHex = false)
+        {
+            this.op = op;
+            this.ext = extension;
+            this.isHex = isHex;
+        }
+    }
+}
