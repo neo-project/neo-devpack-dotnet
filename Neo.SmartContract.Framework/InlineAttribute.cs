f769bccc20f784f9d555d78a348cf75f2b592d43
Shargon <shargon@gmail.com>
2018-12-03 14:54:43 +0100

    Update InlineAttribute.cs

--- Neo.SmartContract.Framework/InlineAttribute.cs
+++ Neo.SmartContract.Framework/InlineAttribute.cs
@@ -3,22 +3,32 @@ using System;
 
 namespace Neo.SmartContract.Framework
 {
-    // OpCode with data in hex or ascii
+    /// <summary>
+    /// OpCode with data in hex or ascii
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor, AllowMultiple = true)]
     public sealed class InlineAttribute : Attribute
     {
-        // opcode
-        public OpCode opCode { get; }
-        // opcode data (can be hex "ab01ab" or ascii "Runtime.Notify")
-        public string opData { get; }
-        // if opcode data is Hex or ascii
-        public bool isHex { get; }
+        /// <summary>
+        /// opcode
+        /// </summary>
+        public OpCode OpCode { get; }
+
+        /// <summary>
+        /// opcode data (can be hex "ab01ab" or ascii "Runtime.Notify")
+        /// </summary>
+        public string OpData { get; }
+        
+        /// <summary>
+        /// if opcode data is Hex or ascii
+        /// </summary>
+        public bool IsHex { get; }
 
         public InlineAttribute(OpCode opCode, string opData = "", bool isHex = false)
         {
-            this.opCode = opCode;
-            this.opData = opData;
-            this.isHex = isHex;
+            OpCode = opCode;
+            OpData = opData;
+            IsHex = isHex;
         }
     }
 }
