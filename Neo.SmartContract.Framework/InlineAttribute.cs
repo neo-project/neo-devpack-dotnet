a8cd68e0581ce4b123cbd251d027637af4c0dde7
Igor Machado <igor.machado@gmail.com>
2018-12-03 10:21:32 -0200

    comments

--- Neo.SmartContract.Framework/InlineAttribute.cs
+++ Neo.SmartContract.Framework/InlineAttribute.cs
@@ -3,15 +3,15 @@ using System;
 
 namespace Neo.SmartContract.Framework
 {
-    // OpCode with Extension parameters
+    // OpCode with data in hex or ascii
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor, AllowMultiple = true)]
     public sealed class InlineAttribute : Attribute
     {
-        // opcode id
+        // opcode
         public OpCode opCode { get; }
         // opcode data (can be hex "ab01ab" or ascii "Runtime.Notify")
         public string opData { get; }
-        // if extension is Hex or ascii
+        // if opcode data is Hex or ascii
         public bool isHex { get; }
 
         public InlineAttribute(OpCode opCode, string opData = "", bool isHex = false)
