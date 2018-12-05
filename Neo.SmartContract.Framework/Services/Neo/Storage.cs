f95a38b23eb9aea893f6ec1609a8f61750fd6715
Igor Machado <igor.machado@gmail.com>
2018-12-05 14:40:56 -0200

    extern

--- Neo.SmartContract.Framework/Services/Neo/Storage.cs
+++ Neo.SmartContract.Framework/Services/Neo/Storage.cs
@@ -90,83 +90,83 @@ namespace Neo.SmartContract.Framework.Services.Neo
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Get")]
-        public static byte[] Get(byte[] key);
+        public static extern byte[] Get(byte[] key);
 
         /// <summary>
         /// Returns the byte[] value corresponding to given string key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Get")]
-        public static byte[] Get(string key);
+        public static extern byte[] Get(string key);
 
         /// <summary>
         /// Writes byte[] value on byte[] key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Put")]
-        public static void Put(byte[] key, byte[] value);
+        public static extern void Put(byte[] key, byte[] value);
 
         /// <summary>
         /// Writes BigInteger value on byte[] key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Put")]
-        public static void Put(byte[] key, BigInteger value);
+        public static extern void Put(byte[] key, BigInteger value);
 
         /// <summary>
         /// Writes string value on byte[] key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Put")]
-        public static void Put(byte[] key, string value);
+        public static extern void Put(byte[] key, string value);
 
         /// <summary>
         /// Writes byte[] value on string key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Put")]
-        public static void Put(string key, byte[] value);
+        public static extern void Put(string key, byte[] value);
 
         /// <summary>
         /// Writes BigInteger value on string key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Put")]
-        public static void Put(string key, BigInteger value);
+        public static extern void Put(string key, BigInteger value);
 
         /// <summary>
         /// Writes string value on string key for current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Put")]
-        public static void Put(string key, string value);
+        public static extern void Put(string key, string value);
 
         /// <summary>
         /// Deletes byte[] key from current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Delete")]
-        public static void Delete(byte[] key);
+        public static extern void Delete(byte[] key);
 
         /// <summary>
         /// Deletes string key from given Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Delete")]
-        public static void Delete(string key);
+        public static extern void Delete(string key);
 
         /// <summary>
         /// Returns a byte[] to byte[] iterator for a byte[] prefix on current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Find")]
-        public static Iterator<byte[], byte[]> Find(byte[] prefix);
+        public static extern Iterator<byte[], byte[]> Find(byte[] prefix);
 
         /// <summary>
         /// Returns a string to byte[] iterator for a string prefix on current Storage context
         /// </summary>
         [Syscall("Neo.Storage.GetContext")]
         [Syscall("Neo.Storage.Find")]
-        public static Iterator<string, byte[]> Find(string prefix);
+        public static extern Iterator<string, byte[]> Find(string prefix);
     }
 }
