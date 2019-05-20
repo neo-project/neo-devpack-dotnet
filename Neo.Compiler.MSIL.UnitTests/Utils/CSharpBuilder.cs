39fb4d54681208c1fba00337d2dbed216644a958
erikzhang <erik@neo.org>
2019-05-21 00:04:13 +0800

    Remove `needlib`

--- Neo.Compiler.MSIL.UnitTests/Utils/CSharpBuilder.cs
+++ Neo.Compiler.MSIL.UnitTests/Utils/CSharpBuilder.cs
@@ -1,33 +1,33 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.CodeAnalysis.Emit;
+using System.IO;
+using System.Reflection;
 
 namespace Neo.Compiler.MSIL.Utils
 {
     class CSharpBuilder
     {
-        public static Microsoft.CodeAnalysis.Emit.EmitResult BuildDllBySignleFile(string filename, string frameworkfile,System.IO.Stream streamDll,System.IO.Stream streamPdb )
+        public static EmitResult BuildDllBySignleFile(string filename, string frameworkfile, Stream streamDll, Stream streamPdb)
         {
             //set curpath
             var pepath = typeof(CSharpBuilder).Assembly.Location;
-            pepath = System.IO.Path.GetDirectoryName(pepath);
-            System.IO.Directory.SetCurrentDirectory(pepath);
+            pepath = Path.GetDirectoryName(pepath);
+            Directory.SetCurrentDirectory(pepath);
 
 
-            var srccode = System.IO.File.ReadAllText(filename);
-            var tree = Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree.ParseText(srccode);
+            var srccode = File.ReadAllText(filename);
+            var tree = CSharpSyntaxTree.ParseText(srccode);
 
-            var op = new CSharpCompilationOptions(Microsoft.CodeAnalysis.OutputKind.DynamicallyLinkedLibrary);
-            var ref1 = MetadataReference.CreateFromFile("needlib" + System.IO.Path.DirectorySeparatorChar + "mscorlib.dll");
-            var ref2 = MetadataReference.CreateFromFile("needlib" + System.IO.Path.DirectorySeparatorChar + "System.dll");
-            var ref3 = MetadataReference.CreateFromFile("needlib" + System.IO.Path.DirectorySeparatorChar + "System.Numerics.dll");
-            var ref4 = MetadataReference.CreateFromFile(frameworkfile);
-            var comp = Microsoft.CodeAnalysis.CSharp.CSharpCompilation.Create("aaa.dll", new[] { tree },
-               new[] { ref1, ref2, ref3, ref4 }, op);
+            var op = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+            var coreDir = Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location);
+            var comp = CSharpCompilation.Create("aaa.dll", new[] { tree }, new[]
+            {
+                MetadataReference.CreateFromFile(Path.Combine(coreDir, "mscorlib.dll")),
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(frameworkfile)
+            }, op);
 
-            var file = System.IO.Path.GetFileNameWithoutExtension(filename);
             var result = comp.Emit(streamDll, streamPdb);
             return result;
         }
