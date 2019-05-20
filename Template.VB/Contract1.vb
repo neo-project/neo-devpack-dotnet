c667d35c975d7d06d9957f7690f0c724dfc21a8f
erikzhang <erik@neo.org>
2019-05-20 20:12:58 +0800

    Update the project templates to .NET Standard 2.0

--- Template.VB/Contract1.vb
+++ Template.VB/Contract1.vb
@@ -1,4 +1,9 @@
-﻿Public Class $itemname$ : Inherits SmartContract
+﻿Imports Neo.SmartContract.Framework
+Imports Neo.SmartContract.Framework.Services.Neo
+Imports System
+Imports System.Numerics
+
+Public Class $itemname$ : Inherits SmartContract
     Public Shared Function Main(ByVal operation As String, ByVal args() As Object) As Boolean
         Storage.Put("Hello", "World")
         Return True
