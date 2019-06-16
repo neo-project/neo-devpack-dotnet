6a042d6da9beeb7072c94b89bf0dd08d18ec4340
lights li <lightsever@hotmail.com>
2019-06-16 21:13:34 +0800

    unittest for staticvar (#65)
    * shift for bigint
    
    * unittest for static var.
    
    without jsonconfig.
    
    * Fix tests

---
+++ tests/Neo.Compiler.MSIL.UnitTests/TestClasses/Contract_StaticVar.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo.Compiler.MSIL.TestClasses
+{
+
+    class Contract_staticvar : SmartContract.Framework.SmartContract
+    {
+        static int a1 = 1;
+
+        public static object Main(string method, object[] args)
+        {
+            testadd();
+            testmulti();
+            return a1;
+        }
+
+        static void testadd()
+        {
+            a1 += 5;
+        }
+        static void testmulti()
+        {
+            a1 *= 7;
+        }
+    }
+
+}
