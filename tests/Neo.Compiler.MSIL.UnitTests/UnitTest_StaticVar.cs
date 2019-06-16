0ded42a3c42482fd5c7f38cfcb32ea9f2b357135
lights <lightsever@hotmail.com>
2019-06-16 11:18:35 +0800

    unittest for static var.
    without jsonconfig.

---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_StaticVar.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.Utils;
+using Neo.VM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo.Compiler.MSIL
+{
+    [TestClass]
+    public class UnitTest_StaticVar
+    {
+        [TestMethod]
+        public void Test_StaticVar()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_staticvar.cs");
+            StackItem[] _params = new StackItem[] { "testfunc", new StackItem[0] };
+            var result = testengine.ExecuteTestCase(_params);
+
+            //test (1+5)*7 == 42
+            StackItem wantresult = 42;
+            var bequal = wantresult.Equals(result.Pop());
+            Assert.IsTrue(bequal);
+        }
+
+    }
+
+}
