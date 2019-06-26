f57a2157e96ce4a99c344612b29c4ed19caa13da
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-06-26 18:52:41 -0300

    Project template for MacOS using embedded compiler

---
+++ templates/Template.CSharpMacOS/SmartContractTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace SmartContractTemplate
+{
+    public class SmartContractTemplate : SmartContract
+    {
+        public static object Main(string operation, object[] args)
+        {
+            Storage.Put("Hello", "World");
+            return true;
+        }
+    }
+}
