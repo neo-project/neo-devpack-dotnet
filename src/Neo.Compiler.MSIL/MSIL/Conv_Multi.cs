cbb3ca1518a7d3757456cced98f1d45b0e2b1b02
lights li <lightsever@hotmail.com>
2019-08-09 18:37:22 +0800

    add better error info when miss dll (#81)
    * add better error info when miss dll
    
    * Optimize
    
    * Prevent throw null exception

--- src/Neo.Compiler.MSIL/MSIL/Conv_Multi.cs
+++ src/Neo.Compiler.MSIL/MSIL/Conv_Multi.cs
@@ -475,13 +475,14 @@ namespace Neo.Compiler.MSIL
             string[] calldata = null;
 
             Mono.Cecil.MethodDefinition defs = null;
+            Exception defError = null;
             try
             {
                 defs = refs.Resolve();
             }
-            catch
+            catch (Exception err)
             {
-
+                defError = err;
             }
 
             if (IsNonCall(defs))
@@ -791,6 +792,15 @@ namespace Neo.Compiler.MSIL
 
             if (calltype == 0)
             {
+                if (defs == null && defError != null)
+                {
+                    if (defError is Mono.Cecil.AssemblyResolutionException dllError)
+                    {
+                        logger.Log("<Error>Miss a Symbol in :" + dllError.AssemblyReference.FullName);
+                        logger.Log("<Error>Check DLLs for contract.");
+                    }
+                    throw defError;
+                }
                 //之前的所有尝试都无效，那也不一定是个不存在的函数，有可能在别的模块里
                 if (TryInsertMethod(outModule, defs))
                 {
