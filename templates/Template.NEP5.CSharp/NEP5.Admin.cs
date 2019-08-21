2700181866f87e9961179bb7957d89b46318f5ae
Belane <srbelane@gmail.com>
2019-08-21 14:05:08 +0200

    draft

---
+++ templates/Template.NEP5.CSharp/NEP5.Admin.cs
@@ -0,0 +1,29 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+
+namespace Template.NEP5.CSharp
+{
+    public partial class NEP5 : SmartContract
+    {
+        private static bool Deploy()
+        {
+            if (!Runtime.CheckWitness(Owner()))
+            {
+                return false;
+            }
+
+            StorageMap contract = Storage.CurrentContext.CreateMap(GetStoragePrefixContract());
+            if (contract.Get("totalSupply").Length != 0)
+                throw new Exception ("Contract already deployed");
+
+            StorageMap balances = Storage.CurrentContext.CreateMap(GetStoragePrefixBalance());
+            balances.Put(Owner(), MaxSupply());
+            contract.Put("totalSupply", MaxSupply());
+
+            // TODO event Transfer
+
+            return true;
+        }
+    }
+}
---
+++ templates/Template.NEP5.CSharp/NEP5.Helpers.cs
@@ -0,0 +1,25 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Template.NEP5.CSharp
+{
+    public partial class NEP5 : SmartContract
+    {
+        private static bool ValidateAddress(byte[] address)
+        {
+            if (address.Length != 20)
+                return false;
+            if (address[0] != 0x23) // TODO: check neo 3.0 address version
+                return false;
+            if (address.AsBigInteger() == 0)
+                return false;
+            return true;
+        }
+
+        private static bool IsPayable(byte[] address)
+        {
+            var c = Blockchain.GetContract(address);
+            return c == null || c.IsPayable;
+        }
+    }
+}
