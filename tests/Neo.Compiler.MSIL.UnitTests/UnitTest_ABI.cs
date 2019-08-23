3fb6ad91797c0edaeb5290540c7ca119a7dd89e3
Shargon <Shargon@gmail.com>
2019-08-23 18:47:18 +0200

    Fix ut

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ABI.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ABI.cs
@@ -10,7 +10,7 @@ namespace Neo.Compiler.MSIL
         public void Test_ABI()
         {
             var testengine = new TestEngine();
-            testengine.AddEntryScript("./TestClasses/Contract_Event.cs");
+            testengine.AddEntryScript("./TestClasses/Contract_Abi.cs");
             string expectABI = @"{""hash"":""0x77811b3127dea2df1a18230f91396fbcf8c648f4"",""entryPoint"":""Main"",""methods"":[{""name"":""Main"",""parameters"":[{""name"":""method"",""type"":""String""},{""name"":""args"",""type"":""Array""}],""returnType"":""Void""},{""name"":""readOnlyTrue"",""parameters"":[],""returnType"":""Void""},{""name"":""readOnlyFalse1"",""parameters"":[],""returnType"":""Void""},{""name"":""readOnlyFalse2"",""parameters"":[],""returnType"":""Void""}],""readOnlyMethods"":[""readOnlyTrue""],""events"":[{""name"":""transfer"",""parameters"":[{""name"":""arg1"",""type"":""ByteArray""},{""name"":""arg2"",""type"":""ByteArray""},{""name"":""arg3"",""type"":""Integer""}]}]}";
             Assert.AreEqual(testengine.scriptEntry.finialABI.ToString(), expectABI);
         }
