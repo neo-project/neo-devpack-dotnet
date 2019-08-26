66563915179b518231d419bbcc4eb28fb799ae80
Shargon <shargon@gmail.com>
2019-08-26 17:07:32 +0200

    Ut

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_Switch.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_Switch.cs
@@ -1,14 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Compiler.MSIL.Utils;
 using Neo.VM;
+using System;
 
 namespace Neo.Compiler.MSIL
 {
     [TestClass]
     public class UnitTest_Switch
     {
+        /// <summary>
+        /// switch of more than 6 entries require a ComputeStringHash method
+        /// </summary>
         [TestMethod]
-        public void Test_Switch()
+        public void Test_InvalidSwitch()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_SwitchInvalid.cs");
+            Assert.ThrowsException<Exception>(() => testengine.ExecuteTestCaseStandard("1"));
+        }
+
+        [TestMethod]
+        public void Test_ValidSwitch()
         {
             TestEngine testengine;
             RandomAccessStack<StackItem> result;
@@ -18,7 +30,7 @@ namespace Neo.Compiler.MSIL
             for (int x = 0; x <= 5; x++)
             {
                 testengine = new TestEngine();
-                testengine.AddEntryScript("./TestClasses/Contract_Switch.cs");
+                testengine.AddEntryScript("./TestClasses/Contract_SwitchValid.cs");
 
                 result = testengine.ExecuteTestCaseStandard(x.ToString());
                 Assert.AreEqual(result.Pop().GetBigInteger(), x + 1);
@@ -27,7 +39,7 @@ namespace Neo.Compiler.MSIL
             // Test default
 
             testengine = new TestEngine();
-            testengine.AddEntryScript("./TestClasses/Contract_Switch.cs");
+            testengine.AddEntryScript("./TestClasses/Contract_SwitchValid.cs");
 
             result = testengine.ExecuteTestCaseStandard("default");
             Assert.AreEqual(result.Pop().GetBigInteger(), 99);
