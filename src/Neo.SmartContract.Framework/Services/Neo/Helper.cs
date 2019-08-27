20b239153c0cdcbbfe179c39b3d7167f670b986a
Shargon <shargon@gmail.com>
2019-08-27 17:21:37 +0200

    Optimized StorageMap (#89)
    * Optimized StorageMap
    
    * Remove 0s
    
    * Add unit test for storage access
    
    * Add comments
    
    * Changed unit test, for don't pass the prefix
    
    * Fix comment
    
    * Change to AsByteArray
    
    * Fix AsByteArray from byte
    
    * ToByteArray
    
    * Fix comment
    
    * Move and fix
    
    * Clean blank lines
    
    * Fix format
    
    * Fix format

--- src/Neo.SmartContract.Framework/Services/Neo/Helper.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Helper.cs
@@ -9,67 +9,85 @@ namespace Neo.SmartContract.Framework.Services.Neo
             return new StorageMap
             {
                 Context = context,
+                Prefix = prefix.AsByteArray()
+            };
+        }
+
+        public static StorageMap CreateMap(this StorageContext context, byte[] prefix)
+        {
+            return new StorageMap
+            {
+                Context = context,
                 Prefix = prefix
+            };
+        }
+
+        public static StorageMap CreateMap(this StorageContext context, byte prefix)
+        {
+            return new StorageMap
+            {
+                Context = context,
+                Prefix = prefix.ToByteArray()
             };
         }
 
         public static void Delete(this StorageMap map, byte[] key)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key);
+            byte[] k = map.Prefix.Concat(key);
             Storage.Delete(map.Context, k);
         }
 
         public static void Delete(this StorageMap map, string key)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key.AsByteArray());
+            byte[] k = map.Prefix.Concat(key.AsByteArray());
             Storage.Delete(map.Context, k);
         }
 
         public static byte[] Get(this StorageMap map, byte[] key)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key);
+            byte[] k = map.Prefix.Concat(key);
             return Storage.Get(map.Context, k);
         }
 
         public static byte[] Get(this StorageMap map, string key)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key.AsByteArray());
+            byte[] k = map.Prefix.Concat(key.AsByteArray());
             return Storage.Get(map.Context, k);
         }
 
         public static void Put(this StorageMap map, byte[] key, byte[] value)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key);
+            byte[] k = map.Prefix.Concat(key);
             Storage.Put(map.Context, k, value);
         }
 
         public static void Put(this StorageMap map, byte[] key, BigInteger value)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key);
+            byte[] k = map.Prefix.Concat(key);
             Storage.Put(map.Context, k, value);
         }
 
         public static void Put(this StorageMap map, byte[] key, string value)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key);
+            byte[] k = map.Prefix.Concat(key);
             Storage.Put(map.Context, k, value);
         }
 
         public static void Put(this StorageMap map, string key, byte[] value)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key.AsByteArray());
+            byte[] k = map.Prefix.Concat(key.AsByteArray());
             Storage.Put(map.Context, k, value);
         }
 
         public static void Put(this StorageMap map, string key, BigInteger value)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key.AsByteArray());
+            byte[] k = map.Prefix.Concat(key.AsByteArray());
             Storage.Put(map.Context, k, value);
         }
 
         public static void Put(this StorageMap map, string key, string value)
         {
-            byte[] k = map.Prefix.AsByteArray().Concat(new byte[] { 0 }).Concat(key.AsByteArray());
+            byte[] k = map.Prefix.Concat(key.AsByteArray());
             Storage.Put(map.Context, k, value);
         }
     }
---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_StorageMap.cs
@@ -0,0 +1,127 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.Utils;
+using Neo.VM.Types;
+using System.Linq;
+
+namespace Neo.Compiler.MSIL
+{
+    [TestClass]
+    public class UnitTest_StorageMap
+    {
+        private void Put(TestEngine testengine, string method, byte[] prefix, byte[] key, byte[] value)
+        {
+            var result = testengine.ExecuteTestCaseStandard(method, new ByteArray(key), new ByteArray(value));
+            var rItem = result.Pop();
+            Assert.IsInstanceOfType(rItem, typeof(Integer));
+            Assert.AreEqual(1, rItem.GetBigInteger());
+            Assert.AreEqual(1,
+                testengine.Storages
+                .Count(a =>
+                    a.Key.Key.SequenceEqual(prefix.Concat(key)) &&
+                    a.Value.Value.SequenceEqual(value) &&
+                    !a.Value.IsConstant
+                    ));
+        }
+
+        private byte[] Get(TestEngine testengine, string method, byte[] prefix, byte[] key)
+        {
+            var result = testengine.ExecuteTestCaseStandard(method, new ByteArray(key));
+            Assert.AreEqual(1, result.Count);
+            var rItem = result.Pop();
+            Assert.IsInstanceOfType(rItem, typeof(ByteArray));
+            Assert.AreEqual(1, testengine.Storages.Count(a => a.Key.Key.SequenceEqual(prefix.Concat(key))));
+            return rItem.GetByteArray();
+        }
+
+        private bool Delete(TestEngine testengine, string method, byte[] prefix, byte[] key)
+        {
+            var result = testengine.ExecuteTestCaseStandard(method, new ByteArray(key));
+            Assert.AreEqual(1, result.Count);
+            var rItem = result.Pop();
+            Assert.IsInstanceOfType(rItem, typeof(Boolean));
+            Assert.AreEqual(0, testengine.Storages.Count(a => a.Key.Key.SequenceEqual(prefix.Concat(key))));
+            return rItem.GetBoolean();
+        }
+
+        [TestMethod]
+        public void Test_Byte()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_StorageMap.cs");
+
+            var prefix = new byte[] { 0xAA };
+            var key = new byte[] { 0x01, 0x02, 0x03 };
+            var value = new byte[] { 0x04, 0x05, 0x06 };
+
+            // Put
+
+            Put(testengine, "TestPutByte", prefix, key, value);
+
+            // Get
+
+            var getVal = Get(testengine, "TestGetByte", prefix, key);
+            CollectionAssert.AreEqual(value, getVal);
+
+            // Delete
+
+            var del = Delete(testengine, "TestDeleteByte", prefix, key);
+            Assert.IsTrue(del);
+            del = Delete(testengine, "TestDeleteByte", prefix, key);
+            Assert.IsFalse(del);
+        }
+
+        [TestMethod]
+        public void Test_ByteArray()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_StorageMap.cs");
+
+            var prefix = new byte[] { 0x00, 0xFF };
+            var key = new byte[] { 0x01, 0x02, 0x03 };
+            var value = new byte[] { 0x04, 0x05, 0x06 };
+
+            // Put
+
+            Put(testengine, "TestPutByteArray", prefix, key, value);
+
+            // Get
+
+            var getVal = Get(testengine, "TestGetByteArray", prefix, key);
+            CollectionAssert.AreEqual(value, getVal);
+
+            // Delete
+
+            var del = Delete(testengine, "TestDeleteByteArray", prefix, key);
+            Assert.IsTrue(del);
+            del = Delete(testengine, "TestDeleteByteArray", prefix, key);
+            Assert.IsFalse(del);
+        }
+
+        [TestMethod]
+        public void Test_String()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_StorageMap.cs");
+
+            var prefix = new byte[] { 0x61, 0x61 };
+            var key = new byte[] { 0x01, 0x02, 0x03 };
+            var value = new byte[] { 0x04, 0x05, 0x06 };
+
+            // Put
+
+            Put(testengine, "TestPutString", prefix, key, value);
+
+            // Get
+
+            var getVal = Get(testengine, "TestGetString", prefix, key);
+            CollectionAssert.AreEqual(value, getVal);
+
+            // Delete
+
+            var del = Delete(testengine, "TestDeleteString", prefix, key);
+            Assert.IsTrue(del);
+            del = Delete(testengine, "TestDeleteString", prefix, key);
+            Assert.IsFalse(del);
+        }
+    }
+}
