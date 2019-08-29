199f60f1a2d6015045af64ce35d4485f11f7b2ac
Igor Machado <igor.machado@gmail.com>
2019-08-29 15:06:21 +0800

    remove IScriptContainer

--- src/Neo.SmartContract.Framework/Services/Neo/Notification.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Notification.cs
@@ -1,6 +1,6 @@
 namespace Neo.SmartContract.Framework.Services.Neo
 {
-    public class Notification : IScriptContainer
+    public class Notification
     {
         public byte[] ScriptHash { get; }
         public object State { get; }
