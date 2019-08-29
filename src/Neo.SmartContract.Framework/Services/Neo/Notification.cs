627831b8c57236a7e473830088c9154113425cde
Shargon <shargon@gmail.com>
2019-08-29 10:07:10 +0200

    Add comments

--- src/Neo.SmartContract.Framework/Services/Neo/Notification.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Notification.cs
@@ -2,8 +2,14 @@ namespace Neo.SmartContract.Framework.Services.Neo
 {
     public class Notification : IApiInterface
     {
+        /// <summary>
+        /// Sender script hash
+        /// </summary>
         public byte[] ScriptHash;
 
+        /// <summary>
+        /// Notification's state
+        /// </summary>
         public object State;
     }
 }
