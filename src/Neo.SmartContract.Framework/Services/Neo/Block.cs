c6c98605ff06ba167fbf7f7ecc0b54f845a9f0fc
Shargon <shargon@gmail.com>
2019-09-11 18:07:25 +0200

    Update block

--- src/Neo.SmartContract.Framework/Services/Neo/Block.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Block.cs
@@ -3,5 +3,7 @@ namespace Neo.SmartContract.Framework.Services.Neo
     public class Block 
     {
         public readonly byte[] Hash;
+
+        // TODO: Wait for merge https://github.com/neo-project/neo/pull/1081
     }
 }
