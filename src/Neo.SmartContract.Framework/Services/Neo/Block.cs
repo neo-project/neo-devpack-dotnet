7f2f83bb751762d754f77fdd840a62768bddcef6
Shargon <shargon@gmail.com>
2019-09-19 14:23:40 +0200

    Update values

--- src/Neo.SmartContract.Framework/Services/Neo/Block.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Block.cs
@@ -3,6 +3,13 @@ namespace Neo.SmartContract.Framework.Services.Neo
     public class Block 
     {
         public readonly byte[] Hash;
+        public readonly uint Version;
+        public readonly byte[] PrevHash;
+        public readonly byte[] MerkleRoot;
+        public readonly ulong Timestamp;
+        public readonly uint Index;
+        public readonly byte[] NextConsensus;
+        public readonly int TransactionsCount;
 
         // TODO: Wait for merge https://github.com/neo-project/neo/pull/1081
     }
--- src/Neo.SmartContract.Framework/Services/Neo/Transaction.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Transaction.cs
@@ -3,6 +3,13 @@ namespace Neo.SmartContract.Framework.Services.Neo
     public class Transaction
     {
         public readonly byte[] Hash;
+        public readonly byte Version;
+        public readonly uint Nonce;
+        public readonly byte[] Sender;
+        public readonly long SystemFee;
+        public readonly long NetworkFee;
+        public readonly uint ValidUntilBlock;
+        public readonly byte[] Script;
 
         // TODO: Wait for merge https://github.com/neo-project/neo/pull/1081
     }
