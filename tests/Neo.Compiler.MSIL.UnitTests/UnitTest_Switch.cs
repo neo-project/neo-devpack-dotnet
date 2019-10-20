fc0d4e21a2f86c18dcbcc81b91212dcf2182f599
Shargon <shargon@gmail.com>
2019-10-20 09:30:09 +0200

    Switch unit test (#87)
    * Switch unit test
    
    * Clean
    
    * Update UnitTest_Switch.cs
    
    * Valid switch
    
    * Test invalid switch
    
    * Ut
    
    * Test wrong switch

---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_Switch.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.Utils;
+using Neo.VM;
+using System;
+
+namespace Neo.Compiler.MSIL
+{
+    [TestClass]
+    public class UnitTest_Switch
+    {
+        /// <summary>
+        /// switch of more than 6 entries require a ComputeStringHash method
+        /// </summary>
+        [TestMethod]
+        public void Test_InvalidSwitch()
+        {
+            var testengine = new TestEngine();
+            Assert.ThrowsException<Exception>(() => testengine.AddEntryScript("./TestClasses/Contract_SwitchInvalid.cs"));
+        }
+
+        [TestMethod]
+        public void Test_ValidSwitch()
+        {
+            TestEngine testengine;
+            RandomAccessStack<StackItem> result;
+
+            // Test cases
+
+            for (int x = 0; x <= 5; x++)
+            {
+                testengine = new TestEngine();
+                testengine.AddEntryScript("./TestClasses/Contract_SwitchValid.cs");
+
+                result = testengine.ExecuteTestCaseStandard(x.ToString());
+                Assert.AreEqual(result.Pop().GetBigInteger(), x + 1);
+            }
+
+            // Test default
+
+            testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_SwitchValid.cs");
+
+            result = testengine.ExecuteTestCaseStandard("default");
+            Assert.AreEqual(result.Pop().GetBigInteger(), 99);
+        }
+    }
+}
