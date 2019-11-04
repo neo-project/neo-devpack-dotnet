460062c9c6a978557a551144cb43375d3da6c16e
lights <lightsever@hotmail.com>
2019-11-04 22:39:02 +0800

    add unittest framework for neo-devpack-dotnet.

---
+++ Neo.Compiler.MSIL.UnitTests/UnitTest_Appcall.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.Utils;
+using Neo.VM;
+
+namespace Neo.Compiler.MSIL
+{
+    [TestClass]
+    public class UnitTest_AppCall
+    {
+        [TestMethod]
+        public void Test_Appcall()
+        {
+            //var hash = UInt160.Parse("0102030405060708090A0102030405060708090A");
+            //var testengine = new TestEngine();
+
+            //testengine.Snapshot.Contracts.Add(hash, new Ledger.ContractState()
+            //{
+            //    Manifest = new SmartContract.Manifest.ContractManifest(),
+            //    Script = testengine.Build("./TestClasses/Contract1.cs").finalNEF
+            //});
+
+            ////will appcall 0102030405060708090A0102030405060708090A
+            //testengine.AddEntryScript("./TestClasses/Contract_appcall.cs");
+
+            //var result = testengine.GetMethod("testfunc").Run();
+            //StackItem wantresult = new byte[] { 1, 2, 3, 4 };
+
+            //var bequal = wantresult.Equals(result);
+            //Assert.IsTrue(bequal);
+
+        }
+    }
+}
