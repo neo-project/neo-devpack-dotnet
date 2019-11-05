7d7011f45e1ca42872110d53748e3ca65e118a29
Shargon <shargon@gmail.com>
2019-11-05 07:27:02 +0100

    Update NeoModule.cs

--- src/Neo.Compiler.MSIL/NeoModule.cs
+++ src/Neo.Compiler.MSIL/NeoModule.cs
@@ -183,6 +183,11 @@ namespace Neo.Compiler
             name = value.field.DeclaringType.FullName + "::" + value.field.Name;
             displayName = value.displayName;
             paramtypes = value.paramtypes;
+            
+            if (value.returntype != "System.Void")
+            {
+                throw new NotSupportedException($"NEP-3 does not support return types for events. Expected: `System.Void`, Detected: `{value.returntype}`");
+            }
         }
     }
 
