559ff0ce427f48b347d9a9be6258638714ddf8cd
Shargon <shargon@gmail.com>
2019-12-11 23:18:46 +0100

    Remove compiler directives

--- src/Neo.Compiler.MSIL/MSIL/ILModule.cs
+++ src/Neo.Compiler.MSIL/MSIL/ILModule.cs
@@ -20,14 +20,12 @@ namespace Neo.Compiler.MSIL
         public void LoadModule(System.IO.Stream dllStream, System.IO.Stream pdbStream)
         {
             this.module = Mono.Cecil.ModuleDefinition.ReadModule(dllStream);
-            //#if WITHPDB
             if (pdbStream != null)
             {
                 var debugInfoLoader = new Mono.Cecil.Pdb.PdbReaderProvider();
 
                 module.ReadSymbols(debugInfoLoader.GetSymbolReader(module, pdbStream));
             }
-            //#endif
             if (module.HasAssemblyReferences)
             {
                 foreach (var ar in module.AssemblyReferences)
@@ -52,7 +50,6 @@ namespace Neo.Compiler.MSIL
                         foreach (var nt in t.NestedTypes)
                         {
                             mapType[nt.FullName] = new ILType(this, nt, logger);
-
                         }
                     }
                 }
