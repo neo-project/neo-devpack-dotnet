2c16f74bec559bd7066f3aeb9c37b5dc638e0054
lights li <lightsever@hotmail.com>
2019-12-17 10:10:16 +0800

    Add UnitTest for Array and fixed int Array Init  problem (#167)

---
+++ tests/Neo.Compiler.MSIL.UnitTests/TestClasses/Contract_Array.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace Neo.Compiler.MSIL.TestClasses
+{
+    struct State
+    {
+        public byte[] from;
+        public byte[] to;
+        public BigInteger amount;
+    }
+
+    class Contract_Array : SmartContract.Framework.SmartContract
+    {
+        public static object TestIntArray()
+        {
+            var arrobj = new int[3];
+            arrobj[0] = 0;
+            arrobj[1] = 1;
+            arrobj[2] = 2;
+            return arrobj;
+        }
+
+        public static object TestDefaultArray()
+        {
+            var arrobj = new int[3];
+            if (arrobj[0] == 0) return true;
+            return false;
+        }
+
+        public static object TestIntArrayInit()
+        {
+            var arrobj = new int[] { 1, 2, 3 };
+            arrobj[1] = 4;
+            arrobj[2] = 5;
+            return arrobj;
+        }
+
+        public static object TestStructArray()
+        {
+            var s = new State();
+            var sarray = new State[3];
+            sarray[2] = s;
+            return sarray[2];
+        }
+
+        public static object TestStructArrayInit()
+        {
+            var s = new State();
+            State[] states = new State[] { s };
+            for (var i = 0; i < 1; i++)
+            {
+                State state = states[i];
+                return state;
+            }
+            return null;
+        }
+    }
+}
---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_Array.cs
@@ -0,0 +1,76 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.Utils;
+using Neo.VM.Types;
+using System.Linq;
+
+namespace Neo.Compiler.MSIL
+{
+    [TestClass]
+    public class UnitTest_Array
+    {
+        [TestMethod]
+        public void Test_IntArray()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_Array.cs");
+            var result = testengine.ExecuteTestCaseStandard("testIntArray");
+
+            //test 0,1,2
+            Assert.IsTrue(result.TryPop(out Array arr));
+            CollectionAssert.AreEqual(new int[] { 0, 1, 2 }, arr.Cast<PrimitiveType>().Select(u => (int)u.ToBigInteger()).ToArray());
+        }
+
+        [TestMethod]
+        public void Test_IntArrayInit()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_Array.cs");
+            var result = testengine.ExecuteTestCaseStandard("testIntArrayInit");
+
+            //test 1,4,5
+            Assert.IsTrue(result.TryPop(out Array arr));
+            CollectionAssert.AreEqual(new int[] { 1, 4, 5 }, arr.Cast<Integer>().Select(u => (int)u.ToBigInteger()).ToArray());
+        }
+
+        /* TODO: We should uncomment this when NEWARRAY_T was done
+        [TestMethod]
+        public void Test_DefaultArray()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_Array.cs");
+            var result = testengine.ExecuteTestCaseStandard("TestDefaultArray");
+
+            //test true
+            Assert.IsTrue(result.TryPop(out Boolean b) && b.ToBoolean());
+        }
+        */
+
+        [TestMethod]
+        public void Test_StructArray()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_Array.cs");
+            var result = testengine.ExecuteTestCaseStandard("testStructArray");
+
+            //test (1+5)*7 == 42
+            var neostruct = result.Pop() as Struct;
+
+            var bequal = neostruct != null;
+            Assert.IsTrue(bequal);
+        }
+
+        [TestMethod]
+        public void Test_StructArrayInit()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_Array.cs");
+            var result = testengine.ExecuteTestCaseStandard("testStructArrayInit");
+
+            //test (1+5)*7 == 42
+            var neostruct = result.Pop() as Struct;
+
+            var bequal = neostruct != null;
+            Assert.IsTrue(bequal);
+        }
+    }
+}
