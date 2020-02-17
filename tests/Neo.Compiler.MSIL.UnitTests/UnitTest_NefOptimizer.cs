1f2a1786076060cfd766d1f64a915a957b9ff478
Shargon <shargon@gmail.com>
2020-02-17 19:13:40 +0100

    PUSHA

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_NefOptimizer.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_NefOptimizer.cs
@@ -91,6 +91,46 @@ namespace Neo.Compiler.MSIL
             Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPNE_L);
         }
 
+        [TestMethod]
+        public void Test_Optimize_Recalculate_Positive_PUSHA()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.PUSHA, ToJumpLArg(7));  // ─┐
+            scriptBefore.Emit(VM.OpCode.NOP);                   //  │
+            scriptBefore.Emit(VM.OpCode.NOP);                   //  │
+            scriptBefore.Emit(VM.OpCode.RET);                   // <┘
+
+            var optimized = new NefOptimizer(scriptBefore.ToArray()).Optimize();
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(VM.OpCode.PUSHA, ToJumpLArg(5));   // ─┐
+            scriptAfter.Emit(VM.OpCode.RET);                    // <┘
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_Negative_PUSHA()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.PUSHA, ToJumpLArg(9));      // ───┐
+            scriptBefore.Emit(VM.OpCode.PUSH1);                     // <┐ │
+            scriptBefore.Emit(VM.OpCode.RET);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.NOP);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.NOP);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.PUSHA, ToJumpLArg(-4));     //  x<┘
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(VM.OpCode.PUSHA, ToJumpLArg(7));       // ───┐
+            scriptAfter.Emit(VM.OpCode.PUSH1);                      // <┐ │
+            scriptAfter.Emit(VM.OpCode.RET);                        //  │ │
+            scriptAfter.Emit(VM.OpCode.PUSHA, ToJumpLArg(-2));      //  x<┘
+
+            var optimized = new NefOptimizer(scriptBefore.ToArray()).Optimize();
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
         private void Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode biGJumpOpCode)
         {
             var smallJumpOpCode = (VM.OpCode)(biGJumpOpCode - 1);
