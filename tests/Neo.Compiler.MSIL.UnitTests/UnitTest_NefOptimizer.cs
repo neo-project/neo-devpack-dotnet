cf4f8c81652d09dfe28273c67f008681f65a99d6
Shargon <shargon@gmail.com>
2020-03-02 13:09:29 +0100

    Optimizer for NEF scripts (#189)
    * Optimizer
    
    * Optimizer
    
    * Add More UT
    
    * PUSHA
    
    * Add commandLineParser
    
    * format
    
    * 1.move Optimizer to  Optimizer/
    2.add unittest param for open Optimizer
    
    * add optimize radio display
    
    * dotnet format
    
    * fixed RecalculateJumps
    
    * 1.重写NefOptimizer的结构
    将优化分成三个明确的步骤
    a.LoadNef
    b.Optimize
    c.LinkNef
    
    暂时屏蔽了优化
    
    * 完成了Link结构，实现了delete Nops方法
    
    * 整理结构
    
    将优化处理器独立出来
    
    将OptimizeTool独立出来
    
    * add miss files
    
    * 实现短地址优化，发现有些问题
    
    * 重构完毕
    
    通过所有UnitTest
    
    * I hate dotnet format.
    
    * use deleteDeadcode to replace deleteNop
    
    It's delete much more code for long nefcode like “unittest Test_SwitchLong”
    
    * clear code
    
    * clear code
    
    * Clean code
    
    * format and a little fix
    
    * format and optimize
    
    * format: remove whitespace
    
    * format: remove whitespace
    
    * optimize and a little fix
    
    * fix SetOpCode
    add some comment
    
    * fix SetOpCode
    
    fix format
    
    * Clean code
    
    * Fix typo
    
    * Clean code and enable NOP optimization
    
    * Arguments in lowercase
    
    * Revert delete nop change
    
    * Clean code
    
    * Add readonly
    
    * Remove static WriteTo
    
    * Fix comment
    
    * Add JMP_NextOpCode as DeadCode
    
    * Move outside JMP2
    
    * rename NeedRightAdds to HasChangedAddress, add optimizer option arguments, fix ut
    
    * format
    
    * up comments
    
    * add both NeedRightAddress and WillChangeAddress
    
    * Clean doble blank line
    
    * reset csproj
    
    * reset Neo.Compiler.MSIL.UnitTests.csproj
    
    * reset neovm-3.0.0-CI00206
    
    * reset Neo.Compiler.MSIL.UnitTests.csproj/add debug config
    
    * reset Neo.Compiler.MSIL.csproj
    
    * add optimizer init method
    
    * add optimize parser enum type
    
    * format
    
    * remove Name field
    
    * rename Main to Compile
    
    Co-authored-by: lights li <lightsever@hotmail.com>
    Co-authored-by: ShawnYun <42930111+ShawnYun@users.noreply.github.com>
    Co-authored-by: Luchuan <luchuan@ngd.neo.org>

---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_NefOptimizer.cs
@@ -0,0 +1,227 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.Optimizer;
+using Neo.VM;
+using System.Buffers.Binary;
+
+namespace Neo.Compiler.MSIL
+{
+    [TestClass]
+    public class UnitTest_NefOptimizer
+    {
+        [TestMethod]
+        public void Test_Optimize_RemoveNOPS()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.NOP);
+            scriptBefore.Emit(VM.OpCode.NOP);
+
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray());
+
+            using var scriptAfter = new ScriptBuilder();
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMP_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMP_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMP_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_CALL_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.CALL_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.CALL_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPEQ_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPEQ_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPEQ_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPGE_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPGE_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPGE_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPGT_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPGT_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPGT_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPIFNOT_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPIFNOT_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPIFNOT_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPIF_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPIF_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPIF_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPLE_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPLE_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPLE_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPLT_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPLT_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPLT_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_JMPNE_L()
+        {
+            Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode.JMPNE_L);
+            Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode.JMPNE_L);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_Positive_PUSHA()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.PUSHA, ToJumpLArg(7));  // ─┐
+            scriptBefore.Emit(VM.OpCode.NOP);                   //  │
+            scriptBefore.Emit(VM.OpCode.NOP);                   //  │
+            scriptBefore.Emit(VM.OpCode.RET);                   // <┘
+
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray());
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(VM.OpCode.PUSHA, ToJumpLArg(5));   // ─┐
+            scriptAfter.Emit(VM.OpCode.RET);                    // <┘
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_Recalculate_Negative_PUSHA()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.PUSHA, ToJumpLArg(9));      // ───┐
+            scriptBefore.Emit(VM.OpCode.PUSH1);                     // <┐ │
+            scriptBefore.Emit(VM.OpCode.RET);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.NOP);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.NOP);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.PUSHA, ToJumpLArg(-4));     //  x<┘
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(VM.OpCode.PUSHA, ToJumpLArg(7));       // ───┐
+            scriptAfter.Emit(VM.OpCode.PUSH1);                      // <┐ │
+            scriptAfter.Emit(VM.OpCode.RET);                        //  │ │
+            scriptAfter.Emit(VM.OpCode.PUSHA, ToJumpLArg(-2));      //  x<┘
+
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray());
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        private void Test_Optimize_Recalculate_Positive_JMPX_L(VM.OpCode biGJumpOpCode)
+        {
+            var smallJumpOpCode = (VM.OpCode)(biGJumpOpCode - 1);
+
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(biGJumpOpCode, ToJumpLArg(7));    // ─┐
+            scriptBefore.Emit(VM.OpCode.NOP);                   //  │
+            scriptBefore.Emit(VM.OpCode.NOP);                   //  │
+            scriptBefore.Emit(VM.OpCode.RET);                   // <┘
+
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray());
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(smallJumpOpCode, ToJumpArg(2));    // ─┐
+            scriptAfter.Emit(VM.OpCode.RET);                    // <┘
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        private void Test_Optimize_Recalculate_Negative_JMPX_L(VM.OpCode biGJumpOpCode)
+        {
+            var smallJumpOpCode = (VM.OpCode)(biGJumpOpCode - 1);
+
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(biGJumpOpCode, ToJumpLArg(9));        // ───┐
+            scriptBefore.Emit(VM.OpCode.PUSH1);                     // <┐ │
+            scriptBefore.Emit(VM.OpCode.RET);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.NOP);                       //  │ │
+            scriptBefore.Emit(VM.OpCode.NOP);                       //  │ │
+            scriptBefore.Emit(biGJumpOpCode, ToJumpLArg(-4));       //  x<┘
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(smallJumpOpCode, ToJumpArg(4));        // ───┐
+            scriptAfter.Emit(VM.OpCode.PUSH1);                      // <┐ │
+            scriptAfter.Emit(VM.OpCode.RET);                        //  │ │
+            scriptAfter.Emit(smallJumpOpCode, ToJumpArg(-2));       //  x<┘
+
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray());
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_JMP_LNext()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.JMP_L, ToJumpLArg(5));       // ───┐
+            scriptBefore.Emit(VM.OpCode.PUSH1);                      // <──┘
+
+            // useshortaddress before deleteuselessjmp
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray(), new OptimizeParserType[] { OptimizeParserType.USE_SHORT_ADDRESS, OptimizeParserType.DELETE_USERLESS_JMP });
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(VM.OpCode.PUSH1);
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+
+            // deleteuselessjmp before useshortaddress
+            optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray(), new OptimizeParserType[] { OptimizeParserType.DELETE_USERLESS_JMP, OptimizeParserType.USE_SHORT_ADDRESS });
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+
+            // use deleteuselessjmp only
+            optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray(), new OptimizeParserType[] { OptimizeParserType.DELETE_USERLESS_JMP });
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        [TestMethod]
+        public void Test_Optimize_JMP_Next()
+        {
+            using var scriptBefore = new ScriptBuilder();
+            scriptBefore.Emit(VM.OpCode.JMP, ToJumpArg(2));        // ───┐
+            scriptBefore.Emit(VM.OpCode.PUSH1);                    // <──┘
+
+            var optimized = NefOptimizeTool.Optimize(scriptBefore.ToArray(), new OptimizeParserType[] { OptimizeParserType.DELETE_USERLESS_JMP });
+
+            using var scriptAfter = new ScriptBuilder();
+            scriptAfter.Emit(VM.OpCode.PUSH1);
+
+            CollectionAssert.AreEqual(scriptAfter.ToArray(), optimized);
+        }
+
+        private byte[] ToJumpLArg(int value)
+        {
+            var ret = new byte[4];
+            BinaryPrimitives.WriteInt32LittleEndian(ret, value);
+            return ret;
+        }
+
+        private byte[] ToJumpArg(int value)
+        {
+            return new byte[1] { (byte)value };
+        }
+    }
+}
