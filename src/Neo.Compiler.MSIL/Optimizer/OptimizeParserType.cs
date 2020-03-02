bba28afcff4bb4a5e8c50dc835845b5fdd635aec
Luchuan <luchuan@neo.org>
2020-03-02 09:40:42 +0800

    remove Name field

--- src/Neo.Compiler.MSIL/Optimizer/OptimizeParserType.cs
+++ src/Neo.Compiler.MSIL/Optimizer/OptimizeParserType.cs
@@ -5,28 +5,26 @@ namespace Neo.Compiler.Optimizer
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     internal class OptimizeParserAttribute : Attribute
     {
-        public string Name { get; }
         public Type Type { get; }
 
-        public OptimizeParserAttribute(string name, Type type)
+        public OptimizeParserAttribute(Type type)
         {
-            Name = name;
             Type = type;
         }
     }
 
     public enum OptimizeParserType : byte
     {
-        [OptimizeParser("deletedeadcode", typeof(Parser_DeleteDeadCode))]
+        [OptimizeParser(typeof(Parser_DeleteDeadCode))]
         DELETE_DEAD_CODDE = 0x01,
 
-        [OptimizeParser("deletenop", typeof(Parser_DeleteNop))]
+        [OptimizeParser(typeof(Parser_DeleteNop))]
         DELETE_NOP = 0x02,
 
-        [OptimizeParser("deleteuselessjmp", typeof(Parser_DeleteUselessJmp))]
+        [OptimizeParser(typeof(Parser_DeleteUselessJmp))]
         DELETE_USERLESS_JMP = 0x03,
 
-        [OptimizeParser("useshortaddress", typeof(Parser_UseShortAddress))]
+        [OptimizeParser(typeof(Parser_UseShortAddress))]
         USE_SHORT_ADDRESS = 0x04,
     }
 }
