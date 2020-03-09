d7f3037e259f94a58b87574d717db976da50eb8a
lights <lightsever@hotmail.com>
2020-03-09 10:33:54 +0800

    add value equal test

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_TypeConvert.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_TypeConvert.cs
@@ -19,12 +19,19 @@ namespace Neo.Compiler.MSIL.UnitTests
             Assert.IsTrue(result.TryPop(out Array arr));
             Assert.IsTrue(arr[0].Type == StackItemType.Integer);
             Assert.IsTrue(arr[1].Type == StackItemType.ByteArray);
+            Assert.IsTrue((arr[0] as PrimitiveType).ToBigInteger() == (arr[1] as PrimitiveType).ToBigInteger());
+            
             Assert.IsTrue(arr[2].Type == StackItemType.Integer);
             Assert.IsTrue(arr[3].Type == StackItemType.ByteArray);
+            Assert.IsTrue((arr[2] as PrimitiveType).ToBigInteger() == (arr[3] as PrimitiveType).ToBigInteger());
+
             Assert.IsTrue(arr[4].Type == StackItemType.ByteArray);
             Assert.IsTrue(arr[5].Type == StackItemType.Integer);
+            Assert.IsTrue((arr[4] as PrimitiveType).ToBigInteger() == (arr[5] as PrimitiveType).ToBigInteger());
+
             Assert.IsTrue(arr[6].Type == StackItemType.ByteArray);
             Assert.IsTrue(arr[7].Type == StackItemType.Integer);
+            Assert.IsTrue((arr[6] as PrimitiveType).ToBigInteger() == (arr[7] as PrimitiveType).ToBigInteger());
         }
     }
 }
