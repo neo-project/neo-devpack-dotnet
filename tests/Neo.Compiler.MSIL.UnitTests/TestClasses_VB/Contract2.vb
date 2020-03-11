4d6929041bdfdf22a9f17ea9e7e3aa2c7ccf7750
lights <lightsever@hotmail.com>
2020-03-11 15:31:46 +0800

    fix vb.net unittest

--- tests/Neo.Compiler.MSIL.UnitTests/TestClasses_VB/Contract2.vb
+++ tests/Neo.Compiler.MSIL.UnitTests/TestClasses_VB/Contract2.vb
@@ -7,9 +7,9 @@ Public Class SmartContract2 : Inherits SmartContract.Framework.SmartContract
         Main = UnitTest_002()
     End Function
 
-    Public Shared Function UnitTest_002()
-        'Dim buffer = New Byte() {1, 2, 3, 4}
-        UnitTest_002 = 3
+    Public Shared Function UnitTest_002() 'no return type here.
+        Dim buffer = New Byte() {1, 2, 3, 4}
+        UnitTest_002 = buffer(2)
     End Function
 
 End Class
