1f7b4a1e974e3bdaf826a8be71e19ebee075dffb
Shargon <shargon@gmail.com>
2020-03-13 10:06:38 +0100

    Update nugets

--- tests/Neo.Compiler.MSIL.UnitTests/Utils/TestSnapshot.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/Utils/TestSnapshot.cs
@@ -15,6 +15,7 @@ namespace Neo.Compiler.MSIL.UnitTests.Utils
         private DataCache<SerializableWrapper<uint>, HeaderHashList> _HeaderHashList = new TestDataCache<SerializableWrapper<uint>, HeaderHashList>();
         private MetaDataCache<HashIndexState> _BlockHashIndex = new TestMetaDataCache<HashIndexState>();
         private MetaDataCache<HashIndexState> _HeaderHashIndex = new TestMetaDataCache<HashIndexState>();
+        private MetaDataCache<ContractIdState> _ContractId = new TestMetaDataCache<ContractIdState>();
 
         public override DataCache<UInt256, TrimmedBlock> Blocks => _Blocks;
 
@@ -23,6 +24,8 @@ namespace Neo.Compiler.MSIL.UnitTests.Utils
         public override DataCache<UInt160, ContractState> Contracts => _Contracts;
 
         public override DataCache<StorageKey, StorageItem> Storages => _Storages;
+
+        public override MetaDataCache<ContractIdState> ContractId => _ContractId;
 
         public override DataCache<SerializableWrapper<uint>, HeaderHashList> HeaderHashList => _HeaderHashList;
 
