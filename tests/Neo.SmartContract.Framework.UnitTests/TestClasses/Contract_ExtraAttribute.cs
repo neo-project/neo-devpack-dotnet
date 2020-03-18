16b5334debff9410321960a619940f3129212236
Ricardo <ricardo.prado@simpli.com.br>
2020-03-18 20:32:01 -0300

    Rebase

---
+++ tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_ExtraAttribute.cs
@@ -0,0 +1,16 @@
+using Neo.SmartContract.Framework;
+
+
+namespace Neo.SmartContract.Framework.UnitTests.TestClasses
+{ 
+
+    [ManifestExtraAttribute("Author", "Neo")]
+    [ManifestExtraAttribute("E-mail", "dev@neo.org")]
+    class Contract_ExtraAttribute : SmartContract
+    {
+        public static object Main(string method, object[] args)
+        {
+            return true;
+        }
+    }
+}
