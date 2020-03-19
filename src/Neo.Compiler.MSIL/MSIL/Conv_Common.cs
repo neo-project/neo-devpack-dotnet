2363b6d21187e93b2762e2c6bd49d9c0d16902bf
Luchuan <luchuan@neo.org>
2020-03-19 18:55:44 +0800

    Add support for static string array (#214) * fix static string array * format

--- src/Neo.Compiler.MSIL/MSIL/Conv_Common.cs
+++ src/Neo.Compiler.MSIL/MSIL/Conv_Common.cs
@@ -145,6 +145,17 @@ namespace Neo.Compiler.MSIL
             _ConvertPushDataArray(data, src, to);
         }
 
+        private void _ConvertPushStringArray(string[] strArray, OpCode src, NeoMethod to)
+        {
+            for (int i = strArray.Length - 1; i >= 0; i--)
+            {
+                var str = strArray[i];
+                _ConvertPushString(str, src, to);
+            }
+            _ConvertPushNumber(strArray.Length, src, to);
+            _Insert1(VM.OpCode.PACK, "", to);
+        }
+
         private int _ConvertPushI8WithConv(ILMethod from, long i, OpCode src, NeoMethod to)
         {
             var next = from.GetNextCodeAddr(src.addr);
@@ -286,6 +297,10 @@ namespace Neo.Compiler.MSIL
                     else if (_src is BigInteger bisrc)
                     {
                         _ConvertPushNumber(bisrc, null, to);
+                    }
+                    else if (_src is string[] strArray)
+                    {
+                        _ConvertPushStringArray(strArray, null, to);
                     }
                     else
                     {
