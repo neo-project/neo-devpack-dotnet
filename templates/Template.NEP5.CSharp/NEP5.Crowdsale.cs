595e89860c354fd225bf562507d84f3eec56b690
belane <belane@users.noreply.github.com>
2020-03-22 02:46:29 +0100

    NEP5 Template for NEO3 (#187)
    * sc and tests
    
    * dotnet format
    
    * Prepare for new SYSCALL
    
    * Add FeaturesAttribute
    
    * post build SC compilation
    
    * revert GetCurrentTransaction
    
    * Add ContractId
    
    * fix Deploy
    
    * check null balances
    
    * updates from master
    
    * Update crowdsale get balance
    
    Co-Authored-By: Luchuan <luchuan@ngd.neo.org>
    
    * ToBigInteger
    
    * dotnet format
    
    * migrate args
    
    * NeoToken little-endian
    
    Co-Authored-By: GripZhang <xiuboard@foxmail.com>
    
    * GasToken little-endian
    
    Co-Authored-By: GripZhang <xiuboard@foxmail.com>
    
    * totalSupply and ut
    
    * supply factor
    
    * shargon improvements
    
    * li optimizations
    
    * more changes
    
    * add 214
    
    Co-authored-by: Shargon <Shargon@gmail.com>
    Co-authored-by: Luchuan <luchuan@ngd.neo.org>
    Co-authored-by: GripZhang <xiuboard@foxmail.com>
    Co-authored-by: lights li <lightsever@hotmail.com>

---
+++ templates/Template.NEP5.CSharp/NEP5.Crowdsale.cs
@@ -0,0 +1,76 @@
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+using System.Numerics;
+
+namespace Template.NEP5.CSharp
+{
+    public partial class NEP5 : SmartContract
+    {
+        private static BigInteger GetTransactionAmount(object state)
+        {
+            var notification = (object[])state;
+            // Checks notification format
+            if (notification.Length != 4) return 0;
+            // Only allow Transfer notifications
+            if ((string)notification[0] != "Transfer") return 0;
+            // Check dest
+            if ((byte[])notification[2] != ExecutionEngine.ExecutingScriptHash) return 0;
+            // Amount
+            var amount = (BigInteger)notification[3];
+            if (amount < 0) return 0;
+            return amount;
+        }
+
+        private static bool Mint()
+        {
+            if (Runtime.InvocationCounter != 1)
+                throw new Exception();
+
+            var notifications = Runtime.GetNotifications();
+            if (notifications.Length == 0)
+                throw new Exception("Contribution transaction not found");
+
+            BigInteger neo = 0;
+            BigInteger gas = 0;
+
+            for (int i = 0; i < notifications.Length; i++)
+            {
+                var notification = notifications[i];
+
+                if (notification.ScriptHash == NeoToken)
+                {
+                    neo += GetTransactionAmount(notification.State);
+                }
+                else if (notification.ScriptHash == GasToken)
+                {
+                    gas += GetTransactionAmount(notification.State);
+                }
+            }
+
+            StorageMap contract = Storage.CurrentContext.CreateMap(StoragePrefixContract);
+            var supply = contract.Get("totalSupply");
+            if (supply == null)
+                throw new Exception("Contract not deployed");
+
+            var current_supply = supply.ToBigInteger();
+            var avaliable_supply = MaxSupply - current_supply;
+
+            var contribution = (neo * TokensPerNEO) + (gas * TokensPerGAS);
+            if (contribution <= 0)
+                throw new Exception();
+            if (contribution > avaliable_supply)
+                throw new Exception();
+
+            StorageMap balances = Storage.CurrentContext.CreateMap(StoragePrefixBalance);
+            Transaction tx = (Transaction)ExecutionEngine.ScriptContainer;
+            var balance = balances.Get(tx.Sender)?.ToBigInteger() ?? 0;
+            balances.Put(tx.Sender, balance + contribution);
+            contract.Put("totalSupply", current_supply + contribution);
+
+            OnTransfer(null, tx.Sender, balance + contribution);
+            return true;
+        }
+    }
+}
---
+++ templates/Template.NEP5.CSharp/NEP5.Methods.cs
@@ -0,0 +1,54 @@
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace Template.NEP5.CSharp
+{
+    public partial class NEP5 : SmartContract
+    {
+        private static BigInteger TotalSupply()
+        {
+            StorageMap contract = Storage.CurrentContext.CreateMap(StoragePrefixContract);
+            return contract.Get("totalSupply")?.ToBigInteger() ?? 0;
+        }
+
+        private static BigInteger BalanceOf(byte[] account)
+        {
+            if (!ValidateAddress(account)) throw new FormatException("The parameter 'account' SHOULD be 20-byte addresses.");
+
+            StorageMap balances = Storage.CurrentContext.CreateMap(StoragePrefixBalance);
+            return balances.Get(account)?.ToBigInteger() ?? 0;
+        }
+
+        private static bool Transfer(byte[] from, byte[] to, BigInteger amount)
+        {
+            if (!ValidateAddress(from)) throw new FormatException("The parameter 'from' SHOULD be 20-byte addresses.");
+            if (!ValidateAddress(to)) throw new FormatException("The parameters 'to' SHOULD be 20-byte addresses.");
+            if (!IsPayable(to)) return false;
+            if (amount <= 0) throw new InvalidOperationException("The parameter amount MUST be greater than 0.");
+            if (!Runtime.CheckWitness(from)) return false;
+
+            StorageMap balances = Storage.CurrentContext.CreateMap(StoragePrefixBalance);
+            BigInteger fromAmount = balances.Get(from).ToBigInteger();
+
+            if (fromAmount < amount) return false;
+            if (amount == 0 || from == to) return true;
+
+            if (fromAmount == amount)
+            {
+                balances.Delete(from);
+            }
+            else
+            {
+                balances.Put(from, fromAmount - amount);
+            }
+
+            BigInteger toAmount = balances.Get(to)?.ToBigInteger() ?? 0;
+            balances.Put(to, toAmount + amount);
+
+            OnTransfer(from, to, amount);
+            return true;
+        }
+    }
+}
