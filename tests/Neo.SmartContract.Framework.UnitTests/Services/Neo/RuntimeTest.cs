6b29aaf4a8cafa51e48c841f307c9e5cdd21ce1f
ZhangHaoqiang <gripzhang@outlook.com>
2020-03-26 15:41:18 +0800

    Add GasLeft UT

--- tests/Neo.SmartContract.Framework.UnitTests/Services/Neo/RuntimeTest.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/Services/Neo/RuntimeTest.cs
@@ -89,6 +89,17 @@ namespace Neo.SmartContract.Framework.UnitTests.Services.Neo
             Assert.IsInstanceOfType(item, typeof(Integer));
             Assert.AreEqual(0x01, item.GetBigInteger());
         }
+        
+        [TestMethod]
+        public void Test_GasLeft()
+        {
+            var result = _engine.ExecuteTestCaseStandard("getGasLeft");
+            Assert.AreEqual(1, result.Count);
+
+            var item = result.Pop();
+            Assert.IsInstanceOfType(item, typeof(Integer));
+            Assert.AreEqual(-1, item.GetBigInteger());
+        }
 
         [TestMethod]
         public void Test_Time()
