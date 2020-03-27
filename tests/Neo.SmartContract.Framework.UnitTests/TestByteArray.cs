dca22dc2fc254f069d4d31edb4783cda82e5963b
Luchuan <luchuan@neo.org>
2020-03-27 19:07:44 +0800

    remove useless code

--- tests/Neo.SmartContract.Framework.UnitTests/TestByteArray.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestByteArray.cs
@@ -88,41 +88,19 @@ namespace Neo.SmartContract.Framework.UnitTests
         [TestMethod]
         public void TestLast()
         {
-            //var testengine = new TestEngine();
-            //testengine.AddEntryScript("./TestClasses/Contract_ByteArray.cs");
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_ByteArray.cs");
 
-            //var result = testengine.ExecuteTestCaseStandard("testByteLast");
+            var result = testengine.ExecuteTestCaseStandard("testByteLast");
 
-            //Assert.AreEqual(1, testengine.Notifications.Count);
-            //var array = testengine.Notifications[0].State as Array;
-            //Assert.AreEqual(1, array.Count);
-            //var buffer = array[0] as Buffer;
-            //Assert.AreEqual(new byte[] { 0x32 }, buffer.ConvertTo(StackItemType.ByteArray));
+            Assert.AreEqual(1, testengine.Notifications.Count);
+            var array = testengine.Notifications[0].State as Array;
+            Assert.AreEqual(1, array.Count);
+            var buffer = array[0] as Buffer;
+            Assert.AreEqual(new byte[] { 0x32 }, buffer.ConvertTo(StackItemType.ByteArray));
 
-            //buffer = result.Pop() as Buffer;
-            //Assert.AreEqual(new byte[] { 0x32 }, buffer.ConvertTo(StackItemType.ByteArray));
-
-
-            Dictionary<byte[], int> map = new Dictionary<byte[], int>();
-
-            var k1 = new byte[] { 0x00, 0x01 };
-            var k2 = new byte[] { 0x01, 0x01 };
-            map[k1] = 1;
-            map[k2] = 2;
-
-            System.Console.WriteLine("before map[k1]: " + map[k1] + "\t k1 hashcode: " + k1.GetHashCode());
-            System.Console.WriteLine("before map[k2]: " + map[k2] + "\t k2 hashcode: " + k2.GetHashCode());
-
-            k1[0] = 0x01;
-            k2[0] = 0x00;
-
-            System.Console.WriteLine("after map[k1]: " + map[k1] + "\t k1 hashcode: " + k1.GetHashCode());
-            System.Console.WriteLine("after map[k2]: " + map[k2] + "\t k2 hashcode: " + k2.GetHashCode());
-
-            int a = 1;
-            System.Console.WriteLine(a + " hashcode " + a.GetHashCode());
-            a = 2;
-            System.Console.WriteLine(a + " hashcode " + a.GetHashCode());
+            buffer = result.Pop() as Buffer;
+            Assert.AreEqual(new byte[] { 0x32 }, buffer.ConvertTo(StackItemType.ByteArray));
         }
     }
 }
