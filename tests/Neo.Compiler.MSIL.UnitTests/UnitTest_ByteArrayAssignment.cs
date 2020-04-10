afe8f4e21b7c610179a91c12d98fe2567a6f644b
Shawn <yangxiaoyun@neo.org>
2020-04-10 17:23:45 +0800

    format

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ByteArrayAssignment.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ByteArrayAssignment.cs
@@ -13,9 +13,9 @@ namespace Neo.Compiler.MSIL.UnitTests
             var testengine = new TestEngine();
             testengine.AddEntryScript("./TestClasses/Contract_ByteArrayAssignment.cs");
 
-            var result = testengine.GetMethod("testByteArrayAssignment").Run();//new test method01
+            var result = testengine.GetMethod("testByteArrayAssignment").Run();
             StackItem wantresult = new byte[] { 0x01, 0x02, 0x04 };
-            
+
             Assert.AreEqual(wantresult.ConvertTo(StackItemType.ByteString), result.ConvertTo(StackItemType.ByteString));
         }
     }
