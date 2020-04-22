50035cbfafddd9c5e527e9afb0ec9c42a3ef646d
Owen Zhang <38493437+superboyiii@users.noreply.github.com>
2020-04-22 10:37:04 +0800

    Release neon v3.0.0-Preview2 (#250) * Release neon v3.0.0-Preview2 * Update Syscall("System.Contract.CreateStandardAccount") in UT * Fix Fix * Fix CreateStandardAccount * Add UT * Fix comment Co-authored-by: Shargon <shargon@gmail.com>

--- tests/Neo.SmartContract.Framework.UnitTests/Services/Neo/ContractTest.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/Services/Neo/ContractTest.cs
@@ -183,5 +183,40 @@ namespace Neo.SmartContract.Framework.UnitTests.Services.Neo
             Assert.AreEqual(VMState.FAULT, _engine.State);
             Assert.AreEqual(0, result.Count);
         }
+
+        [TestMethod]
+        public void Test_CreateStandardAccount()
+        {
+            // Wrong pubKey
+
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("createStandardAccount", new byte[] { 0x01, 0x02 });
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+            Assert.AreEqual(0, result.Count);
+
+            _engine.Reset();
+
+            // Good pubKey (compressed)
+
+            _engine.Reset();
+            result = _engine.ExecuteTestCaseStandard("createStandardAccount", new byte[] { 0x02, 0x48, 0x6f, 0xd1, 0x57, 0x02, 0xc4, 0x49, 0x0a, 0x26, 0x70, 0x31, 0x12, 0xa5, 0xcc, 0x1d, 0x09, 0x23, 0xfd, 0x69, 0x7a, 0x33, 0x40, 0x6b, 0xd5, 0xa1, 0xc0, 0x0e, 0x00, 0x13, 0xb0, 0x9a, 0x70 });
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(1, result.Count);
+
+            var item = result.Pop();
+            Assert.IsTrue(item.Type == StackItemType.ByteString);
+            Assert.AreEqual("e30262c57431d82b8295174f762fa36a5be973fd", item.GetSpan().ToHexString());
+
+            // Good pubKey (uncompressed)
+
+            _engine.Reset();
+            result = _engine.ExecuteTestCaseStandard("createStandardAccount", new byte[] { 0x04, 0x48, 0x6f, 0xd1, 0x57, 0x02, 0xc4, 0x49, 0x0a, 0x26, 0x70, 0x31, 0x12, 0xa5, 0xcc, 0x1d, 0x09, 0x23, 0xfd, 0x69, 0x7a, 0x33, 0x40, 0x6b, 0xd5, 0xa1, 0xc0, 0x0e, 0x00, 0x13, 0xb0, 0x9a, 0x70, 0x05, 0x43, 0x6c, 0x08, 0x2c, 0x2c, 0x88, 0x08, 0x5b, 0x4b, 0x53, 0xd5, 0x4c, 0x55, 0x66, 0xba, 0x44, 0x8d, 0x5c, 0x3e, 0x2a, 0x2a, 0x5c, 0x3a, 0x3e, 0xa5, 0x00, 0xe1, 0x40, 0x77, 0x55, 0x9c });
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(1, result.Count);
+
+            item = result.Pop();
+            Assert.IsTrue(item.Type == StackItemType.ByteString);
+            Assert.AreEqual("e30262c57431d82b8295174f762fa36a5be973fd", item.GetSpan().ToHexString());
+        }
     }
 }
