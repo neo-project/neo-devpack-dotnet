9197fdb036aaba73f417e9a40d9c287a3d2447c1
Shawn <yangxiaoyun@neo.org>
2020-05-20 10:12:28 +0800

    fix equals

--- src/Neo.SmartContract.Framework/Helper.cs
+++ src/Neo.SmartContract.Framework/Helper.cs
@@ -150,8 +150,8 @@ namespace Neo.SmartContract.Framework
 
         public static bool Equals(this byte[] left, byte[] right)
         {
-            if (left.Length != right.Length) return false;
             if (left == null || right == null) return false;
+            if (left.Length != right.Length) return false; 
             for (int i = 0; i < left.Length; i++)
             {
                 if (left[i] != right[i]) return false;
