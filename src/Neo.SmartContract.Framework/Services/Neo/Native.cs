ba7b9ff204c743f1b2bc6be5e725e737e944e691
陈志同 <chenzhitong12@hotmail.com>
2020-06-12 11:46:13 +0800

    单元测试终于跑通了

--- src/Neo.SmartContract.Framework/Services/Neo/Native.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Native.cs
@@ -4,109 +4,109 @@ namespace Neo.SmartContract.Framework.Services.Neo
 {
     public class Native
     {
-        public static class NEO
+        public class NEO
         {
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string Name(string method = "name");
+            public static extern string Name(string method = "name", object[] arguments = null);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string Symbol(string method = "symbol");
+            public static extern string Symbol(string method = "symbol", object[] arguments = null);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string Decimals(string method = "decimals");
+            public static extern int Decimals(string method = "decimals", object[] arguments = null);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string TotalSupply(string method = "totalSupply");
+            public static extern BigInteger TotalSupply(string method = "totalSupply", object[] arguments = null);
 
             /// <param name="method">Please input &quot;BalanceOf&quot;</param>
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string BalanceOf(string method, byte[] scriptHash);
+            public static extern BigInteger BalanceOf(string method, byte[] scriptHash);
 
             /// <param name="method">Please input &quot;Transfer&quot;</param>
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string Transfer(string method, byte[] from, byte[] to, BigInteger amount);
+            public static extern bool Transfer(string method, byte[] from, byte[] to, BigInteger amount);
 
             /// <param name="method">Please input &quot;RegisterCandidate&quot;</param>
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string RegisterCandidate(string method, byte[] publicKey);
+            public static extern bool RegisterCandidate(string method, byte[] publicKey);
 
             /// <param name="method">Please input &quot;UnregisterCandidate&quot;</param>
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string UnregisterCandidate(string method, byte[] publicKey);
+            public static extern bool UnregisterCandidate(string method, byte[] publicKey);
 
             /// <param name="method">Please input &quot;Vote&quot;</param>
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string Vote(string method, byte[] scriptHash, byte[][] candidatePublicKey);
+            public static extern bool Vote(string method, byte[] scriptHash, byte[][] candidatePublicKey);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string GetValidators(string method = "GetValidators");
+            public static extern byte[][] GetValidators(string method = "GetValidators", object[] arguments = null);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string GetCandidates(string method = "GetCandidates");
+            public static extern byte[][] GetCandidates(string method = "GetCandidates", object[] arguments = null);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string GetCommittee(string method = "GetCommittee");
+            public static extern byte[][] GetCommittee(string method = "GetCommittee", object[] arguments = null);
 
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string GetNextBlockValidators(string method = "GetNextBlockValidators");
+            public static extern byte[][] GetNextBlockValidators(string method = "GetNextBlockValidators", object[] arguments = null);
 
             /// <param name="method">Please input &quot;UnclaimedGas&quot;</param>
             [Appcall("0xde5f57d430d3dece511cf975a8d37848cb9e0525")]
-            public static extern string UnclaimedGas(string method, byte[] scriptHash, uint endBlockIndex);
+            public static extern BigInteger UnclaimedGas(string method, byte[] scriptHash, uint endBlockIndex);
         }
 
-        public static class GAS
+        public class GAS
         {
             [Appcall("0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc")]
-            public static extern string Name(string method = "name");
+            public static extern string Name(string method = "name", object[] arguments = null);
 
             [Appcall("0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc")]
-            public static extern string Symbol(string method = "symbol");
+            public static extern string Symbol(string method = "symbol", object[] arguments = null);
 
             [Appcall("0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc")]
-            public static extern string Decimals(string method = "decimals");
+            public static extern int Decimals(string method = "decimals", object[] arguments = null);
 
             [Appcall("0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc")]
-            public static extern string TotalSupply(string method = "totalSupply");
+            public static extern BigInteger TotalSupply(string method = "totalSupply", object[] arguments = null);
 
             /// <param name="method">Please input &quot;BalanceOf&quot;</param>
             [Appcall("0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc")]
-            public static extern string BalanceOf(string method, byte[] scriptHash);
+            public static extern BigInteger BalanceOf(string method, byte[] scriptHash);
 
             [Appcall("0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc")]
-            public static extern string Transfer(string method, byte[] from, byte[] to, BigInteger amount);
+            public static extern bool Transfer(string method, byte[] from, byte[] to, BigInteger amount);
         }
 
-        public static class Policy
+        public class Policy
         {
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string GetMaxTransactionsPerBlock(string method = "GetMaxTransactionsPerBlock");
+            public static extern BigInteger GetMaxTransactionsPerBlock(string method = "GetMaxTransactionsPerBlock", object[] arguments = null);
 
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string GetFeePerByte(string method = "GetFeePerByte");
+            public static extern BigInteger GetFeePerByte(string method = "GetFeePerByte", object[] arguments = null);
 
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string GetBlockedAccounts(string method = "GetBlockedAccounts");
+            public static extern byte[][] GetBlockedAccounts(string method = "GetBlockedAccounts", object[] arguments = null);
 
             /// <param name="method">Please input &quot;SetMaxBlockSize&quot;</param>
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string SetMaxBlockSize(string method, BigInteger maxBlockSize);
+            public static extern bool SetMaxBlockSize(string method, BigInteger maxBlockSize);
 
             /// <param name="method">Please input &quot;SetMaxTransactionsPerBlock&quot;</param>
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string SetMaxTransactionsPerBlock(string method, BigInteger maxTransactionsPerBlock);
+            public static extern bool SetMaxTransactionsPerBlock(string method, BigInteger maxTransactionsPerBlock);
 
             /// <param name="method">Please input &quot;SetFeePerByte&quot;</param>
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string SetFeePerByte(string method, BigInteger feePerByte);
+            public static extern bool SetFeePerByte(string method, BigInteger feePerByte);
 
             /// <param name="method">Please input &quot;BlockAccount&quot;</param>
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string BlockAccount(string method, byte[] scriptHash);
+            public static extern bool BlockAccount(string method, byte[] scriptHash);
 
             /// <param name="method">Please input &quot;UnblockAccount&quot;</param>
             [Appcall("0xce06595079cd69583126dbfd1d2e25cca74cffe9")]
-            public static extern string UnblockAccount(string method, byte[] scriptHash);
+            public static extern bool UnblockAccount(string method, byte[] scriptHash);
         }
     }
 }
--- tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_Native.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_Native.cs
@@ -9,30 +9,30 @@ namespace Neo.Compiler.MSIL.TestClasses
         [DisplayName("NEO_Decimals")]
         public static int NEO_Decimals()
         {
-            return (int)Native.NEO.Decimals();
+            return (int)Native.NEO.Decimals("decimals", new object[0]);
         }
 
         [DisplayName("NEO_Name")]
         public static string NEO_Name()
         {
-            return (string)Native.NEO.Name();
+            return (string)Native.NEO.Name("name", new object[0]);
         }
 
         [DisplayName("GAS_Decimals")]
         public static int GAS_Decimals()
         {
-            return (int)Native.GAS.Decimals();
+            return (int)Native.GAS.Decimals("decimals", new object[0]);
         }
 
         [DisplayName("GAS_Name")]
         public static string GAS_Name()
         {
-            return (string)Native.GAS.Name();
+            return (string)Native.GAS.Name("name", new object[0]);
         }
 
         public static BigInteger Policy_GetFeePerByte()
         {
-            return (BigInteger)Native.Policy.GetFeePerByte();
+            return (BigInteger)Native.Policy.GetFeePerByte("getFeePerByte", new object[0]);
         }
     }
 }
