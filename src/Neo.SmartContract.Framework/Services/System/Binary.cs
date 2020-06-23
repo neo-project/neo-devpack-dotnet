02507c7baacd10251064f4ff09d29fdaa5faf32e
Shargon <Shargon@gmail.com>
2020-06-23 11:11:28 +0200

    Add Base64 test

--- src/Neo.SmartContract.Framework/Services/System/Binary.cs
+++ src/Neo.SmartContract.Framework/Services/System/Binary.cs
@@ -5,9 +5,9 @@ namespace Neo.SmartContract.Framework.Services.System
     public class Binary
     {
         [Syscall("System.Binary.Base64Decode")]
-        public extern byte[] Base64Decode(string input);
+        public static extern byte[] Base64Decode(string input);
 
         [Syscall("System.Binary.Base64Encode")]
-        public extern string Base64Encode(byte[] input);
+        public static extern string Base64Encode(byte[] input);
     }
 }
