742945c3766803bb4167f4dcb28174fb3b07904a
ShawnYun <yangxiaoyun@neo.org>
2020-07-29 15:47:18 +0800

    Add ContractCall and UTs, delete Appcall (#327)
    * add ConTractCall and UTs, delete Appcall
    
    * fix
    
    * Disable warnings
    
    * spilt and fix
    
    * del ID
    
    * fix Neo and Policy, Add UT
    
    * del blank line
    
    * fix internal
    
    Co-authored-by: erikzhang <erik@neo.org>
    Co-authored-by: Shargon <shargon@gmail.com>

---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ContractCall.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.UnitTests.Utils;
+using Neo.IO.Json;
+using Neo.SmartContract.Manifest;
+using Neo.VM;
+using Neo.VM.Types;
+
+namespace Neo.Compiler.MSIL.UnitTests
+{
+    [TestClass]
+    public class UnitTest_ContractCall
+    {
+        private TestEngine _engine;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var hash = UInt160.Parse("0102030405060708090A0102030405060708090A");
+            _engine = new TestEngine();
+            _engine.Snapshot.Contracts.Add(hash, new Ledger.ContractState()
+            {
+                Script = _engine.Build("./TestClasses/Contract1.cs").finalNEF,
+                Manifest = ContractManifest.FromJson(JObject.Parse(_engine.Build("./TestClasses/Contract1.cs").finalManifest)),
+            });
+
+            //will ContractCall 0102030405060708090A0102030405060708090A
+            _engine.AddEntryScript("./TestClasses/Contract_ContractCall.cs");
+        }
+
+        [TestMethod]
+        public void Test_ContractCall()
+        {
+            var result = _engine.GetMethod("testContractCall").Run().ConvertTo(StackItemType.ByteString);
+            Assert.AreEqual(VMState.HALT, _engine.State);
+
+            StackItem wantresult = new byte[] { 1, 2, 3, 4 };
+            var bequal = wantresult.Equals(result);
+            Assert.IsTrue(bequal);
+        }
+
+        [TestMethod]
+        public void Test_ContractCall_Void()
+        {
+            var result = _engine.ExecuteTestCaseStandard("testContractCallVoid");
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+}
--- tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_Native.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_Native.cs
@@ -1,6 +1,7 @@
 using Neo.SmartContract.Framework.Services.Neo;
 using System.ComponentModel;
 using System.Numerics;
+using System;
 
 namespace Neo.Compiler.MSIL.TestClasses
 {
@@ -9,30 +10,74 @@ namespace Neo.Compiler.MSIL.TestClasses
         [DisplayName("NEO_Decimals")]
         public static int NEO_Decimals()
         {
-            return (int)Native.NEO("decimals", new object[0]);
+            return (int)NEO.Decimals();
         }
 
         [DisplayName("NEO_Name")]
         public static string NEO_Name()
         {
-            return (string)Native.NEO("name", new object[0]);
+            return NEO.Name();
+        }
+
+        [DisplayName("NEO_BalanceOf")]
+        public static BigInteger NEO_BalanceOf(byte[] account)
+        {
+            return NEO.BalanceOf(account);
+        }
+
+        [DisplayName("NEO_GetValidators")]
+        public static string[] NEO_GetValidators()
+        {
+            return NEO.GetValidators();
+        }
+
+        [DisplayName("NEO_RegisterCandidate")]
+        public static bool NEO_RegisterCandidate(byte[] pubkey)
+        {
+            return NEO.RegisterCandidate(pubkey);
+        }
+
+        [DisplayName("NEO_GetCandidates")]
+        public static (string, BigInteger)[] NEO_GetCandidates()
+        {
+            return NEO.GetCandidates();
         }
 
         [DisplayName("GAS_Decimals")]
         public static int GAS_Decimals()
         {
-            return (int)Native.GAS("decimals", new object[0]);
+            return (int)GAS.Decimals();
         }
 
         [DisplayName("GAS_Name")]
         public static string GAS_Name()
         {
-            return (string)Native.GAS("name", new object[0]);
+            return GAS.Name();
         }
 
         public static BigInteger Policy_GetFeePerByte()
         {
-            return (BigInteger)Native.Policy("getFeePerByte", new object[0]);
+            return Policy.GetFeePerByte();
+        }
+
+        public static bool Policy_SetMaxTransactionsPerBlock(uint value)
+        {
+            return Policy.SetMaxTransactionsPerBlock(value);
+        }
+
+        public static uint Policy_GetMaxTransactionsPerBlock()
+        {
+            return Policy.GetMaxTransactionsPerBlock();
+        }
+
+        public static bool Policy_BlockAccount(byte[] account)
+        {
+            return Policy.BlockAccount(account);
+        }
+
+        public static object[] Policy_GetBlockedAccounts()
+        {
+            return Policy.GetBlockedAccounts();
         }
     }
 }
