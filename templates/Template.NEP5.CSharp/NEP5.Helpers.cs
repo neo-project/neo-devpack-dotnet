b9d4ca166ddb08b0d1eaecdd2b81387191ee3ffa
陈志同 <chenzhitong12@hotmail.com>
2020-07-31 01:12:51 -0500

    Numerous modifications and optimization for Template.NEP5.CSharp (#304)

--- templates/Template.NEP5.CSharp/NEP5.Helpers.cs
+++ templates/Template.NEP5.CSharp/NEP5.Helpers.cs
@@ -5,19 +5,8 @@ namespace Template.NEP5.CSharp
 {
     public partial class NEP5 : SmartContract
     {
-        private static bool ValidateAddress(byte[] address)
-        {
-            if (address.Length != 20)
-                return false;
-            if (address.ToBigInteger() == 0)
-                return false;
-            return true;
-        }
+        private static bool ValidateAddress(byte[] address) => address.Length == 20 && address.ToBigInteger() != 0;
 
-        private static bool IsPayable(byte[] address)
-        {
-            var c = Blockchain.GetContract(address);
-            return c == null || c.IsPayable;
-        }
+        private static bool IsPayable(byte[] address) => Blockchain.GetContract(address)?.IsPayable ?? true;
     }
 }
