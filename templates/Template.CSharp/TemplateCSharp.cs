d788a57c40b1b169a7f25dc757c33c7c67fb7bd2
superboyiii <573504781@qq.com>
2020-07-31 17:03:25 +0800

    Update templates to the latest

--- templates/Template.CSharp/TemplateCSharp.cs
+++ templates/Template.CSharp/TemplateCSharp.cs
@@ -3,15 +3,15 @@ using Neo.SmartContract.Framework.Services.Neo;
 using System;
 using System.Numerics;
 
-namespace $safeprojectname$
+namespace neo
 {
     [ManifestExtra("Author", "Neo")]
     [ManifestExtra("Email", "dev@neo.org")]
     [ManifestExtra("Description", "This is a contract example")]
     [Features(ContractFeatures.HasStorage)]
-    public class Contract1 : SmartContract
+    public class TemplateCSharp : SmartContract
     {
-        public static bool Main()
+        public static bool Main(string operation, object[] args)
         {
             Storage.Put("Hello", "World");
             return true;
--- templates/Template.VB/TemplateVB.vb
+++ templates/Template.VB/TemplateVB.vb
@@ -3,8 +3,8 @@ Imports Neo.SmartContract.Framework.Services.Neo
 Imports System
 Imports System.Numerics
 
-Public Class $itemname$ : Inherits SmartContract
-    Public Shared Function Main() As Boolean
+Public Class TemplateVB : Inherits SmartContract
+    Public Shared Function Main(ByVal operation As String, ByVal args() As Object) As Boolean
         Storage.Put("Hello", "World")
         Return True
     End Function
