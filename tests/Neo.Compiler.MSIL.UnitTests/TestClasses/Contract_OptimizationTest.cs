20b5c821ca195ad45648e422bab490bcd86c6a5c
ShawnYun <shawn.yung@foxmail.com>
2020-08-11 11:04:34 +0800

    Update tests/Neo.Compiler.MSIL.UnitTests/TestClasses/Contract_OptimizationTest.cs
    Co-authored-by: Luchuan <luchuan@ngd.neo.org>

--- tests/Neo.Compiler.MSIL.UnitTests/TestClasses/Contract_OptimizationTest.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/TestClasses/Contract_OptimizationTest.cs
@@ -6,6 +6,7 @@ namespace Neo.Compiler.MSIL.TestClasses
     public class Contract_OptimizationTest : SmartContract.Framework.SmartContract
     {
         private static byte[] Owner = "Ne9ipxm2sPUaetvh3ZvjhyCzRZqP355dTZ".ToScriptHash();
+
         public static bool Verify()
         {
             return Runtime.CheckWitness(Owner);
