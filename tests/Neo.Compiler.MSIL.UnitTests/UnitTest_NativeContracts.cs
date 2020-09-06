c81a95cbebf543ab0818bfdd5f1a34d1291609f3
Shargon <shargon@gmail.com>
2020-09-06 16:22:40 +0200

    Test name

---
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_NativeContracts.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.MSIL.UnitTests.Utils;
+
+namespace Neo.Compiler.MSIL.UnitTests
+{
+    [TestClass]
+    public class Contract_NativeContracts
+    {
+        [TestMethod]
+        public void Test_NEO()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_NativeContracts.cs");
+
+            var result = testengine.ExecuteTestCaseStandard("nEOName");
+
+            Assert.AreEqual(VM.VMState.HALT, testengine.State);
+            Assert.AreEqual(0, result.Count);
+
+            var entry = result.Pop();
+
+            Assert.AreEqual("NEO", entry.GetString());
+        }
+
+        [TestMethod]
+        public void Test_GAS()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_NativeContracts.cs");
+
+            var result = testengine.ExecuteTestCaseStandard("gASName");
+
+            Assert.AreEqual(VM.VMState.HALT, testengine.State);
+            Assert.AreEqual(0, result.Count);
+
+            var entry = result.Pop();
+
+            Assert.AreEqual("GAS", entry.GetString());
+        }
+    }
+}
