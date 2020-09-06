a565c014a5f78f5bc824e9d34cdba3b668084d0f
Shargon <shargon@gmail.com>
2020-09-06 16:37:58 +0200

    Fix UT

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_NativeContracts.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_NativeContracts.cs
@@ -1,15 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Compiler.MSIL.UnitTests.Utils;
+using Neo.SmartContract;
+using Neo.VM;
 
 namespace Neo.Compiler.MSIL.UnitTests
 {
     [TestClass]
     public class Contract_NativeContracts
     {
+        private TestSnapshot snapshot = new TestSnapshot();
+
+        [TestInitialize]
+        public void Test_Init()
+        {
+            byte[] script;
+            using (ScriptBuilder sb = new ScriptBuilder())
+            {
+                sb.EmitSysCall(ApplicationEngine.Neo_Native_Deploy);
+                script = sb.ToArray();
+            }
+
+            typeof(TestSnapshot).GetProperty("PersistingBlock").SetValue(snapshot, new Network.P2P.Payloads.Block() { Index = 0 });
+            var testengine = new TestEngine(TriggerType.System, null, snapshot);
+            testengine.LoadScript(script);
+            Assert.AreEqual(VMState.HALT, testengine.Execute());
+        }
+
         [TestMethod]
         public void Test_NEO()
         {
-            var testengine = new TestEngine();
+            var testengine = new TestEngine(TriggerType.Application, null, snapshot);
             testengine.AddEntryScript("./TestClasses/Contract_NativeContracts.cs");
 
             var result = testengine.ExecuteTestCaseStandard("nEOName");
@@ -25,7 +45,7 @@ namespace Neo.Compiler.MSIL.UnitTests
         [TestMethod]
         public void Test_GAS()
         {
-            var testengine = new TestEngine();
+            var testengine = new TestEngine(TriggerType.Application, null, snapshot);
             testengine.AddEntryScript("./TestClasses/Contract_NativeContracts.cs");
 
             var result = testengine.ExecuteTestCaseStandard("gASName");
