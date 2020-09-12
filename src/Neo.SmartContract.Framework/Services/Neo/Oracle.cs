2d52b8dc9c8f4d85850e4d2fd7021fa31b22a8ad
Shargon <shargon@gmail.com>
2020-09-12 12:12:08 +0200

    Asynchronous Oracle (#337) * Add syscall * Typo * Update nuget * Test native call (#349) * Test name * Update UnitTest_NativeContracts.cs * Fix UT * Fix ut * Update Neo.Compiler.MSIL.csproj Co-authored-by: Vitor Nazário Coelho <vncoelho@gmail.com> Co-authored-by: Erik Zhang <erik@neo.org>

---
+++ src/Neo.SmartContract.Framework/Services/Neo/Oracle.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS0626
+
+namespace Neo.SmartContract.Framework.Services.Neo
+{
+    [Contract("0x3c05b488bf4cf699d0631bf80190896ebbf38c3b")]
+    public class Oracle
+    {
+        public static extern string Name();
+        public static extern byte[][] GetOracleNodes();
+        public static extern void Request(string url, string filter, string callback, object userData, long gasForResponse);
+    }
+}
