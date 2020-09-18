d83b18f56ff8f8ac204204587bb36d0ffa20fd2c
Harry <harrypierson@ngd.neo.org>
2020-09-18 13:34:11 -0700

    remove ValidateAddress from NEP5 contract template

--- templates/Template.NEP5.CSharp/NEP5.Helpers.cs
+++ templates/Template.NEP5.CSharp/NEP5.Helpers.cs
@@ -6,8 +6,6 @@ namespace Template.NEP5.CSharp
 {
     public partial class NEP5 : SmartContract
     {
-        private static bool ValidateAddress(UInt160 address) => address != UInt160.Zero;
-
         private static bool IsPayable(UInt160 address) => Blockchain.GetContract(address)?.IsPayable ?? true;
     }
 }
