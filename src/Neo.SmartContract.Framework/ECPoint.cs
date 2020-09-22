9b3f1de57fb3e4c4daed4cc04874402884112f9d
Harry <harrypierson@ngd.neo.org>
2020-09-22 10:31:19 -0700

    dotnet format

--- src/Neo.SmartContract.Framework/ECPoint.cs
+++ src/Neo.SmartContract.Framework/ECPoint.cs
@@ -7,7 +7,7 @@ namespace Neo.Cryptography.ECC
         [OpCode(OpCode.DUP)]
         [OpCode(OpCode.SIZE)]
         [OpCode(OpCode.PUSHDATA1, "0121")] // 0x01 == 1 byte data length, 0x21 == 33 bytes expected array size
-        [OpCode(OpCode.NUMEQUAL)]       
+        [OpCode(OpCode.NUMEQUAL)]
         [OpCode(OpCode.ASSERT)]
         public static extern explicit operator ECPoint(byte[] value);
 
--- src/Neo.SmartContract.Framework/UInt160.cs
+++ src/Neo.SmartContract.Framework/UInt160.cs
@@ -9,7 +9,7 @@ namespace Neo
         [OpCode(OpCode.DUP)]
         [OpCode(OpCode.SIZE)]
         [OpCode(OpCode.PUSHDATA1, "0114")] // 0x01 == 1 byte data length, 0x14 == 20 bytes expected array size
-        [OpCode(OpCode.NUMEQUAL)]       
+        [OpCode(OpCode.NUMEQUAL)]
         [OpCode(OpCode.ASSERT)]
         public static extern explicit operator UInt160(byte[] value);
 
