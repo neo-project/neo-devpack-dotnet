09b40083cf1b337ef625b9f8840a3500f8cbb9a1
Tommo-L <luchuan@ngd.neo.org>
2020-09-23 17:19:48 +0800

    optimize

--- templates/Template.Oracle.CSharp/OracleDemo.cs
+++ templates/Template.Oracle.CSharp/OracleDemo.cs
@@ -8,20 +8,22 @@ namespace Template.OracleDemo.CSharp
         public static void DoRequest()
         {
             string url = "http://127.0.0.1:8080/test";
-            string filter = "$.value"; // JSONPath, { "value": "hello world" }
+            string filter = "$.value";  // JSONPath, { "value": "hello world" }
             string callback = "callback";
-            object userdata = new byte[] { 0x00 };
+            object userdata =  "userdata";  // arbitrary type
             long gasForResponse = 10000000; // minimum fee 
 
             Oracle.Request(url, filter, callback, userdata, gasForResponse);
         }
 
-        public static void Callback(string url, object userdata, int code, string result)
+        public static void Callback(string url, string userdata, int code, string result)
         {
             object ret = Json.Deserialize(result); // [ "hello world" ]
             object[] arr = (object[])ret;
-            string content = (string)arr[0];
-            Runtime.Log(content);
+            string value = (string)arr[0];
+
+            Runtime.Log("userdata: " + userdata);
+            Runtime.Log("response value: " + value);
         }
     }
 }
