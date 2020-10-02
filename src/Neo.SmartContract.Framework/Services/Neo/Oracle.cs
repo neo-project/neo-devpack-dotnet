4edc260c58816feb3c0051850e3d07f84a1aa146
Shargon <shargon@gmail.com>
2020-10-02 10:02:27 +0200

    Add minimum oracle fee

--- src/Neo.SmartContract.Framework/Services/Neo/Oracle.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Oracle.cs
@@ -5,6 +5,7 @@ namespace Neo.SmartContract.Framework.Services.Neo
     [Contract("0x3c05b488bf4cf699d0631bf80190896ebbf38c3b")]
     public class Oracle
     {
+        public const uint MinimumResponseFee = 10_000_000;
         public static extern string Name { get; }
         public static extern void Request(string url, string filter, string callback, object userData, long gasForResponse);
     }
