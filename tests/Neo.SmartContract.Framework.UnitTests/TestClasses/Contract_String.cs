5747695dfa6b23bd6060ac7e8613625cec864b8b
Shargon <shargon@gmail.com>
2020-11-13 15:55:04 +0100

    Update Contract_String.cs

--- tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_String.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_String.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
-using Neo.SmartContract.Framework.Services.System;
 
 namespace Neo.Compiler.MSIL.TestClasses
 {
