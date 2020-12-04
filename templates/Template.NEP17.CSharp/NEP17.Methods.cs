da701add9ee003cba13a6fdb54aa90e03cbc2131
superboyiii <573504781@qq.com>
2020-12-04 16:29:17 +0800

    Format

--- templates/Template.NEP17.CSharp/NEP17.Methods.cs
+++ templates/Template.NEP17.CSharp/NEP17.Methods.cs
@@ -40,7 +40,7 @@ namespace Template.NEP17.CSharp
             if (AssetStorage.GetPaymentStatus())
             {
                 Payment(from, amount);
-            }else
+            } else
             {
                 throw new Exception("Payment is disable on this contract!");
             }
