e2cf49b74a6d4885865942f7dd5a71204a21848c
Shargon <shargon@gmail.com>
2020-12-10 10:29:40 +0100

    Update

--- src/Neo.SmartContract.Framework/Services/Neo/ManagementContract.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/ManagementContract.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Neo.SmartContract.Framework.Services.Neo
 {
     [Contract("0xdf18cb2476964c241558ed1e2e8881dcd2d50bde")]
@@ -12,5 +8,9 @@ namespace Neo.SmartContract.Framework.Services.Neo
         public static extern int Id { get; }
         public static extern uint ActiveBlockIndex { get; }
         public static extern Contract GetContract(UInt160 hash);
+        public static extern Contract Deploy(byte[] nefFile, string manifest);
+        public static extern void Update(byte[] nefFile, string manifest);
+        public static extern void Destroy();
+
     }
 }
--- tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_CreateAndUpdate.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_CreateAndUpdate.cs
@@ -7,7 +7,7 @@ namespace Neo.Compiler.MSIL.TestClasses
         public static int OldContract(byte[] nefFile, string manifest)
         {
             Contract.Update(script, manifest);
-            Contract.Call(ManagementContract.Hash, "update", new object[] { nefFile, manifest });
+            ManagementContract.Update(nefFile, manifest);
             return 123;
         }
     }
