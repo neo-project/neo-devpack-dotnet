61581b765c698c917838f110325a6d57d2fbada0
Erik Zhang <erik@neo.org>
2021-01-10 23:30:38 +0800

    Fix Iterator

--- src/Neo.SmartContract.Framework/Services/Neo/Iterator.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Iterator.cs
@@ -1,19 +1,26 @@
-using System.Collections.Generic;
-
 namespace Neo.SmartContract.Framework.Services.Neo
 {
-    public class Iterator<TKey, TValue> : IApiInterface
+    public class Iterator
     {
         [Syscall("System.Iterator.Create")]
-        public static extern Iterator<TKey, TValue> Create(Map<TKey, TValue> entry);
+        public static extern Iterator<T> Create<T>(T[] array);
 
         [Syscall("System.Iterator.Create")]
-        public static extern Iterator<TKey, TValue> Create(IEnumerable<TValue> entry);
+        public static extern Iterator<(TKey, TValue)> Create<TKey, TValue>(Map<TKey, TValue> map);
+
+        [Syscall("System.Iterator.Create")]
+        public static extern Iterator<byte> Create(byte[] buffer);
+
+        [Syscall("System.Iterator.Create")]
+        public static extern Iterator<byte> Create(ByteString buffer);
 
         [Syscall("System.Iterator.Next")]
         public extern bool Next();
+    }
 
-        public extern TValue Value
+    public class Iterator<T> : IApiInterface
+    {
+        public extern T Value
         {
             [Syscall("System.Iterator.Value")]
             get;
