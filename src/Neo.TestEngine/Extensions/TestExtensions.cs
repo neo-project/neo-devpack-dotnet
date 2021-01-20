a79b25eb92e581c4e5e3de59cc953a93f662583e
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2021-01-20 13:59:21 -0300

    use contract id instead of hard coded id

--- src/Neo.TestEngine/Extensions/TestExtensions.cs
+++ src/Neo.TestEngine/Extensions/TestExtensions.cs
@@ -3,6 +3,7 @@ using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
 using Neo.Persistence;
 using Neo.SmartContract;
+using Neo.SmartContract.Native;
 
 namespace Neo.TestingEngine
 {
@@ -10,31 +11,34 @@ namespace Neo.TestingEngine
     {
         public static void ContractAdd(this StoreView snapshot, ContractState contract)
         {
-            var key = new KeyBuilder(-1, 8).Add(contract.Hash);
+            var key = new KeyBuilder(NativeContract.ContractManagement.Id, 8).Add(contract.Hash);
             snapshot.Storages.Add(key, new StorageItem(contract));
         }
         public static bool ContainsContract(this StoreView snapshot, UInt160 hash)
         {
-            var key = new KeyBuilder(0, 8).Add(hash);
-            return snapshot.Storages.Contains(key);
+            return NativeContract.ContractManagement.GetContract(snapshot, hash) != null;
         }
 
         public static void DeleteContract(this StoreView snapshot, UInt160 hash)
         {
-            var key = new KeyBuilder(0, 8).Add(hash);
-            snapshot.Storages.Delete(key);
+            var contract = NativeContract.ContractManagement.GetContract(snapshot, hash);
+            if (contract != null)
+            {
+                var key = new KeyBuilder(contract.Id, 8).Add(hash);
+                snapshot.Storages.Delete(key);
+            }
         }
 
         public static void DeployNativeContracts(this StoreView snapshot, Block persistingBlock = null)
         {
             persistingBlock ??= new Block() { Index = 0 };
-            var method = typeof(SmartContract.Native.ContractManagement).GetMethod("OnPersist", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var method = typeof(ContractManagement).GetMethod("OnPersist", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
             var engine = new TestEngine(TriggerType.OnPersist, null, snapshot, persistingBlock);
-            method.Invoke(SmartContract.Native.NativeContract.ContractManagement, new object[] { engine });
+            method.Invoke(NativeContract.ContractManagement, new object[] { engine });
         }
         public static bool TryContractAdd(this StoreView snapshot, ContractState contract)
         {
-            var key = new KeyBuilder(-1, 8).Add(contract.Hash);
+            var key = new KeyBuilder(NativeContract.ContractManagement.Id, 8).Add(contract.Hash);
             if (snapshot.Storages.Contains(key))
             {
                 return false;
