0b81068617c4ce3db43f4f7fc6cb28e4623def5f
Shargon <shargon@gmail.com>
2021-01-22 14:53:45 +0100

    Remove CheckCommittee methods

--- src/Neo.SmartContract.Framework/Services/Neo/Policy.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Policy.cs
@@ -13,10 +13,6 @@ namespace Neo.SmartContract.Framework.Services.Neo
         public static extern long GetMaxBlockSystemFee();
         public static extern BigInteger GetFeePerByte();
         public static extern string[] IsBlocked(UInt160 account);
-        public static extern bool SetMaxBlockSize(uint value);
-        public static extern bool SetMaxTransactionsPerBlock(uint value);
-        public static extern bool SetMaxBlockSystemFee(long value);
-        public static extern bool SetFeePerByte(long value);
         public static extern bool BlockAccount(UInt160 account);
         public static extern bool UnblockAccount(UInt160 account);
     }
