778f68f60fb813c1f463121d56c341eee5d9ac92
Shargon <shargon@gmail.com>
2021-01-28 12:10:30 +0100

    Update NFT

--- src/Neo.SmartContract.Framework/Services/Neo/NameService.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/NameService.cs
@@ -16,6 +16,7 @@ namespace Neo.SmartContract.Framework.Services.Neo
         public static extern BigInteger BalanceOf(UInt160 owner);
         public static extern UInt160 OwnerOf(string name);
         public static extern Map<string, object> Properties(string name);
+        public static extern Iterator<string> Tokens();
         public static extern Iterator<string> TokensOf(UInt160 owner);
         public static extern bool Transfer(UInt160 from, UInt160 to, string name);
 
