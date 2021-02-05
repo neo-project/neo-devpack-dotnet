0ceaf14a37785c14ee9cffeb5685c438f7587c12
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2021-02-05 18:44:22 -0300

    fix import

--- tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ABI_ContractPermission.cs
+++ tests/Neo.Compiler.MSIL.UnitTests/UnitTest_ABI_ContractPermission.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Neo.Compiler.MSIL.UnitTests.Utils;
 using Neo.IO.Json;
+using Neo.TestingEngine;
 
 namespace Neo.Compiler.MSIL.UnitTests
 {
