2612a4c444baf208b11a957ecc4f6572604a7b65
Shargon <shargon@gmail.com>
2021-02-22 09:12:53 +0100

    Sync with neo changes (#470)
    * Remove isConstant
    
    * Fix UT
    
    * add params

--- tests/Neo.SmartContract.Framework.UnitTests/TestBlockchain.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestBlockchain.cs
@@ -41,14 +41,14 @@ namespace Neo.SmartContract.Framework.UnitTests
         {
             foreach (TransactionState tx in txs)
             {
-                snapshot.Add(NativeContract.Ledger.CreateStorageKey(Prefix_Transaction, tx.Transaction.Hash), new StorageItem(tx, true));
+                snapshot.Add(NativeContract.Ledger.CreateStorageKey(Prefix_Transaction, tx.Transaction.Hash), new StorageItem(tx));
             }
         }
 
         public static void BlocksAdd(this DataCache snapshot, UInt256 hash, TrimmedBlock block)
         {
-            snapshot.Add(NativeContract.Ledger.CreateStorageKey(Prefix_BlockHash, block.Index), new StorageItem(hash.ToArray(), true));
-            snapshot.Add(NativeContract.Ledger.CreateStorageKey(Prefix_Block, hash), new StorageItem(block.ToArray(), true));
+            snapshot.Add(NativeContract.Ledger.CreateStorageKey(Prefix_BlockHash, block.Index), new StorageItem(hash.ToArray()));
+            snapshot.Add(NativeContract.Ledger.CreateStorageKey(Prefix_Block, hash), new StorageItem(block.ToArray()));
         }
 
         public static TrimmedBlock Trim(this Block block)
