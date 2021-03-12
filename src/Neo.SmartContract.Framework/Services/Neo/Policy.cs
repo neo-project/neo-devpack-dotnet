040460fe34f753e1a2d3d5a78c284fb026469422
Erik Zhang <erik@neo.org>
2021-03-12 16:05:56 +0800

    Update Policy.cs

--- src/Neo.SmartContract.Framework/Services/Neo/Policy.cs
+++ src/Neo.SmartContract.Framework/Services/Neo/Policy.cs
@@ -1,16 +1,14 @@
 #pragma warning disable CS0626
 
-using System.Numerics;
-
 namespace Neo.SmartContract.Framework.Services.Neo
 {
     [Contract("0xcc5e4edd9f5f8dba8bb65734541df7a1c081c67b")]
     public class Policy
     {
         public static extern UInt160 Hash { [ContractHash] get; }
-        public static extern BigInteger GetFeePerByte();
-        public static extern BigInteger GetExecFeeFactor();
-        public static extern BigInteger GetStoragePrice();
-        public static extern string[] IsBlocked(UInt160 account);
+        public static extern long GetFeePerByte();
+        public static extern uint GetExecFeeFactor();
+        public static extern uint GetStoragePrice();
+        public static extern bool IsBlocked(UInt160 account);
     }
 }
