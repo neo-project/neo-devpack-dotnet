e7ca1fa8ac6c6974a4323e0ddb201c2bdcef14bb
Qiao Jin <jinqiao0423@gmail.com>
2021-03-29 19:06:09 +0800

    throw exception if size incorrect

--- src/Neo.SmartContract.Framework/ECPoint.cs
+++ src/Neo.SmartContract.Framework/ECPoint.cs
@@ -8,15 +8,15 @@ namespace Neo.Cryptography.ECC
         [OpCode(OpCode.DUP)]
         [OpCode(OpCode.SIZE)]
         [OpCode(OpCode.PUSHINT8, "21")] // 0x21 == 33 bytes expected array size
-        [OpCode(OpCode.NUMEQUAL)]
-        [OpCode(OpCode.ASSERT)]
+        [OpCode(OpCode.JMPEQ, "3")]
+        [OpCode(OpCode.THROW)]
         public static extern explicit operator ECPoint(byte[] value);
 
         [OpCode(OpCode.DUP)]
         [OpCode(OpCode.SIZE)]
         [OpCode(OpCode.PUSHINT8, "21")] // 0x21 == 33 bytes expected array size
-        [OpCode(OpCode.NUMEQUAL)]
-        [OpCode(OpCode.ASSERT)]
+        [OpCode(OpCode.JMPEQ, "3")]
+        [OpCode(OpCode.THROW)]
         public static extern explicit operator ECPoint(ByteString value);
 
         [OpCode(OpCode.CONVERT, StackItemType.Buffer)]
