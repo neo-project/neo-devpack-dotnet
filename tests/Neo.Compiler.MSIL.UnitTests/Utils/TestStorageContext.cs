72b935ee3fddf06ceb457802251f4e2ecc29c815
Erik Zhang <erik@neo.org>
2021-03-30 19:13:34 +0800

    Remove MSIL UTs

--- tests/Neo.Compiler.MSIL.UnitTests/Utils/TestStorageContext.cs
+++
@@ -1,8 +0,0 @@
-namespace Neo.Compiler.MSIL.UnitTests.Utils
-{
-    class TestStorageContext
-    {
-        public bool IsReadOnly { get; set; }
-        public UInt160 ScriptHash { get; set; }
-    }
-}
