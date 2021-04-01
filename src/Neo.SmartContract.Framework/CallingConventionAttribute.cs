c8d1a933016dd86c0e9517f68d1e1634a707cacd
Erik Zhang <erik@neo.org>
2021-04-01 22:11:10 +0800

    Optimize StorageMap

--- src/Neo.SmartContract.Framework/CallingConventionAttribute.cs
+++ src/Neo.SmartContract.Framework/CallingConventionAttribute.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace Neo.SmartContract.Framework
 {
-    [AttributeUsage(AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor)]
     class CallingConventionAttribute : Attribute
     {
         public CallingConventionAttribute(CallingConvention callingConvention)
