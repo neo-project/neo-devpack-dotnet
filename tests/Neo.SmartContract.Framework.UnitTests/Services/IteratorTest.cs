da72e1df277fe16f9198e0c6bb4000db927eee90
Erik Zhang <erik@neo.org>
2021-04-03 02:18:24 +0800

    Iterator support foreach statement (#495)

--- tests/Neo.SmartContract.Framework.UnitTests/Services/IteratorTest.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/Services/IteratorTest.cs
@@ -31,6 +31,19 @@ namespace Neo.SmartContract.Framework.UnitTests.Services
         }
 
         [TestMethod]
+        public void TestNextIntArrayForeach()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("testNextIntArrayForeach", new Array(new StackItem[] { 1, 2, 3 }));
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(1, result.Count);
+
+            var item = result.Pop();
+            Assert.IsInstanceOfType(item, typeof(Integer));
+            Assert.AreEqual(6, item.GetInteger());
+        }
+
+        [TestMethod]
         public void TestNextIntArrayBase()
         {
             _engine.Reset();
