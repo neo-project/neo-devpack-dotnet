ac821669f5dbe3263cfd47b8b74a19262c67a30c
Shargon <shargon@gmail.com>
2021-04-08 13:15:46 +0200

    add ut

---
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_Polymorphism.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Numerics;
+
+namespace Neo.Compiler.CSharp.UnitTests.TestClasses
+{
+    public abstract class A : SmartContract.Framework.SmartContract
+    {
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class B : A
+    {
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Contract_Polymorphism : B
+    {
+
+    }
+}
