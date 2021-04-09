6965fdee980804da6e552abef811f2e3209afa71
Erik Zhang <erik@neo.org>
2021-04-09 14:16:25 +0800

    Support polymorphism (#523)

---
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_Polymorphism.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Numerics;
+
+namespace Neo.Compiler.CSharp.UnitTests.TestClasses
+{
+    public abstract class A : SmartContract.Framework.SmartContract
+    {
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public virtual string test() { return "base"; }
+    }
+
+    public abstract class B : A
+    {
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Contract_Polymorphism : B
+    {
+        public override string test()
+        {
+            return "test";
+        }
+    }
+}
