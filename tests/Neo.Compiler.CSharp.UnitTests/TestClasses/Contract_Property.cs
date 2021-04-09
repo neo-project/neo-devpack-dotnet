49264713ac23ecd580848632506babbe66f6abdd
Shargon <shargon@gmail.com>
2021-04-09 12:08:47 +0200

    test this

--- tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_Property.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_Property.cs
@@ -3,5 +3,15 @@ namespace Neo.Compiler.CSharp.UnitTests.TestClasses
     public class Contract_Property : SmartContract.Framework.SmartContract
     {
         public static string Symbol => "TokenSymbol";
+
+        public string this[int index]
+        {
+            get => index.ToString();
+        }
+
+        public string testIndex(int index)
+        {
+            return this[index];
+        }
     }
 }
--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Property.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Property.cs
@@ -6,14 +6,30 @@ namespace Neo.Compiler.CSharp.UnitTests
     [TestClass]
     public class UnitTest_Property
     {
-        [TestMethod]
-        public void UnitTest_TestABIOffsetWithoutOptimizer()
+        private TestEngine testEngine;
+
+        [TestInitialize]
+        public void Init()
         {
-            var testEngine = new TestEngine();
+            testEngine = new TestEngine();
             testEngine.AddEntryScript("./TestClasses/Contract_Property.cs");
+        }
+
+        [TestMethod]
+        public void TestABIOffsetWithoutOptimizer()
+        {
+            testEngine.Reset();
             var abi = testEngine.Manifest["abi"];
             var property = abi["methods"].GetArray()[0];
             Assert.AreEqual("symbol", property["name"].GetString());
+        }
+
+        [TestMethod]
+        public void IndexTest()
+        {
+            testEngine.Reset();
+            var result = testEngine.ExecuteTestCaseStandard("testIndex", 5);
+            Assert.AreEqual("5", result.Pop().GetString());
         }
     }
 }
