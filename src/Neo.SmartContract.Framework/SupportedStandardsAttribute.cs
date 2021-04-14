0ee6fcc13e4b9acb1522381628eb932ed92b9135
Erik Zhang <erik@neo.org>
2021-04-14 19:18:35 +0800

    Add Nep11Token base class to framework (#531)

--- src/Neo.SmartContract.Framework/SupportedStandardsAttribute.cs
+++ src/Neo.SmartContract.Framework/SupportedStandardsAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Neo.SmartContract.Framework
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class SupportedStandardsAttribute : Attribute
     {
         public SupportedStandardsAttribute(params string[] supportedStandards)
