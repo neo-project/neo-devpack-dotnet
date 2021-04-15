7cce71a191828175f8d75ad3ff2f13189dc20290
Erik Zhang <erik@neo.org>
2021-04-15 11:12:39 +0800

    Sync to Neo 3.0.0-CI01266

--- src/Neo.SmartContract.Framework/Nep11Token.cs
+++ src/Neo.SmartContract.Framework/Nep11Token.cs
@@ -71,7 +71,7 @@ namespace Neo.SmartContract.Framework
             if (owner is null || !owner.IsValid)
                 throw new Exception("The argument \"owner\" is invalid");
             StorageMap accountMap = new(Storage.CurrentContext, Prefix_AccountToken);
-            return accountMap.Find(owner, FindOptions.ValuesOnly);
+            return accountMap.Find(owner, FindOptions.KeysOnly | FindOptions.RemovePrefix);
         }
 
         public static bool Transfer(UInt160 to, ByteString tokenId, object data)
@@ -145,7 +145,7 @@ namespace Neo.SmartContract.Framework
                 balanceMap.Put(owner, balance);
             ByteString key = owner + tokenId;
             if (increment > 0)
-                accountMap[key] = tokenId;
+                accountMap.Put(key, 0);
             else
                 accountMap.Delete(key);
         }
