fb0bf81125840aa429b2733f36547ea5121e961f
Shargon <shargon@gmail.com>
2021-04-20 10:45:37 +0200

    Add ContractTrust (#582) * ContractTrust * Validate * Allow array * rename * orderby * Fix * * Order by length * Update ValidateContractTrust * remove params Co-authored-by: Erik Zhang <erik@neo.org>

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_ABI_Attributes.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_ABI_Attributes.cs
@@ -4,15 +4,17 @@ using Neo.Compiler.CSharp.UnitTests.Utils;
 namespace Neo.Compiler.CSharp.UnitTests
 {
     [TestClass]
-    public class UnitTest_ABI_ContractPermission
+    public class UnitTest_ABI_Attributes
     {
         [TestMethod]
-        public void TestWildcardContractPermission()
+        public void TestAbiAttributes()
         {
             var testEngine = new TestEngine();
-            testEngine.AddEntryScript("./TestClasses/Contract_ABIContractPermission.cs");
+            testEngine.AddEntryScript("./TestClasses/Contract_ABIAttributes.cs");
             var permissions = testEngine.Manifest["permissions"].ToString(false);
             Assert.AreEqual(@"[{""contract"":""0x01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4"",""methods"":[""a"",""b""]},{""contract"":""*"",""methods"":[""c""]}]", permissions);
+            var trust = testEngine.Manifest["trusts"].ToString(false);
+            Assert.AreEqual(@"[""0x0a0b00ff00ff00ff00ff00ff00ff00ff00ff00a4""]", trust);
         }
     }
 }
