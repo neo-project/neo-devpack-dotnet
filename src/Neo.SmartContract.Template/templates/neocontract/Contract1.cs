57296a4dbd5986746f7b2fec5cd746a7e094fae1
Jinghui Liao <jinghui@wayne.edu>
2021-08-21 08:40:49 +0800

    Update Contract1.cs

--- src/Neo.SmartContract.Template/templates/neocontract/Contract1.cs
+++ src/Neo.SmartContract.Template/templates/neocontract/Contract1.cs
@@ -1,13 +1,3 @@
-// Copyright (C) 2015-2021 The Neo Project.
-// 
-// The Neo.SmartContract.Template is free software distributed under the MIT 
-// software license, see the accompanying file LICENSE in the main directory 
-// of the project or http://www.opensource.org/licenses/mit-license.php 
-// for more details.
-// 
-// Redistribution and use in source and binary forms with or without
-// modifications are permitted.
-
 using Neo;
 using Neo.SmartContract;
 using Neo.SmartContract.Framework;
