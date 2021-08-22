032452235079a9f452c8903d717e2714eac7334a
Jinghui Liao <jinghui@wayne.edu>
2021-08-23 00:09:03 +0800

    binary value

--- src/Neo.SmartContract.Framework/Services/TriggerType.cs
+++ src/Neo.SmartContract.Framework/Services/TriggerType.cs
@@ -5,22 +5,22 @@ namespace Neo.SmartContract.Framework.Services
         /// <summary>
         /// Indicate that the contract is triggered by the system to execute the OnPersist method of the native contracts.
         /// </summary>
-        OnPersist = 0x01,
+        OnPersist = 0b_0000_0001,
 
         /// <summary>
         /// Indicate that the contract is triggered by the system to execute the PostPersist method of the native contracts.
         /// </summary>
-        PostPersist = 0x02,
+        PostPersist = 0b_0000_0010,
 
         /// <summary>
         /// Indicates that the contract is triggered by the verification of a <see cref="IVerifiable"/>.
         /// </summary>
-        Verification = 0x20,
+        Verification = 0b_0010_0000,
 
         /// <summary>
         /// Indicates that the contract is triggered by the execution of transactions.
         /// </summary>
-        Application = 0x40,
+        Application = 0b_0100_0000,
 
         /// <summary>
         /// The combination of all system triggers.
