0d68b4758d79c4e60043b45a27f9c0919404c41d
Shargon <shargon@gmail.com>
2021-10-19 14:46:26 +0200

    Change UT

--- tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_DuplicateNames.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_DuplicateNames.cs
@@ -1,17 +1,15 @@
+using System;
+using System.Numerics;
 using System.ComponentModel;
-using Neo.SmartContract.Framework;
 
 namespace Neo.Compiler.CSharp.UnitTests.TestClasses
 {
     public class Contract_DuplicateNames : SmartContract.Framework.SmartContract
     {
-        public delegate void OnSomethingDelegate(ByteString data);
-        public delegate void OnSomethingElseDelegate(ByteString data);
+        [DisplayName("Notify")]
+        public static event Action<BigInteger> Notice;
 
-        [DisplayName("OnSomething")]
-        public static event OnSomethingDelegate Something;
-
-        [DisplayName("OnSomething")]
-        public static event OnSomethingElseDelegate SomethingElse;
+        [DisplayName("Notify")]
+        public static event Action<BigInteger> AA;
     }
 }
