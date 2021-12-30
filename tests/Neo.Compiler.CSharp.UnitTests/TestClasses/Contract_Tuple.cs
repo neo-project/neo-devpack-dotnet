045a65e4d8a7ff4d9f700eb7587605e84bcccbaf
Shuai <bfshm@qq.com>
2021-12-30 16:53:23 +0800

    init

---
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_Tuple.cs
@@ -0,0 +1,39 @@
+using System.Numerics;
+
+namespace Neo.Compiler.CSharp.UnitTests.TestClasses
+{
+    struct StructA
+    {
+        public byte[] from;
+        public byte[] to;
+        public BigInteger amount;
+        public BigInteger index;
+        public StructB data;
+    }
+
+    struct StructB
+    {
+        public byte[] hash;
+        public BigInteger height;
+    }
+
+
+    public class Contract_Tuple : SmartContract.Framework.SmartContract
+    {
+
+        public static (BigInteger, BigInteger, BigInteger, BigInteger) GetResult()
+        {
+            return (1, 2, 3, 4);
+        }
+
+        public static object T1()
+        {
+            var state = new StructA();
+            state.data = new StructB();
+            BigInteger index = 0;
+            (state.amount, state.data.height, _, index) = GetResult();
+            state.index = index;
+            return state;
+        }
+    }
+}
---
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Tuple.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.CSharp.UnitTests.Utils;
+using Neo.VM.Types;
+using System.Linq;
+
+namespace Neo.Compiler.CSharp.UnitTests
+{
+    [TestClass]
+    public class UnitTest_Tuple
+    {
+
+        [TestMethod]
+        public void Test_Assign()
+        {
+            var testengine = new TestEngine();
+            testengine.AddEntryScript("./TestClasses/Contract_Tuple.cs");
+            var result = testengine.ExecuteTestCaseStandard("t1");
+
+            var tuple = result.Pop<Struct>();
+            Assert.AreEqual(5, tuple.Count);
+            Assert.AreEqual(1, tuple[2].GetInteger());
+            Assert.AreEqual(4, tuple[3].GetInteger());
+            Assert.AreEqual(2, ((Struct)tuple[4])[1].GetInteger());
+        }
+
+    }
+}
