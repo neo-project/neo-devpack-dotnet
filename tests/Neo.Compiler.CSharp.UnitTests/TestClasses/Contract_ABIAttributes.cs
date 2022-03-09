c4aca59582fc2ddbc0483e06e5c82349c3b539c8
Shargon <shargon@gmail.com>
2022-03-09 13:05:13 +0100

    c# v10 (#713)
    * c# 10
    
    * Use SymbolEqualityComparer.Default
    
    * Use SymbolEqualityComparer.Default
    
    * Update nuget
    
    * Update nuget
    
    * Clean format
    
    * fix nullable
    
    * fix
    
    * Update System.CommandLine
    
    * fix Main
    
    Co-authored-by: Erik Zhang <erik@neo.org>

--- tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_ABIAttributes.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_ABIAttributes.cs
@@ -1,12 +1,11 @@
 using Neo.SmartContract.Framework.Attributes;
 
-namespace Neo.Compiler.CSharp.UnitTests.TestClasses
+namespace Neo.Compiler.CSharp.UnitTests.TestClasses;
+
+[ContractPermission("*", "c")]
+[ContractPermission("0x01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4", "a", "b")]
+[ContractTrust("0x0a0b00ff00ff00ff00ff00ff00ff00ff00ff00a4")]
+public class Contract_ABIAttributes : SmartContract.Framework.SmartContract
 {
-    [ContractPermission("*", "c")]
-    [ContractPermission("0x01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4", "a", "b")]
-    [ContractTrust("0x0a0b00ff00ff00ff00ff00ff00ff00ff00ff00a4")]
-    public class Contract_ABIAttributes : SmartContract.Framework.SmartContract
-    {
-        public static int test() => 0;
-    }
+    public static int test() => 0;
 }
