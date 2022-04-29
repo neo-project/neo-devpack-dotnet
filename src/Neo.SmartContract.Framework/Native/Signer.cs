6e5435be4c7ea0b80c38409553a6d03a20ef4742
Shargon <shargon@gmail.com>
2022-04-30 01:16:15 +0200

    Sync GetTransactionSigners (#731) * Update * Fix ut * Update Neo.Compiler.CSharp.csproj * Signers * Fix UT * Add classes * One file per class * Fix ut

---
+++ src/Neo.SmartContract.Framework/Native/Signer.cs
@@ -0,0 +1,24 @@
+// Copyright (C) 2015-2021 The Neo Project.
+// 
+// The Neo.SmartContract.Framework is free software distributed under the MIT 
+// software license, see the accompanying file LICENSE in the main directory 
+// of the project or http://www.opensource.org/licenses/mit-license.php 
+// for more details.
+// 
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+using Neo.Cryptography.ECC;
+
+namespace Neo.SmartContract.Framework.Native
+{
+    public class Signer
+    {
+        public byte[] Data;
+        public UInt160 Account;
+        public WitnessScope Scopes;
+        public UInt160[] AllowedContracts;
+        public ECPoint[] AllowedGroups;
+        public WitnessRule[] Rules;
+    }
+}
