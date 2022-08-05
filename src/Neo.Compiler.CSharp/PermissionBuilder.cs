8a49b33c4f1422023eecf09b2684fb78408b09f5
Shine Li <bfshm@qq.com>
2022-08-05 14:15:13 +0800

    3.4.0 (#761) * update * fix * 3.4.0 * fix Co-authored-by: Erik Zhang <erik@neo.org>

--- src/Neo.Compiler.CSharp/PermissionBuilder.cs
+++ src/Neo.Compiler.CSharp/PermissionBuilder.cs
@@ -8,7 +8,7 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
-using Neo.IO.Json;
+using Neo.Json;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -77,7 +77,7 @@ namespace Neo.Compiler
                     permissions.Add(new JObject
                     {
                         ["contract"] = group.Key,
-                        ["methods"] = new JArray(group.OrderBy(p => p).Select(p => (JString)p))
+                        ["methods"] = new JArray(group.OrderBy(p => p).Select(p => (JString)p!))
                     });
                 foreach (string hash in wildcardHashes.OrderBy(p => p))
                     permissions.Add(new JObject
@@ -89,7 +89,7 @@ namespace Neo.Compiler
                     permissions.Add(new JObject
                     {
                         ["contract"] = "*",
-                        ["methods"] = new JArray(wildcardMethods.OrderBy(p => p).Select(p => (JString)p))
+                        ["methods"] = new JArray(wildcardMethods.OrderBy(p => p).Select(p => (JString)p!))
                     });
             }
             return permissions;
--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Types.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Types.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Compiler.CSharp.UnitTests.Utils;
 using Neo.IO;
-using Neo.IO.Json;
+using Neo.Json;
 using Neo.VM;
 using Neo.VM.Types;
 using Neo.Wallets;
