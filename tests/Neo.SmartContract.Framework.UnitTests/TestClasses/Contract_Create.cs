c63c89556aac702a260875d6b5dc2f413b035153
Shuai <bfshm@qq.com>
2023-05-15 15:59:34 +0800

    init

--- tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_Create.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestClasses/Contract_Create.cs
@@ -3,11 +3,24 @@ using Neo.SmartContract.Framework.Services;
 
 namespace Neo.SmartContract.Framework.UnitTests.TestClasses
 {
+
     public class Contract_Create : SmartContract
     {
         public static string OldContract()
         {
             return ContractManagement.GetContract(Runtime.ExecutingScriptHash).Manifest.Name;
+        }
+
+        public static Contract GetContractById(int id)
+        {
+            return ContractManagement.GetContractById(id);
+        }
+
+        public static object GetContractHashes()
+        {
+            var iter = ContractManagement.GetContractHashes();
+            iter.Next();
+            return iter.Value.Item2;
         }
     }
 }
