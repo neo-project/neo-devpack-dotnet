33932ddc11ba6c733c21a5733dc463f4553b3d47
Shargon <shargon@gmail.com>
2023-11-03 11:47:15 +0100

    Ensure that the nef file is serializable (#809)
    * Ensure that the nef file is serializable
    
    * Update neo nuget
    
    * change return
    
    * Ensure that the ContractManifest also is checked
    
    * format
    
    * Add comment
    
    * Control manifest error
    
    * Revert code reading optimizations

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_ABI_Attributes.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_ABI_Attributes.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Compiler.CSharp.UnitTests.Utils;
+using Neo.Json;
 
 namespace Neo.Compiler.CSharp.UnitTests
 {
@@ -11,10 +13,10 @@ namespace Neo.Compiler.CSharp.UnitTests
         {
             var testEngine = new TestEngine();
             testEngine.AddEntryScript("./TestClasses/Contract_ABIAttributes.cs");
-            var permissions = testEngine.Manifest["permissions"].ToString(false);
+            var permissions = new JArray(testEngine.Manifest.Permissions.Select(p => p.ToJson()).ToArray()).ToString(false);
             Assert.AreEqual(@"[{""contract"":""0x01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4"",""methods"":[""a"",""b""]},{""contract"":""*"",""methods"":[""c""]}]", permissions);
-            var trust = testEngine.Manifest["trusts"].ToString(false);
-            Assert.AreEqual(@"[""0x0a0b00ff00ff00ff00ff00ff00ff00ff00ff00a4""]", trust);
+            var trust = testEngine.Manifest.Trusts.ToJson(p => p.ToJson());
+            Assert.AreEqual(@"[""0x0a0b00ff00ff00ff00ff00ff00ff00ff00ff00a4""]", trust.ToString(false));
         }
     }
 }
