5302960790302310647f539c9eb15190c5c004a9
Jimmy <jimmy@r3e.network>
2023-11-06 18:37:38 +0800

    fix char to bigiteger (#811)

--- src/Neo.Compiler.CSharp/MethodConvert.cs
+++ src/Neo.Compiler.CSharp/MethodConvert.cs
@@ -4085,6 +4085,7 @@ namespace Neo.Compiler
                         endTarget.Instruction = AddInstruction(OpCode.NOP);
                     }
                     return true;
+                case "System.Numerics.BigInteger.implicit operator System.Numerics.BigInteger(char)":
                 case "System.Numerics.BigInteger.implicit operator System.Numerics.BigInteger(sbyte)":
                 case "System.Numerics.BigInteger.implicit operator System.Numerics.BigInteger(byte)":
                 case "System.Numerics.BigInteger.implicit operator System.Numerics.BigInteger(short)":
