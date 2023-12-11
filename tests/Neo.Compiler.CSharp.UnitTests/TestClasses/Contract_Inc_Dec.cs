1fed7a707c7bb0d22af0cc975201e6b4cc2f1da4
Jinghui Liao <jimmy@r3e.network>
2023-12-11 16:36:48 +0800

    fix all and add unit test cases

---
+++ tests/Neo.Compiler.CSharp.UnitTests/TestClasses/Contract_Inc_Dec.cs
@@ -0,0 +1,147 @@
+using System;
+using System.ComponentModel;
+
+namespace Neo.Compiler.CSharp.UnitTests.TestClasses
+{
+    public class Contract2_Inc_Dec : SmartContract.Framework.SmartContract
+    {
+        private static uint _property;
+
+        public static uint UnitTest_Property_Inc_Checked()
+        {
+            _property = uint.MaxValue;
+            checked
+            {
+                ++_property;
+                _property++;
+            }
+            return _property;
+        }
+
+        public static uint UnitTest_Property_Inc_UnChecked()
+        {
+            _property = uint.MaxValue;
+            unchecked
+            {
+                ++_property;
+                _property++;
+            }
+            return _property;
+        }
+
+        public static uint UnitTest_Property_Dec_Checked()
+        {
+            _property = uint.MinValue;
+            checked
+            {
+                --_property;
+                _property--;
+            }
+            return _property;
+        }
+        public static uint UnitTest_Property_Dec_UnChecked()
+        {
+            _property = uint.MinValue;
+            unchecked
+            {
+                --_property;
+                _property--;
+            }
+            return _property;
+        }
+
+        public static uint UnitTest_Local_Dec_Checked()
+        {
+            uint local = uint.MinValue;
+            checked
+            {
+                --local;
+                local--;
+            }
+            return local;
+        }
+
+        public static uint UnitTest_Local_Dec_UnChecked()
+        {
+            uint local = uint.MinValue;
+            unchecked
+            {
+                --local;
+                local--;
+            }
+            return local;
+        }
+
+        public static uint UnitTest_Local_Inc_Checked()
+        {
+            uint local = uint.MaxValue;
+            checked
+            {
+                ++local;
+                local++;
+            }
+            return local;
+        }
+
+        public static uint UnitTest_Local_Inc_UnChecked()
+        {
+            uint local = uint.MaxValue;
+            unchecked
+            {
+                ++local;
+                local++;
+            }
+            return local;
+        }
+
+        public static uint UnitTest_Param_Dec_Checked(uint param)
+        {
+            param = uint.MinValue;
+            checked
+            {
+                --param;
+                param--;
+            }
+            return param;
+        }
+
+        public static uint UnitTest_Param_Dec_UnChecked(uint param)
+        {
+            param = uint.MinValue;
+            unchecked
+            {
+                --param;
+                param--;
+            }
+            return param;
+        }
+
+        public static uint UnitTest_Param_Inc_Checked(uint param)
+        {
+            param = uint.MaxValue;
+            checked
+            {
+                ++param;
+                param++;
+            }
+            return param;
+        }
+
+        public static uint UnitTest_Param_Inc_UnChecked(uint param)
+        {
+            param = uint.MaxValue;
+            unchecked
+            {
+                ++param;
+                param++;
+            }
+            return param;
+        }
+
+        public static void UnitTest_Not_DeadLoop()
+        {
+            for (uint i = 5; i < 7; i--) ;
+        }
+
+    }
+}
---
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Inc_Dec.cs
@@ -0,0 +1,131 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Compiler.CSharp.UnitTests.Utils;
+using Neo.VM;
+
+namespace Neo.Compiler.CSharp.UnitTests
+{
+    [TestClass]
+    public class UnitTest_Inc_Dec
+    {
+        private TestEngine _engine;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _engine = new TestEngine();
+            _engine.AddEntryScript("./TestClasses/Contract_Inc_Dec.cs");
+        }
+
+        [TestMethod]
+        public void Test_Property_Inc_Checked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Property_Inc_Checked");
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+        }
+
+        [TestMethod]
+        public void Test_Property_Inc_UnChecked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Property_Inc_UnChecked");
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(1, result.Pop().GetInteger());
+        }
+
+        [TestMethod]
+        public void Test_Property_Dec_Checked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Property_Dec_Checked");
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+        }
+
+        [TestMethod]
+        public void Test_Property_Dec_UnChecked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Property_Dec_UnChecked");
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(uint.MaxValue - 1, result.Pop().GetInteger());
+        }
+
+        [TestMethod]
+        public void Test_Local_Inc_Checked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Local_Inc_Checked");
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+        }
+
+        [TestMethod]
+        public void Test_Local_Inc_UnChecked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Local_Inc_UnChecked");
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(1, result.Pop().GetInteger());
+        }
+
+        [TestMethod]
+        public void Test_Local_Dec_Checked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Local_Dec_Checked");
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+        }
+
+        [TestMethod]
+        public void Test_Local_Dec_UnChecked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Local_Dec_UnChecked");
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(uint.MaxValue - 1, result.Pop().GetInteger());
+        }
+
+
+        [TestMethod]
+        public void Test_Param_Inc_Checked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Param_Inc_Checked", 0);
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+        }
+
+        [TestMethod]
+        public void Test_Param_Inc_UnChecked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Param_Inc_UnChecked", 0);
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(1, result.Pop().GetInteger());
+        }
+
+        [TestMethod]
+        public void Test_Param_Dec_Checked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Param_Dec_Checked", 0);
+            Assert.AreEqual(VMState.FAULT, _engine.State);
+        }
+
+        [TestMethod]
+        public void Test_Param_Dec_UnChecked()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Param_Dec_UnChecked", 0);
+            Assert.AreEqual(VMState.HALT, _engine.State);
+            Assert.AreEqual(uint.MaxValue - 1, result.Pop().GetInteger());
+        }
+
+
+        [TestMethod]
+        public void Test_Not_DeadLoop()
+        {
+            _engine.Reset();
+            var result = _engine.ExecuteTestCaseStandard("unitTest_Not_DeadLoop");
+            Assert.AreEqual(VMState.HALT, _engine.State);
+        }
+    }
+}
