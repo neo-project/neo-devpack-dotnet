dfb1d3d1f1f43e0245930ca6a79cd7516a71575e
Jimmy <jimmy@r3e.network>
2024-02-15 21:33:27 +0800

    [Framework Add] predefine manifest (#903)
    * predefine manifest
    
    * one attribute per file
    
    * remove empty line
    
    * Update tests/Neo.SmartContract.Framework.UnitTests/ManifestAttributeTest.cs
    
    * Update src/Neo.SmartContract.Framework/Attributes/ManifestExtraAttribute.cs
    
    * Update src/Neo.SmartContract.Framework/Attributes/ManifestExtraAttribute.cs
    
    * Update src/Neo.Compiler.CSharp/CompilationContext.cs
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>

--- src/Neo.SmartContract.Framework/Attributes/ManifestExtraAttribute.cs
+++ src/Neo.SmartContract.Framework/Attributes/ManifestExtraAttribute.cs
@@ -1,14 +1,15 @@
 // Copyright (C) 2015-2023 The Neo Project.
-// 
-// The Neo.SmartContract.Framework is free software distributed under the MIT 
-// software license, see the accompanying file LICENSE in the main directory 
-// of the project or http://www.opensource.org/licenses/mit-license.php 
+//
+// The Neo.SmartContract.Framework is free software distributed under the MIT
+// software license, see the accompanying file LICENSE in the main directory
+// of the project or http://www.opensource.org/licenses/mit-license.php
 // for more details.
-// 
+//
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
 using System;
+using System.Collections.Generic;
 
 namespace Neo.SmartContract.Framework.Attributes
 {
@@ -18,5 +19,13 @@ namespace Neo.SmartContract.Framework.Attributes
         public ManifestExtraAttribute(string key, string value)
         {
         }
+
+        internal static readonly Dictionary<string, string> AttributeType = new Dictionary<string, string>
+        {
+            { nameof(AuthorAttribute), "Author" },
+            { nameof(EmailAttribute), "E-mail" },
+            { nameof(DescriptionAttribute), "Description" },
+            { nameof(VersionAttribute), "Version" },
+        };
     }
 }
