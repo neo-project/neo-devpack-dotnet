747dfe8c4a73f035f8e22592f4ba68e4145e88e4
Fernando Diaz Toledano <shargon@gmail.com>
2024-02-15 11:10:52 +0100

    remove using

--- src/Neo.SmartContract.Testing/Coverage/CoveredContract.cs
+++ src/Neo.SmartContract.Testing/Coverage/CoveredContract.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Text;
 
 namespace Neo.SmartContract.Testing.Coverage
