50140a22007a703e907b0f42b691a0764dd604fa
Jimmy <jimmy@r3e.network>
2024-02-16 03:29:33 -0800

    update neo (#906) * update neo * Remove init, and compile, now allow to change storage during executions --------- Co-authored-by: Fernando Diaz Toledano <shargon@gmail.com>

--- src/Neo.SmartContract.Testing/TestStorage.cs
+++ src/Neo.SmartContract.Testing/TestStorage.cs
@@ -17,7 +17,7 @@ namespace Neo.SmartContract.Testing
         /// <summary>
         /// Store
         /// </summary>
-        public IStore Store { get; init; } = new MemoryStore();
+        public IStore Store { get; }
 
         /// <summary>
         /// Snapshot
