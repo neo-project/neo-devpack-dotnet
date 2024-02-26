d61e4ffad9e2791ac368a43252d81661d387f9f0
Shargon <shargon@gmail.com>
2024-02-26 01:47:14 -0800

    Update tests/Neo.SmartContract.Framework.TestContracts/Contract_SupportedStandard11Enum.cs

--- tests/Neo.SmartContract.Framework.TestContracts/Contract_SupportedStandard11Enum.cs
+++ tests/Neo.SmartContract.Framework.TestContracts/Contract_SupportedStandard11Enum.cs
@@ -4,7 +4,6 @@ using Neo.SmartContract.Framework.Interfaces;
 
 namespace Neo.SmartContract.Framework.UnitTests.TestClasses
 {
-
     [SupportedStandards(NepStandard.Nep11)]
     public class Contract_SupportedStandard11Enum : Nep11Token<Nep11TokenState>, INep11Payment
     {
