d105ea309235f4ec2a37ac2501d68b607bb0bc47
Fernando Diaz Toledano <shargon@gmail.com>
2024-02-27 12:43:18 +0100

    Expand env

--- tests/Neo.SmartContract.Template.UnitTests/templates/TestCleanup.cs
+++ tests/Neo.SmartContract.Template.UnitTests/templates/TestCleanup.cs
@@ -124,7 +124,7 @@ namespace Neo.SmartContract.Template.UnitTests.templates
                        (coverageOwnable, DebugInfo_Ownable),
                        (coverageOracle, DebugInfo_Oracle)
                        ).
-                       Write(Environment.ExpandEnvironmentVariables(mergeWith), true);
+                       Write(Environment.ExpandEnvironmentVariables(Environment.ExpandEnvironmentVariables(mergeWith)), true);
                 }
             }
 
