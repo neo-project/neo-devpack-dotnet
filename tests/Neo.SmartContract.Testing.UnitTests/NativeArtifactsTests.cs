d1d0f88398c3d86b490cebdbbe6e5101031b4421
Fernando Diaz Toledano <shargon@gmail.com>
2024-02-28 08:23:35 +0100

    clean using

--- tests/Neo.SmartContract.Testing.UnitTests/NativeArtifactsTests.cs
+++ tests/Neo.SmartContract.Testing.UnitTests/NativeArtifactsTests.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System.Linq;
-using System.Numerics;
 using System.Reflection;
 
 namespace Neo.SmartContract.Testing.UnitTests
