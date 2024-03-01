86b77482f6895183cbb9919e6868d6c3b5645491
Shargon <shargon@gmail.com>
2024-02-29 21:48:11 -0800

    Update native contracts (#967) * Update native contracts * clean using * Add ut * Split ut * clean test

--- src/Neo.SmartContract.Testing/Native/ContractManagement.cs
+++ src/Neo.SmartContract.Testing/Native/ContractManagement.cs
@@ -6,20 +6,29 @@ namespace Neo.SmartContract.Testing.Native;
 
 public abstract class ContractManagement : SmartContract
 {
+    #region Compiled data
+
+    public static readonly Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""ContractManagement"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""deploy"",""parameters"":[{""name"":""nefFile"",""type"":""ByteArray""},{""name"":""manifest"",""type"":""ByteArray""}],""returntype"":""Array"",""offset"":0,""safe"":false},{""name"":""deploy"",""parameters"":[{""name"":""nefFile"",""type"":""ByteArray""},{""name"":""manifest"",""type"":""ByteArray""},{""name"":""data"",""type"":""Any""}],""returntype"":""Array"",""offset"":7,""safe"":false},{""name"":""destroy"",""parameters"":[],""returntype"":""Void"",""offset"":14,""safe"":false},{""name"":""getContract"",""parameters"":[{""name"":""hash"",""type"":""Hash160""}],""returntype"":""Array"",""offset"":21,""safe"":true},{""name"":""getContractById"",""parameters"":[{""name"":""id"",""type"":""Integer""}],""returntype"":""Array"",""offset"":28,""safe"":true},{""name"":""getContractHashes"",""parameters"":[],""returntype"":""InteropInterface"",""offset"":35,""safe"":true},{""name"":""getMinimumDeploymentFee"",""parameters"":[],""returntype"":""Integer"",""offset"":42,""safe"":true},{""name"":""hasMethod"",""parameters"":[{""name"":""hash"",""type"":""Hash160""},{""name"":""method"",""type"":""String""},{""name"":""pcount"",""type"":""Integer""}],""returntype"":""Boolean"",""offset"":49,""safe"":true},{""name"":""setMinimumDeploymentFee"",""parameters"":[{""name"":""value"",""type"":""Integer""}],""returntype"":""Void"",""offset"":56,""safe"":false},{""name"":""update"",""parameters"":[{""name"":""nefFile"",""type"":""ByteArray""},{""name"":""manifest"",""type"":""ByteArray""}],""returntype"":""Void"",""offset"":63,""safe"":false},{""name"":""update"",""parameters"":[{""name"":""nefFile"",""type"":""ByteArray""},{""name"":""manifest"",""type"":""ByteArray""},{""name"":""data"",""type"":""Any""}],""returntype"":""Void"",""offset"":70,""safe"":false}],""events"":[{""name"":""Deploy"",""parameters"":[{""name"":""Hash"",""type"":""Hash160""}]},{""name"":""Update"",""parameters"":[{""name"":""Hash"",""type"":""Hash160""}]},{""name"":""Destroy"",""parameters"":[{""name"":""Hash"",""type"":""Hash160""}]}]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":null}");
+
+    #endregion
+
     #region Events
 
-    public delegate void delDeploy(UInt160 Hash);
+    public delegate void delDeploy(UInt160? Hash);
 
     [DisplayName("Deploy")]
     public event delDeploy? OnDeploy;
-    public delegate void delDestroy(UInt160 Hash);
+
+    public delegate void delDestroy(UInt160? Hash);
 
     [DisplayName("Destroy")]
     public event delDestroy? OnDestroy;
-    public delegate void delUpdate(UInt160 Hash);
+
+    public delegate void delUpdate(UInt160? Hash);
 
     [DisplayName("Update")]
     public event delUpdate? OnUpdate;
+
     #endregion
 
     #region Properties
@@ -27,12 +36,12 @@ public abstract class ContractManagement : SmartContract
     /// <summary>
     /// Safe property
     /// </summary>
-    public abstract IIterator ContractHashes { [DisplayName("getContractHashes")] get; }
+    public abstract IIterator? ContractHashes { [DisplayName("getContractHashes")] get; }
 
     /// <summary>
     /// Safe property
     /// </summary>
-    public abstract BigInteger MinimumDeploymentFee { [DisplayName("getMinimumDeploymentFee")] get; [DisplayName("setMinimumDeploymentFee")] set; }
+    public abstract BigInteger? MinimumDeploymentFee { [DisplayName("getMinimumDeploymentFee")] get; [DisplayName("setMinimumDeploymentFee")] set; }
 
     #endregion
 
@@ -42,19 +51,19 @@ public abstract class ContractManagement : SmartContract
     /// Safe method
     /// </summary>
     [DisplayName("getContract")]
-    public abstract ContractState? GetContract(UInt160 hash);
+    public abstract ContractState? GetContract(UInt160? hash);
 
     /// <summary>
     /// Safe method
     /// </summary>
     [DisplayName("getContractById")]
-    public abstract ContractState? GetContractById(BigInteger id);
+    public abstract ContractState? GetContractById(BigInteger? id);
 
     /// <summary>
     /// Safe method
     /// </summary>
     [DisplayName("hasMethod")]
-    public abstract bool HasMethod(UInt160 hash, string method, BigInteger pcount);
+    public abstract bool? HasMethod(UInt160? hash, string? method, BigInteger? pcount);
 
     #endregion
 
@@ -64,13 +73,13 @@ public abstract class ContractManagement : SmartContract
     /// Unsafe method
     /// </summary>
     [DisplayName("deploy")]
-    public abstract ContractState Deploy(byte[] nefFile, byte[] manifest);
+    public abstract ContractState? Deploy(byte[]? nefFile, byte[]? manifest);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("deploy")]
-    public abstract ContractState Deploy(byte[] nefFile, byte[] manifest, object? data = null);
+    public abstract ContractState? Deploy(byte[]? nefFile, byte[]? manifest, object? data = null);
 
     /// <summary>
     /// Unsafe method
@@ -82,13 +91,13 @@ public abstract class ContractManagement : SmartContract
     /// Unsafe method
     /// </summary>
     [DisplayName("update")]
-    public abstract void Update(byte[] nefFile, byte[] manifest);
+    public abstract void Update(byte[]? nefFile, byte[]? manifest);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("update")]
-    public abstract void Update(byte[] nefFile, byte[] manifest, object? data = null);
+    public abstract void Update(byte[]? nefFile, byte[]? manifest, object? data = null);
 
     #endregion
 
--- src/Neo.SmartContract.Testing/Native/CryptoLib.cs
+++ src/Neo.SmartContract.Testing/Native/CryptoLib.cs
@@ -5,6 +5,12 @@ namespace Neo.SmartContract.Testing.Native;
 
 public abstract class CryptoLib : SmartContract
 {
+    #region Compiled data
+
+    public static readonly Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""CryptoLib"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""bls12381Add"",""parameters"":[{""name"":""x"",""type"":""InteropInterface""},{""name"":""y"",""type"":""InteropInterface""}],""returntype"":""InteropInterface"",""offset"":0,""safe"":true},{""name"":""bls12381Deserialize"",""parameters"":[{""name"":""data"",""type"":""ByteArray""}],""returntype"":""InteropInterface"",""offset"":7,""safe"":true},{""name"":""bls12381Equal"",""parameters"":[{""name"":""x"",""type"":""InteropInterface""},{""name"":""y"",""type"":""InteropInterface""}],""returntype"":""Boolean"",""offset"":14,""safe"":true},{""name"":""bls12381Mul"",""parameters"":[{""name"":""x"",""type"":""InteropInterface""},{""name"":""mul"",""type"":""ByteArray""},{""name"":""neg"",""type"":""Boolean""}],""returntype"":""InteropInterface"",""offset"":21,""safe"":true},{""name"":""bls12381Pairing"",""parameters"":[{""name"":""g1"",""type"":""InteropInterface""},{""name"":""g2"",""type"":""InteropInterface""}],""returntype"":""InteropInterface"",""offset"":28,""safe"":true},{""name"":""bls12381Serialize"",""parameters"":[{""name"":""g"",""type"":""InteropInterface""}],""returntype"":""ByteArray"",""offset"":35,""safe"":true},{""name"":""keccak256"",""parameters"":[{""name"":""data"",""type"":""ByteArray""}],""returntype"":""ByteArray"",""offset"":42,""safe"":true},{""name"":""murmur32"",""parameters"":[{""name"":""data"",""type"":""ByteArray""},{""name"":""seed"",""type"":""Integer""}],""returntype"":""ByteArray"",""offset"":49,""safe"":true},{""name"":""ripemd160"",""parameters"":[{""name"":""data"",""type"":""ByteArray""}],""returntype"":""ByteArray"",""offset"":56,""safe"":true},{""name"":""sha256"",""parameters"":[{""name"":""data"",""type"":""ByteArray""}],""returntype"":""ByteArray"",""offset"":63,""safe"":true},{""name"":""verifyWithECDsa"",""parameters"":[{""name"":""message"",""type"":""ByteArray""},{""name"":""pubkey"",""type"":""ByteArray""},{""name"":""signature"",""type"":""ByteArray""},{""name"":""curve"",""type"":""Integer""}],""returntype"":""Boolean"",""offset"":70,""safe"":true}],""events"":[]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":null}");
+
+    #endregion
+
     #region Safe methods
 
     /// <summary>
@@ -46,26 +52,32 @@ public abstract class CryptoLib : SmartContract
     /// <summary>
     /// Safe method
     /// </summary>
+    [DisplayName("keccak256")]
+    public abstract byte[]? Keccak256(byte[]? data);
+
+    /// <summary>
+    /// Safe method
+    /// </summary>
     [DisplayName("murmur32")]
-    public abstract byte[] Murmur32(byte[]? data, BigInteger? seed);
+    public abstract byte[]? Murmur32(byte[]? data, BigInteger? seed);
 
     /// <summary>
     /// Safe method
     /// </summary>
     [DisplayName("ripemd160")]
-    public abstract byte[] Ripemd160(byte[]? data);
+    public abstract byte[]? Ripemd160(byte[]? data);
 
     /// <summary>
     /// Safe method
     /// </summary>
     [DisplayName("sha256")]
-    public abstract byte[] Sha256(byte[]? data);
+    public abstract byte[]? Sha256(byte[]? data);
 
     /// <summary>
     /// Safe method
     /// </summary>
     [DisplayName("verifyWithECDsa")]
-    public abstract bool VerifyWithECDsa(byte[]? message, byte[]? pubkey, byte[]? signature, BigInteger? curve);
+    public abstract bool? VerifyWithECDsa(byte[]? message, byte[]? pubkey, byte[]? signature, BigInteger? curve);
 
     #endregion
 
