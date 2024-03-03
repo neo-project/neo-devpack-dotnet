9d4c3e82794035298af80bfa1e153e09d865c4b7
Anna Shaleva <shaleva.ann@nspcc.ru>
2024-03-03 10:56:23 +0300

    Add documentation to native RoleManagement roles
    It's nice when user-facing things are well-documented.
    
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>

--- src/Neo.SmartContract.Framework/Native/Role.cs
+++ src/Neo.SmartContract.Framework/Native/Role.cs
@@ -10,10 +10,24 @@
 
 namespace Neo.SmartContract.Framework.Native
 {
+    /// <summary>
+    /// Represents the roles in the NEO system.
+    /// </summary>
     public enum Role : byte
     {
+        /// <summary>
+        /// The validators of state. Used to generate and sign the state root.
+        /// </summary>
         StateValidator = 4,
+
+        /// <summary>
+        /// The nodes used to process Oracle requests.
+        /// </summary>
         Oracle = 8,
+
+        /// <summary>
+        /// NeoFS Alphabet nodes.
+        /// </summary>
         NeoFSAlphabetNode = 16
     }
 }
