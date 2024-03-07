609207bd8a01b79d695844cb924fe564e639f6eb
shuai <bfshm@qq.com>
2024-03-07 17:01:05 +0800

    init

---
+++ tests/Neo.Compiler.CSharp.TestContracts/Contract_Pattern.cs
@@ -0,0 +1,33 @@
+using System;
+using Neo.SmartContract.Framework;
+
+namespace Neo.Compiler.CSharp.UnitTests.TestClasses
+{
+    public class Contract_Pattern : SmartContract.Framework.SmartContract
+    {
+
+
+        public bool between(int value)
+        {
+            return value is > 1 and < 100;
+        }
+
+        public bool between2(int value)
+        {
+            return value is (> 1 and < 100);
+        }
+
+        public bool between3(int value)
+        {
+            return value switch
+            {
+                (> 1 and < 50) => true,
+                (>= 50 and < 100) => true,
+                _ => false,
+            };
+        }
+
+    }
+
+
+}
