53a383ab5670d3c22d9761a113a50f7ef1a75ee0
Shine Li <bfshm@qq.com>
2024-03-08 16:16:55 +0800

    Fix is (pattern) (#987)
    * init
    
    * Clean empty lines
    
    * Clean empty lines
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>

---
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Pattern.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.SmartContract.TestEngine;
+
+namespace Neo.Compiler.CSharp.UnitTests
+{
+    [TestClass]
+    public class UnitTest_Pattern
+    {
+        [TestMethod]
+        public void Between_Test()
+        {
+            using var testengine = new TestEngine();
+            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Pattern.cs");
+
+            var result = testengine.ExecuteTestCaseStandard("between", 50);
+            var value = result.Pop().GetBoolean();
+            Assert.AreEqual(true, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between", 1);
+
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between", 100);
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between", 200);
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+        }
+
+        [TestMethod]
+        public void Between2_Test()
+        {
+            using var testengine = new TestEngine();
+            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Pattern.cs");
+
+            var result = testengine.ExecuteTestCaseStandard("between2", 50);
+            var value = result.Pop().GetBoolean();
+            Assert.AreEqual(true, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between2", 1);
+
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between2", 100);
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between2", 200);
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+        }
+
+        [TestMethod]
+        public void Between3_Test()
+        {
+            using var testengine = new TestEngine();
+            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Pattern.cs");
+
+            var result = testengine.ExecuteTestCaseStandard("between3", 50);
+            var value = result.Pop().GetBoolean();
+            Assert.AreEqual(true, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between3", 1);
+
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between3", 100);
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+
+            testengine.Reset();
+            result = testengine.ExecuteTestCaseStandard("between3", 200);
+            value = result.Pop().GetBoolean();
+            Assert.AreEqual(false, value);
+        }
+    }
+}
