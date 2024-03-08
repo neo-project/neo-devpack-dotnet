c4027cdc095dba16bf11f30f8ef3cdc5e630ef1d
陈志同 <bitcoin2077@outlook.com>
2024-03-08 11:58:44 +0800

    Update Contract_Logical.cs

--- tests/Neo.Compiler.CSharp.TestContracts/Contract_Logical.cs
+++ tests/Neo.Compiler.CSharp.TestContracts/Contract_Logical.cs
@@ -20,17 +20,17 @@ namespace Neo.Compiler.CSharp.UnitTests.TestClasses
             return x ^ y;
         }
 
-        public static byte TestLogicalAnd(byte x, byte y)
+        public static int TestLogicalAnd(byte x, byte y)
         {
             return x & y;
         }
 
-        public static byte TestLogicalOr(byte x, byte y)
+        public static int TestLogicalOr(byte x, byte y)
         {
             return x | y;
         }
 
-        public static byte TestLogicalNegation(bool x)
+        public static bool TestLogicalNegation(bool x)
         {
             return !x;
         }
