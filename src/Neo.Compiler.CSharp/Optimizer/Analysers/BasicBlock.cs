1bc2021221b956659306aafeb085d5532fe47d8b
Hecate2 <2474101468@qq.com>
2024-03-25 11:48:09 +0800

    extended basic block

--- src/Neo.Compiler.CSharp/Optimizer/Analysers/BasicBlock.cs
+++ src/Neo.Compiler.CSharp/Optimizer/Analysers/BasicBlock.cs
@@ -6,9 +6,33 @@ using System.Collections.Generic;
 
 namespace Neo.Optimizer
 {
-    static class BasicBlock
+    class BasicBlock
     {
         public static Dictionary<int, Dictionary<int, Instruction>> FindBasicBlocks(NefFile nef, ContractManifest manifest, JToken debugInfo)
             => new InstructionCoverage(nef, manifest, debugInfo).basicBlocks;
+
+        public List<Instruction> instructions { get; set; }
+        public BasicBlock? nextBlock = null;
+        public BasicBlock? jumpTargetBlock1 = null;
+        public BasicBlock? jumpTargetBlock2 = null;
+
+        public BasicBlock(List<Instruction> instructions)
+        {
+            this.instructions = instructions;
+        }
+
+        public void SetNextBasicBlock(BasicBlock block) => this.nextBlock = block;
+        public void SetJumpTargetBlock1(BasicBlock block) => this.jumpTargetBlock1 = block;
+        public void SetJumpTargetBlock2(BasicBlock block) => this.jumpTargetBlock2 = block;
+    }
+
+    class BasicBlocksInContract
+    {
+        List<BasicBlock> basicBlocks;
+        public BasicBlocksInContract(NefFile nef, ContractManifest manifest, JToken debugInfo)
+        {
+            InstructionCoverage coverage = new InstructionCoverage(nef, manifest, debugInfo);
+            Dictionary<int, Dictionary<int, Instruction>> basicBlocks = coverage.basicBlocks;
+        }
     }
 }
