275744d52c8352aa4cbf39f745e5f8a25f4394ec
陈志同 <bitcoin2077@outlook.com>
2024-04-07 17:34:42 +0800

    Update UnitTest_Parameters.cs

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Parameters.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Parameters.cs
@@ -14,15 +14,8 @@ namespace Neo.Compiler.CSharp.UnitTests
             var file = "Contract_BigInteger.cs";
             var path = $"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts\\{file}";
             var outputDir = "output";
-
-            File.Delete($"{currentDir}\\{outputDir}\\{file.Replace(".cs", ".nef")}");
-            File.Delete($"{currentDir}\\{outputDir}\\{file.Replace(".cs", ".manifest.json")}");
-
             var result = Program.Main([path, "-o", outputDir]);
-
             Assert.AreEqual(result, 0);
-            Assert.IsTrue(File.Exists($"{currentDir}\\{outputDir}\\{file.Replace(".cs", ".nef")}"));
-            Assert.IsTrue(File.Exists($"{currentDir}\\{outputDir}\\{file.Replace(".cs", ".manifest.json")}"));
         }
     }
 }
