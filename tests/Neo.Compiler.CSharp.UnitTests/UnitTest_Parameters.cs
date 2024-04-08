181ccfcb5ea17468103b77684f5d374081b90af6
陈志同 <bitcoin2077@outlook.com>
2024-04-08 17:40:34 +0800

    Update UnitTest_Parameters.cs

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Parameters.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Parameters.cs
@@ -7,73 +7,86 @@ namespace Neo.Compiler.CSharp.UnitTests
     [TestClass]
     public class UnitTest_Parameters
     {
+        readonly string csFileDir = $"{System.Environment.CurrentDirectory[..System.Environment.CurrentDirectory.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts";
+
         [TestMethod]
         public void TestNoParameter()
         {
-            var result = Program.Main([]);
-            Assert.AreEqual(result, 2);
+            Assert.AreEqual(Program.Main([]), 2);
         }
 
         [TestMethod]
         public void Test()
         {
-            var currentDir = System.Environment.CurrentDirectory;
-            var file = "Contract_BigInteger.cs";
-            var path = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file);
-            var result = Program.Main([path]);
-            Assert.AreEqual(result, 0);
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path]), 0);
+            Assert.IsTrue(File.Exists(Path.Combine(csFileDir, "bin", "sc", "Contract_BigInteger.nef")));
+            Assert.IsTrue(File.Exists(Path.Combine(csFileDir, "bin", "sc", "Contract_BigInteger.manifest.json")));
         }
 
         [TestMethod]
         public void TestOutput()
         {
-            var currentDir = System.Environment.CurrentDirectory;
-            var file = "Contract_BigInteger.cs";
-            var path = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file);
-            var result = Program.Main([path, "-o", "output"]);
-            Assert.AreEqual(result, 0);
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path, "-o", "output"]), 0);
         }
 
         [TestMethod]
         public void TestBaseName()
         {
-            var currentDir = System.Environment.CurrentDirectory;
-            var file = "Contract_BigInteger.cs";
-            var path = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file);
-            var result = Program.Main([path, "--base-name", "MyContract"]);
-            Assert.AreEqual(result, 0);
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path, "--base-name", "MyContract"]), 0);
         }
 
         [TestMethod]
         public void TestNotCSharpFile()
         {
-            var currentDir = System.Environment.CurrentDirectory;
-            var file = "Contract_BigInteger.txt";
-            var path = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file);
-            var result = Program.Main([path]);
-            Assert.AreEqual(result, 1);
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.txt");
+            Assert.AreEqual(Program.Main([path]), 1);
         }
 
         [TestMethod]
         public void TestNotExist()
         {
-            var currentDir = System.Environment.CurrentDirectory;
-            var file = "Contract_NotExist.cs";
-            var path = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file);
-            var result = Program.Main([path]);
-            Assert.AreEqual(result, 1);
+            var path = Path.Combine(csFileDir, "Contract_NotExist.cs");
+            Assert.AreEqual(Program.Main([path]), 1);
         }
 
         [TestMethod]
         public void TestMultiFile()
         {
-            var currentDir = System.Environment.CurrentDirectory;
-            var file = "Contract_BigInteger.cs";
-            var file2 = "Contract_Math.cs";
-            var path = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file);
-            var path2 = Path.Combine($"{currentDir[..currentDir.IndexOf("Neo.Compiler.CSharp.UnitTests")]}Neo.Compiler.CSharp.TestContracts", file2);
-            var result = Program.Main([path, path2]);
-            Assert.AreEqual(result, 0);
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            var path2 = Path.Combine(csFileDir, "Contract_Math.cs");
+            Assert.AreEqual(Program.Main([path, path2]), 0);
+        }
+
+        [TestMethod]
+        public void TestGenerateArtifacts()
+        {
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path, "--generate-artifacts", "All"]), 0);
+            Assert.IsTrue(File.Exists(Path.Combine(csFileDir, "bin", "sc", "Contract_BigInteger.artifacts.cs")));
+        }
+
+        [TestMethod]
+        public void TestNullAble()
+        {
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path, "--nullable", "Enable"]), 0);
+        }
+
+        [TestMethod]
+        public void TestDebug()
+        {
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path, "--debug"]), 0);
+        }
+
+        [TestMethod]
+        public void TestAssembly()
+        {
+            var path = Path.Combine(csFileDir, "Contract_BigInteger.cs");
+            Assert.AreEqual(Program.Main([path, "--assembly"]), 0);
         }
     }
 }
