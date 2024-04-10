f41e1d22c8ee9afba48df8e7d7d083a7c235d94d
Jim8y <jimmy@r3e.network>
2024-04-10 17:06:00 +0800

    working version

---
+++ examples/Example.SmartContract.NFT.UnitTests/NFTTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.SmartContract;
+using Neo.SmartContract.Manifest;
+using Neo.SmartContract.Testing;
+using Neo.SmartContract.Testing.Coverage;
+using Neo.SmartContract.Testing.TestingStandards;
+
+namespace Example.SmartContract.NFT.UnitTests
+{
+    [TestClass]
+    public class NFTTests : TestBase<SampleLootNFT>
+    {
+        public NFTTests() : base(SampleLootNFT.Nef, SampleLootNFT.Manifest)
+        {
+        }
+
+        [TestMethod]
+        public void TestClaim()
+        {
+            Contract.Claim(7772);
+        }
+
+    }
+}
