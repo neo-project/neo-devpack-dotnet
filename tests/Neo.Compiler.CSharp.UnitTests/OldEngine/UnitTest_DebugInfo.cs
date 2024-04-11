253c30b7dcfffd76e882abf8e88f65eb32529920
Fernando Diaz Toledano <shargon@gmail.com>
2024-04-11 09:53:27 +0200

    Migrate old ut

---
+++ tests/Neo.Compiler.CSharp.UnitTests/OldEngine/UnitTest_DebugInfo.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Json;
+using Neo.SmartContract;
+using System.Linq;
+using Neo.SmartContract.TestEngine;
+
+namespace Neo.Compiler.CSharp.UnitTests.OldEngine
+{
+    [TestClass]
+    public class UnitTest_DebugInfo
+    {
+        [TestMethod]
+        public void Test_DebugInfo()
+        {
+            var testEngine = new TestEngine();
+            testEngine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Event.cs");
+
+            var debugInfo = testEngine.DebugInfo;
+            Assert.AreEqual(testEngine.Nef.Script.Span.ToScriptHash().ToString(), debugInfo["hash"].GetString());
+            Assert.IsTrue(debugInfo.ContainsProperty("documents"));
+            Assert.IsInstanceOfType(debugInfo["documents"], typeof(JArray));
+            Assert.IsTrue((debugInfo["documents"] as JArray).Count > 0);
+            Assert.IsTrue((debugInfo["documents"] as JArray).All(n => n is JString), "All documents items should be string!");
+            Assert.IsTrue(debugInfo.ContainsProperty("methods"));
+            Assert.IsInstanceOfType(debugInfo["methods"], typeof(JArray));
+            Assert.AreEqual(1, (debugInfo["methods"] as JArray).Count);
+            Assert.AreEqual("Neo.Compiler.CSharp.UnitTests.TestClasses.Contract_Event,Main", (debugInfo["methods"] as JArray)[0]["name"].AsString());
+            Assert.AreEqual("3[0]17:28-17:29;4[0]17:13-17:29;5[0]17:13-17:29;6[0]17:13-17:29;7[0]18:28-18:32;8[0]18:28-18:32;10[0]18:13-18:32;11[0]18:13-18:32;12[0]18:13-18:32;13[0]19:9-19:10",
+                string.Join(';', ((debugInfo["methods"] as JArray)[0]["sequence-points"] as JArray).Select(u => u.AsString())));
+            Assert.IsTrue(debugInfo.ContainsProperty("events"));
+            Assert.IsInstanceOfType(debugInfo["events"], typeof(JArray));
+            Assert.AreEqual(1, (debugInfo["events"] as JArray).Count);
+            Assert.AreEqual("Neo.Compiler.CSharp.UnitTests.TestClasses.Contract_Event,Transferred", (debugInfo["events"] as JArray)[0]["name"].AsString());
+            Assert.AreEqual("arg1,ByteArray,0;arg2,ByteArray,1;arg3,Integer,2", string.Join(';', ((debugInfo["events"] as JArray)[0]["params"] as JArray).Select(u => u.AsString())));
+            Assert.IsTrue(debugInfo.ContainsProperty("static-variables"));
+            Assert.AreEqual("MyStaticVar1,Integer,0;MyStaticVar2,Boolean,1", string.Join(';', (debugInfo["static-variables"] as JArray).Select(u => u.AsString())));
+        }
+    }
+}
---
+++ tests/Neo.Compiler.CSharp.UnitTests/Optimizer/BasicBlockTests.cs
@@ -0,0 +1,93 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Optimizer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neo.Compiler.CSharp.UnitTests.Optimizer
+{
+    [TestClass]
+    public class BasicBlockTests
+    {
+        [TestMethod]
+        public void Test_BasicBlockStartEnd()
+        {
+            var files = Directory.GetFiles(OldEngine.Utils.Extensions.TestContractRoot, "Contract*.cs");
+            Parallel.ForEach(files, file =>
+            {
+                try
+                {
+                    TestSingleContractBasicBlockStartEnd(file);
+                }
+                catch
+                {
+                    Console.WriteLine($"Omited: {file}");
+                }
+            });
+        }
+
+        public static void TestSingleContractBasicBlockStartEnd(string fileName)
+        {
+            ContractInBasicBlocks basicBlocks;
+            try
+            {
+                // Compile
+
+                var results = new CompilationEngine(new CompilationOptions()
+                {
+                    Debug = true,
+                    CompilerVersion = "TestingEngine",
+                    Optimize = CompilationOptions.OptimizationType.All,
+                    Nullable = Microsoft.CodeAnalysis.NullableContextOptions.Enable
+                })
+                .CompileSources(fileName);
+
+                var context = results.Single();
+                basicBlocks = new(context.CreateExecutable(), context.CreateManifest(), context.CreateDebugInformation());
+            }
+            catch { return; }
+
+            // TODO: support CALLA and do not return
+
+            List<VM.Instruction> instructions = basicBlocks.GetScriptInstructions().ToList();
+            (_, _, Dictionary<VM.Instruction, HashSet<VM.Instruction>> jumpTargets) =
+                Neo.Optimizer.JumpTarget.FindAllJumpAndTrySourceToTargets(instructions);
+
+            Dictionary<VM.Instruction, VM.Instruction> nextAddrTable = new();
+            VM.Instruction? prev = null;
+            foreach (VM.Instruction i in instructions)
+            {
+                if (prev != null)
+                    nextAddrTable[prev] = i;
+                prev = i;
+            }
+
+            foreach (BasicBlock basicBlock in basicBlocks.basicBlocks)
+            {
+                // Basic block ends with allowed OpCodes only, or the next instruction is a jump target
+                Assert.IsTrue(OpCodeTypes.allowedBasicBlockEnds.Contains(basicBlock.instructions.Last().OpCode) || jumpTargets.ContainsKey(nextAddrTable[basicBlock.instructions.Last()]));
+                // Instructions except the first are not jump targets
+                foreach (VM.Instruction i in basicBlock.instructions.Skip(1))
+                    Assert.IsFalse(jumpTargets.ContainsKey(i));
+                // Other instructions in the basic block are not those in allowedBasicBlockEnds
+                foreach (VM.Instruction i in basicBlock.instructions.Take(basicBlock.instructions.Count - 1))
+                    Assert.IsFalse(OpCodeTypes.allowedBasicBlockEnds.Contains(i.OpCode));
+            }
+
+            // Each jump target starts a new basic block
+            foreach (VM.Instruction target in jumpTargets.Keys)
+                Assert.IsTrue(basicBlocks.basicBlocksByStartingInstruction.ContainsKey(target));
+
+            // Each instruction is included in only 1 basic block
+            HashSet<VM.Instruction> includedInstructions = new();
+            foreach (BasicBlock basicBlock in basicBlocks.basicBlocks)
+                foreach (VM.Instruction instruction in basicBlock.instructions)
+                {
+                    Assert.IsFalse(includedInstructions.Contains(instruction));
+                    includedInstructions.Add(instruction);
+                }
+        }
+    }
+}
