8e75222fcf8632c008ed3edd673f4b2f91d52e07
Fernando Diaz Toledano <shargon@gmail.com>
2024-04-11 10:45:47 +0200

    compile project and avoid errors from notWorking contracts

--- tests/Neo.Compiler.CSharp.UnitTests/TestCleanup.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/TestCleanup.cs
@@ -32,9 +32,8 @@ namespace Neo.Compiler.CSharp.UnitTests
             // Define paths
 
             var artifactsPath = Path.GetFullPath("../../../TestingArtifacts");
-            var utPath = Path.GetFullPath("../../../../Neo.Compiler.CSharp.TestContracts/Working");
-            var testContractsFiles = Directory.GetFiles(utPath);
-            var root = Path.GetPathRoot(utPath) ?? "";
+            var testContractsPath = Path.GetFullPath("../../../../Neo.Compiler.CSharp.TestContracts/Neo.Compiler.CSharp.TestContracts.csproj");
+            var root = Path.GetPathRoot(testContractsPath) ?? "";
 
             // Compile
 
@@ -45,7 +44,7 @@ namespace Neo.Compiler.CSharp.UnitTests
                 Optimize = CompilationOptions.OptimizationType.All,
                 Nullable = Microsoft.CodeAnalysis.NullableContextOptions.Enable
             })
-            .CompileSources(testContractsFiles);
+            .CompileProject(testContractsPath);
 
             // Ensure that all was well compiled
 
