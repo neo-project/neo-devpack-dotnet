226d33ed22ddf52081dfa3b9843fa0cac0e281e8
Jimmy <jimmy@r3e.network>
2024-04-16 17:05:27 +0800

    Optimize the example ut (#1019)
    * add
    
    * update neo
    
    * optimize the test setup to avoid duplicate compile
    
    * add random back
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>

--- examples/Example.SmartContract.NFT.UnitTests/TestCleanup.cs
+++ examples/Example.SmartContract.NFT.UnitTests/TestCleanup.cs
@@ -1,35 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Compiler;
 using Neo.SmartContract.Testing.Coverage;
-using Neo.SmartContract.Testing.Coverage.Formats;
 using Neo.SmartContract.Testing.Extensions;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace Example.SmartContract.NFT.UnitTests
 {
     public class TestCleanup
     {
-        private static readonly Regex WhiteSpaceRegex = new("\\s");
-        internal static readonly Dictionary<Type, NeoDebugInfo> DebugInfos = new();
-
         internal static void EnsureArtifactsUpToDateInternal()
         {
-            if (DebugInfos.Count > 0) return; // Maybe a UT call it
-
             // Define paths
-
             string testContractsPath = Path.GetFullPath("../../../../Example.SmartContract.NFT/Example.SmartContract.NFT.csproj");
             string artifactsPath = Path.GetFullPath("../../../TestingArtifacts");
             var root = Path.GetPathRoot(testContractsPath) ?? "";
 
             // Compile
-
-            var results = new CompilationEngine(new CompilationOptions()
+            var compilationContexts = new CompilationEngine(new CompilationOptions
             {
                 Debug = true,
                 CompilerVersion = "TestingEngine",
@@ -39,10 +29,9 @@ namespace Example.SmartContract.NFT.UnitTests
             .CompileProject(testContractsPath);
 
             // Ensure that all was well compiled
-
-            if (!results.All(u => u.Success))
+            if (!compilationContexts.All(u => u.Success))
             {
-                results.SelectMany(u => u.Diagnostics)
+                compilationContexts.SelectMany(u => u.Diagnostics)
                     .Where(u => u.Severity == Microsoft.CodeAnalysis.DiagnosticSeverity.Error)
                     .ToList().ForEach(Console.Error.WriteLine);
 
@@ -50,72 +39,35 @@ namespace Example.SmartContract.NFT.UnitTests
             }
 
             // Get all artifacts loaded in this assembly
-
-            foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
-            {
-                if (typeof(Neo.SmartContract.Testing.SmartContract).IsAssignableFrom(type))
-                {
-                    // Find result
-
-                    var result = results.Where(u => u.ContractName == type.Name).SingleOrDefault();
-                    if (result == null) continue;
-
-                    // Ensure that it exists
-
-                    DebugInfos[type] = CreateArtifact(result.ContractName!, result, root, Path.Combine(artifactsPath, $"{result.ContractName}.cs"), false);
-                    results.Remove(result);
-                }
-            }
-
-            // Ensure that all match
-
-            if (results.Count() > 0)
-            {
-                foreach (var result in results.Where(u => u.Success))
-                {
-                    CreateArtifact(result.ContractName!, result, root, Path.Combine(artifactsPath, $"{result.ContractName}.cs"), false);
-                }
-
-                Assert.Fail("Error compiling templates");
-            }
+            var result = compilationContexts.FirstOrDefault() ?? throw new ArgumentNullException($"Compilation context is null");
+            // Ensure that it exists
+            CreateArtifact(result.ContractName!, result, root, Path.Combine(artifactsPath, $"{result.ContractName}.cs"));
         }
 
-        private static NeoDebugInfo CreateArtifact(string typeName, CompilationContext context, string rootDebug, string artifactsPath, bool failIfWrong)
+        private static void CreateArtifact(string typeName, CompilationContext context, string rootDebug, string artifactsPath)
         {
-            (var nef, var manifest, var debugInfo) = context.CreateResults(rootDebug);
-            var debug = NeoDebugInfo.FromDebugInfoJson(debugInfo);
+            var (nef, manifest, _) = context.CreateResults(rootDebug);
             var artifact = manifest.GetArtifactsSource(typeName, nef, generateProperties: true);
 
-            string writtenArtifact = File.Exists(artifactsPath) ? File.ReadAllText(artifactsPath) : "";
-            if (string.IsNullOrEmpty(writtenArtifact) || WhiteSpaceRegex.Replace(artifact, "") != WhiteSpaceRegex.Replace(writtenArtifact, ""))
+            if (!File.Exists(artifactsPath))
             {
-                if (!File.Exists(artifactsPath))
+                var directoryPath = Path.GetDirectoryName(artifactsPath) ?? throw new ArgumentNullException($"{nameof(artifactsPath)} directory path is null");
+                if (!Directory.Exists(directoryPath))
                 {
-                    string directoryPath = Path.GetDirectoryName(artifactsPath);
-
-                    if (!Directory.Exists(directoryPath))
-                    {
-                        Directory.CreateDirectory(directoryPath);
-                    }
-
-                    File.Create(artifactsPath).Close();
+                    Directory.CreateDirectory(directoryPath);
                 }
-
-                // Uncomment to overwrite the artifact file
-                try
-                {
-                    File.WriteAllText(artifactsPath, artifact);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    throw;
-                }
-
-                if (failIfWrong) Assert.Fail($"{typeName} artifact was wrong");
+                File.Create(artifactsPath).Close();
             }
 
-            return debug;
+            try
+            {
+                File.WriteAllText(artifactsPath, artifact);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
