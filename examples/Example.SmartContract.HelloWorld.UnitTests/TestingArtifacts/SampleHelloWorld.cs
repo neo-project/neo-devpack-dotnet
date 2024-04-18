3b4d99608d0baed9a668b54ffa99ff634ce4c961
陈志同 <bitcoin2077@outlook.com>
2024-04-18 18:31:06 +0800

    Adding example test projects (#1023) * load contract after compile * fix the nef and manifest issue. * Update src/Neo.SmartContract.Testing/TestingStandards/TestBase.cs Co-authored-by: Shargon <shargon@gmail.com> * Adding Test Projects * fixedbug * Add some test cases * Update neo-devpack-dotnet.sln * format * Update neo-devpack-dotnet.sln * update * add Example.SmartContract.SampleRoyaltyNEP11Token.UnitTests * x * Update main.yml * Update main.yml * Update main.yml * format --------- Co-authored-by: Jim8y <jimmy@r3e.network> Co-authored-by: Jimmy <jinghui@wayne.edu> Co-authored-by: Shargon <shargon@gmail.com>

---
+++ examples/Example.SmartContract.HelloWorld.UnitTests/TestingArtifacts/SampleHelloWorld.cs
@@ -0,0 +1,33 @@
+using Neo.Cryptography.ECC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Neo.SmartContract.Testing;
+
+public abstract class SampleHelloWorld : Neo.SmartContract.Testing.SmartContract
+{
+    #region Compiled data
+
+    public static readonly Neo.SmartContract.Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""SampleHelloWorld"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""sayHello"",""parameters"":[],""returntype"":""String"",""offset"":0,""safe"":true}],""events"":[]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":{""Description"":""A simple \u0060hello world\u0060 contract"",""E-mail"":""dev@neo.org"",""Version"":""0.0.1"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/examples/""}}");
+
+    public static readonly Neo.SmartContract.NefFile Nef = Neo.IO.Helper.AsSerializable<Neo.SmartContract.NefFile>(Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABIMDUhlbGxvLCBXb3JsZCEiAkCFZwp1"));
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Safe property
+    /// </summary>
+    public abstract string? SayHello { [DisplayName("sayHello")] get; }
+
+    #endregion
+
+    #region Constructor for internal use only
+
+    protected SampleHelloWorld(Neo.SmartContract.Testing.SmartContractInitialize initialize) : base(initialize) { }
+
+    #endregion
+}
---
+++ examples/Example.SmartContract.Inscription.UnitTests/TestingArtifacts/SampleInscription.cs
@@ -0,0 +1,53 @@
+using Neo.Cryptography.ECC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Neo.SmartContract.Testing;
+
+public abstract class SampleInscription : Neo.SmartContract.Testing.SmartContract
+{
+    #region Compiled data
+
+    public static readonly Neo.SmartContract.Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""SampleInscription"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""addInscription"",""parameters"":[{""name"":""address"",""type"":""Hash160""},{""name"":""inscription"",""type"":""String""}],""returntype"":""Void"",""offset"":0,""safe"":false},{""name"":""getInscription"",""parameters"":[{""name"":""address"",""type"":""Hash160""}],""returntype"":""String"",""offset"":103,""safe"":true}],""events"":[{""name"":""InscriptionAdded"",""parameters"":[{""name"":""arg1"",""type"":""Hash160""},{""name"":""arg2"",""type"":""String""}]}]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":{""Author"":""core-dev"",""Description"":""A sample inscription contract."",""Version"":""0.0.1"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/examples/""}}");
+
+    public static readonly Neo.SmartContract.NefFile Nef = Neo.IO.Helper.AsSerializable<Neo.SmartContract.NefFile>(Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHhXAAJ4Qfgn7IyqJjIMLVVuYXV0aG9yaXplZDogQ2FsbGVyIGlzIG5vdCB0aGUgYWRkcmVzcyBvd25lcjp5eEGb9mfOQeY/GITCSnjPSnnPDBBJbnNjcmlwdGlvbkFkZGVkQZUBb2FAVwABeEGb9mfOQZJd6DEiAkB6I/xo"));
+
+    #endregion
+
+    #region Events
+
+    public delegate void delInscriptionAdded(UInt160? arg1, string? arg2);
+
+    [DisplayName("InscriptionAdded")]
+    public event delInscriptionAdded? OnInscriptionAdded;
+
+    #endregion
+
+    #region Safe methods
+
+    /// <summary>
+    /// Safe method
+    /// </summary>
+    [DisplayName("getInscription")]
+    public abstract string? GetInscription(UInt160? address);
+
+    #endregion
+
+    #region Unsafe methods
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    [DisplayName("addInscription")]
+    public abstract void AddInscription(UInt160? address, string? inscription);
+
+    #endregion
+
+    #region Constructor for internal use only
+
+    protected SampleInscription(Neo.SmartContract.Testing.SmartContractInitialize initialize) : base(initialize) { }
+
+    #endregion
+}
