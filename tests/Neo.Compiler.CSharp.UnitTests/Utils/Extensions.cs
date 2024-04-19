4d54288880ff657f2592475829cb6d9d5cacbb52
Fernando Diaz Toledano <shargon@gmail.com>
2024-04-19 10:45:32 +0200

    Move files

--- tests/Neo.Compiler.CSharp.UnitTests/Utils/Extensions.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/Utils/Extensions.cs
@@ -1,7 +1,10 @@
+using System.IO;
+
 namespace Neo.Compiler.CSharp.UnitTests.Utils
 {
     static class Extensions
     {
-        public static readonly string TestContractRoot = "../../../../Neo.Compiler.CSharp.TestContracts/";
+        public static readonly string TestContractRoot = Path.GetFullPath("../../../../Neo.Compiler.CSharp.TestContracts/Working") + "/";
+        public static readonly string NotWorkingTestContractRoot = Path.GetFullPath("../../../../Neo.Compiler.CSharp.TestContracts/NotWorking") + "/";
     }
 }
