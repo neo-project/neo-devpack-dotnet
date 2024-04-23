2fc7aeac1c96f1b694f27c4f7d435cd0cddc7fe5
Jim8y <jimmy@r3e.network>
2024-04-23 12:18:02 +0800

    move test to folder

--- tests/Neo.Compiler.CSharp.UnitTests/OldEngine/UnitTest_Record.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/OldEngine/UnitTest_Record.cs
@@ -1,6 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.VM.Types;
-using System.Linq;
 using Neo.SmartContract.TestEngine;
 
 namespace Neo.Compiler.CSharp.UnitTests
@@ -8,15 +7,21 @@ namespace Neo.Compiler.CSharp.UnitTests
     [TestClass]
     public class UnitTest_Record
     {
+        private TestEngine testEngine;
+
+        [TestInitialize]
+        public void Init()
+        {
+            testEngine = new TestEngine();
+            testEngine.AddNoOptimizeEntryScript(Utils.Extensions.TestContractRoot + "Contract_Record.cs");
+        }
+
         [TestMethod]
         public void Test_CreateRecord()
         {
-            var testengine = new TestEngine();
-            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Record.cs");
-
             var name = "klsas";
             var age = 24;
-            var result = testengine.ExecuteTestCaseStandard("test_CreateRecord", name, age);
+            var result = testEngine.ExecuteTestCaseStandard("test_CreateRecord", name, age);
             var arr = result.Pop<Struct>();
             Assert.AreEqual(2, arr.Count);
             Assert.AreEqual(name, arr[0].GetString());
@@ -26,12 +31,9 @@ namespace Neo.Compiler.CSharp.UnitTests
         [TestMethod]
         public void Test_CreateRecord2()
         {
-            var testengine = new TestEngine();
-            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Record.cs");
-
             var name = "klsas";
             var age = 24;
-            var result = testengine.ExecuteTestCaseStandard("test_CreateRecord2", name, age);
+            var result = testEngine.ExecuteTestCaseStandard("test_CreateRecord2", name, age);
             var arr = result.Pop<Struct>();
             Assert.AreEqual(2, arr.Count);
             Assert.AreEqual(name, arr[0].GetString());
@@ -41,12 +43,9 @@ namespace Neo.Compiler.CSharp.UnitTests
         [TestMethod]
         public void Test_UpdateRecord()
         {
-            var testengine = new TestEngine();
-            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Record.cs");
-
             var name = "klsas";
             var age = 24;
-            var result = testengine.ExecuteTestCaseStandard("test_UpdateRecord", name, age);
+            var result = testEngine.ExecuteTestCaseStandard("test_UpdateRecord", name, age);
             var arr = result.Pop<Struct>();
             Assert.AreEqual(2, arr.Count);
             Assert.AreEqual(name, arr[0].GetString());
@@ -56,12 +55,9 @@ namespace Neo.Compiler.CSharp.UnitTests
         [TestMethod]
         public void Test_UpdateRecord2()
         {
-            var testengine = new TestEngine();
-            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Record.cs");
-
             var name = "klsas";
             var age = 2;
-            var result = testengine.ExecuteTestCaseStandard("test_UpdateRecord2", name, age);
+            var result = testEngine.ExecuteTestCaseStandard("test_UpdateRecord2", name, age);
             var arr = result.Pop<Struct>();
             Assert.AreEqual(2, arr.Count);
             Assert.AreEqual("0" + name, arr[0].GetString());
@@ -71,12 +67,9 @@ namespace Neo.Compiler.CSharp.UnitTests
         [TestMethod]
         public void Test_DeconstructRecord()
         {
-            var testengine = new TestEngine();
-            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_Record.cs");
-
             var name = "klsas";
             var age = 24;
-            var result = testengine.ExecuteTestCaseStandard("test_DeconstructRecord", name, age);
+            var result = testEngine.ExecuteTestCaseStandard("test_DeconstructRecord", name, age);
             var arr = result.Pop().GetString();
             Assert.AreEqual(name, arr);
         }
