abd6bbfcf7530bdd66dc03f4f450b60e7c8df436
Fernando Diaz Toledano <shargon@gmail.com>
2024-06-06 11:15:54 +0200

    Migrate UTS

---
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Types_ECPoint.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.SmartContract.Testing;
+using Neo.SmartContract.Testing.Exceptions;
+using Neo.SmartContract.Testing.InvalidTypes;
+using Neo.SmartContract.Testing.TestingStandards;
+
+namespace Neo.Compiler.CSharp.UnitTests
+{
+    [TestClass]
+    public class UnitTest_Types_ECPoint : TestBase<Contract_Types_ECPoint>
+    {
+        public UnitTest_Types_ECPoint() : base(Contract_Types_ECPoint.Nef, Contract_Types_ECPoint.Manifest) { }
+
+        [TestMethod]
+        public void ECPoint_test()
+        {
+            Assert.IsFalse(Contract.IsValid(InvalidECPoint.InvalidLength));
+            Assert.IsFalse(Contract.IsValid(InvalidECPoint.InvalidType));
+            Assert.ThrowsException<TestException>(() => Contract.IsValid(InvalidECPoint.Null));
+            Assert.IsTrue(Contract.IsValid(Cryptography.ECC.ECPoint.Parse("024700db2e90d9f02c4f9fc862abaca92725f95b4fddcc8d7ffa538693ecf463a9", Cryptography.ECC.ECCurve.Secp256r1)));
+
+            Assert.AreEqual("024700db2e90d9f02c4f9fc862abaca92725f95b4fddcc8d7ffa538693ecf463a9", Contract.Ecpoint2String()); // ERROR: Is converted to string, but in bytes
+            Assert.AreEqual("024700db2e90d9f02c4f9fc862abaca92725f95b4fddcc8d7ffa538693ecf463a9", Contract.EcpointReturn()?.ToString());
+            Assert.AreEqual("024700db2e90d9f02c4f9fc862abaca92725f95b4fddcc8d7ffa538693ecf463a9", Contract.Ecpoint2ByteArray()?.ToString());
+        }
+    }
+}
