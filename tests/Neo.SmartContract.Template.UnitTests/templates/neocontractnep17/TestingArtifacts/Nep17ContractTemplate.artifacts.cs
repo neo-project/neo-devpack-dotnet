ada279973c8a4854e1756c27dfec3c879873feec
Jim8y <jimmy@r3e.network>
2024-07-15 08:39:46 +0800

    fix template artifacts

--- tests/Neo.SmartContract.Template.UnitTests/templates/neocontractnep17/TestingArtifacts/Nep17ContractTemplate.artifacts.cs
+++ tests/Neo.SmartContract.Template.UnitTests/templates/neocontractnep17/TestingArtifacts/Nep17ContractTemplate.artifacts.cs
@@ -6,16 +6,16 @@ using System.Numerics;
 
 namespace Neo.SmartContract.Testing;
 
-public abstract class Nep17ContractTemplate : Neo.SmartContract.Testing.SmartContract, Neo.SmartContract.Testing.TestingStandards.IOwnable
+public abstract class Nep17ContractTemplate : Neo.SmartContract.Testing.SmartContract, Neo.SmartContract.Testing.TestingStandards.INep17Standard, Neo.SmartContract.Testing.TestingStandards.IOwnable, Neo.SmartContract.Testing.TestingStandards.IVerificable
 {
     #region Compiled data
 
-    public static readonly Neo.SmartContract.Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Ownable"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""getOwner"",""parameters"":[],""returntype"":""Hash160"",""offset"":0,""safe"":true},{""name"":""setOwner"",""parameters"":[{""name"":""newOwner"",""type"":""Hash160""}],""returntype"":""Void"",""offset"":44,""safe"":false},{""name"":""myMethod"",""parameters"":[],""returntype"":""String"",""offset"":167,""safe"":false},{""name"":""_deploy"",""parameters"":[{""name"":""data"",""type"":""Any""},{""name"":""update"",""type"":""Boolean""}],""returntype"":""Void"",""offset"":187,""safe"":false},{""name"":""update"",""parameters"":[{""name"":""nefFile"",""type"":""ByteArray""},{""name"":""manifest"",""type"":""String""},{""name"":""data"",""type"":""Any""}],""returntype"":""Void"",""offset"":308,""safe"":false},{""name"":""destroy"",""parameters"":[],""returntype"":""Void"",""offset"":347,""safe"":false}],""events"":[{""name"":""SetOwner"",""parameters"":[{""name"":""previousOwner"",""type"":""Hash160""},{""name"":""newOwner"",""type"":""Hash160""}]}]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":{""Author"":""\u003CYour Name Or Company Here\u003E"",""Description"":""\u003CDescription Here\u003E"",""Version"":""\u003CVersion String Here\u003E"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/src/Neo.SmartContract.Template/templates/neocontractowner/Ownable.cs"",""nef"":{""optimization"":""All""}}}");
+    public static readonly Neo.SmartContract.Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Nep17Contract"",""groups"":[],""features"":{},""supportedstandards"":[""NEP-17""],""abi"":{""methods"":[{""name"":""symbol"",""parameters"":[],""returntype"":""String"",""offset"":1146,""safe"":true},{""name"":""decimals"",""parameters"":[],""returntype"":""Integer"",""offset"":1161,""safe"":true},{""name"":""totalSupply"",""parameters"":[],""returntype"":""Integer"",""offset"":43,""safe"":true},{""name"":""balanceOf"",""parameters"":[{""name"":""owner"",""type"":""Hash160""}],""returntype"":""Integer"",""offset"":89,""safe"":true},{""name"":""transfer"",""parameters"":[{""name"":""from"",""type"":""Hash160""},{""name"":""to"",""type"":""Hash160""},{""name"":""amount"",""type"":""Integer""},{""name"":""data"",""type"":""Any""}],""returntype"":""Boolean"",""offset"":279,""safe"":false},{""name"":""getOwner"",""parameters"":[],""returntype"":""Hash160"",""offset"":697,""safe"":true},{""name"":""setOwner"",""parameters"":[{""name"":""newOwner"",""type"":""Hash160""}],""returntype"":""Void"",""offset"":741,""safe"":false},{""name"":""burn"",""parameters"":[{""name"":""account"",""type"":""Hash160""},{""name"":""amount"",""type"":""Integer""}],""returntype"":""Void"",""offset"":861,""safe"":false},{""name"":""mint"",""parameters"":[{""name"":""to"",""type"":""Hash160""},{""name"":""amount"",""type"":""Integer""}],""returntype"":""Void"",""offset"":901,""safe"":false},{""name"":""verify"",""parameters"":[],""returntype"":""Boolean"",""offset"":941,""safe"":true},{""name"":""myMethod"",""parameters"":[],""returntype"":""String"",""offset"":947,""safe"":false},{""name"":""_deploy"",""parameters"":[{""name"":""data"",""type"":""Any""},{""name"":""update"",""type"":""Boolean""}],""returntype"":""Void"",""offset"":967,""safe"":false},{""name"":""update"",""parameters"":[{""name"":""nefFile"",""type"":""ByteArray""},{""name"":""manifest"",""type"":""String""},{""name"":""data"",""type"":""Any""}],""returntype"":""Void"",""offset"":1091,""safe"":false},{""name"":""_initialize"",""parameters"":[],""returntype"":""Void"",""offset"":1130,""safe"":false}],""events"":[{""name"":""Transfer"",""parameters"":[{""name"":""from"",""type"":""Hash160""},{""name"":""to"",""type"":""Hash160""},{""name"":""amount"",""type"":""Integer""}]},{""name"":""SetOwner"",""parameters"":[{""name"":""previousOwner"",""type"":""Hash160""},{""name"":""newOwner"",""type"":""Hash160""}]}]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":{""Author"":""\u003CYour Name Or Company Here\u003E"",""Description"":""\u003CDescription Here\u003E"",""Version"":""\u003CVersion String Here\u003E"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/src/Neo.SmartContract.Template/templates/neocontractnep17/Nep17Contract.cs"",""nef"":{""optimization"":""All""}}}");
 
     /// <summary>
     /// Optimization: "All"
     /// </summary>
-    public static readonly Neo.SmartContract.NefFile Nef = Neo.IO.Helper.AsSerializable<Neo.SmartContract.NefFile>(Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL9o/pDRupTKiWPxJfdrdtkN8n9/wZ1cGRhdGUDAAAP/aP6Q0bqUyolj8SX3a3bZDfJ/f8HZGVzdHJveQAAAA8AAP17AQwB/9swNBBK2CQJSsoAFCgDOiICQFcAAXhB9rRr4kGSXegxQDTcQfgn7IxAVwEBNPUJlyYWDBFObyBBdXRob3JpemF0aW9uITp4StkoUMoAFLOrJAUJIgZ4ELOqDBNvd25lciBtdXN0IGJlIHZhbGlk4TWO////cHgMAf/bMDQZwkpoz0p4zwwIU2V0T3duZXJBlQFvYUBXAAJ5eEGb9mfOQeY/GIRADAVIZWxsb0Gb9mfOQZJd6DEiAkBXAQJ5JgQicnhwaAuXJgxBLVEIMBPOSoBFeHBoStkoUMoAFLOrJAUJIgZoELOqDBFvd25lciBtdXN0IGV4aXN0c+FoDAH/2zA0lMJKC89KaM8MCFNldE93bmVyQZUBb2EMBVdvcmxkDAVIZWxsb0Gb9mfOQeY/GIRAVwADNe3+//8JlyYWDBFObyBhdXRob3JpemF0aW9uLjp6eXg3AABANcn+//+qJhYMEU5vIGF1dGhvcml6YXRpb24uOjcBAEARZnis"));
+    public static readonly Neo.SmartContract.NefFile Nef = Neo.IO.Helper.AsSerializable<Neo.SmartContract.NefFile>(Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL9o/pDRupTKiWPxJfdrdtkN8n9/wtnZXRDb250cmFjdAEAAQ/9o/pDRupTKiWPxJfdrdtkN8n9/wZ1cGRhdGUDAAAPAAD9mARXAAEMB0VYQU1QTEVAVwABeDQDQFcAAXg0A0BXAAF4NANAVwABQFcAARhAWdgmFwwBAEH2tGviQZJd6DFK2CYERRBKYUBXAAF4YXgMAQBBm/ZnzkHmPxiEQFcBAXhwaAuXJgUIIg14StkoUMoAFLOrqiYlDCBUaGUgYXJndW1lbnQgIm93bmVyIiBpcyBpbnZhbGlkLjpBm/ZnzhERiE4QUdBQEsBweGjBRVOLUEGSXegxStgmBEUQ2yEiAkBXAgJBm/ZnzhERiE4QUdBQEsBweGjBRVOLUEGSXegxStgmBEUQ2yFxaXmeSnFFaRC1JgUJIiVpELMmEHhowUVTi1BBL1jF7SIPaXhowUVTi1BB5j8YhAgiAkBXAQR4cGgLlyYFCCINeErZKFDKABSzq6omJAwfVGhlIGFyZ3VtZW50ICJmcm9tIiBpcyBpbnZhbGlkLjp5cGgLlyYFCCINeUrZKFDKABSzq6omIgwdVGhlIGFyZ3VtZW50ICJ0byIgaXMgaW52YWxpZC46ehC1JioMJVRoZSBhbW91bnQgbXVzdCBiZSBhIHBvc2l0aXZlIG51bWJlci46eEH4J+yMqiYFCSImehCYJhh6m3g18v7//6omBQkiE3p5NeX+//9Fe3p5eDQGCCICQFcBBMJKeM9Kec9Kes8MCFRyYW5zZmVyQZUBb2F5cGgLl6okBQkiC3k3AABwaAuXqiYfe3p4E8AfDA5vbk5FUDE3UGF5bWVudHlBYn1bUkVAVwACeZkQtSYLDAZhbW91bnQ6eRCzJgQiIXl4NWr+//9FNdL9//95nko15P3//0ULeXgLNX3///9AVwACeZkQtSYLDAZhbW91bnQ6eRCzJgQiMHmbeDUw/v//qiYODAlleGNlcHRpb246NYr9//95n0o1nP3//0ULeQt4NTX///9ADAH/2zA0EErYJAlKygAUKAM6IgJAVwABeEH2tGviQZJd6DFANNxB+CfsjEBXAQE09QmXJhYMEU5vIEF1dGhvcml6YXRpb24hOnhK2ShQygAUs6skBQkiBngQs6oME293bmVyIG11c3QgYmUgdmFsaWThNI5weAwB/9swNBnCSmjPSnjPDAhTZXRPd25lckGVAW9hQFcAAnl4QZv2Z85B5j8YhEBXAAI1ff///wmXJhYMEU5vIEF1dGhvcml6YXRpb24hOnl4NfL+//9AVwACNVX///8JlyYWDBFObyBBdXRob3JpemF0aW9uITp5eDWR/v//QDUw////QAwFSGVsbG9Bm/ZnzkGSXegxIgJAVwECeSYEInV4cGgLlyYMQS1RCDATzkqARXhwaErZKFDKABSzqyQFCSIGaBCzqgwRb3duZXIgbXVzdCBleGlzdHPhaAwB/9swNT7////CSgvPSmjPDAhTZXRPd25lckGVAW9hDAVXb3JsZAwFSGVsbG9Bm/ZnzkHmPxiEQFcAAzWX/v//CZcmFgwRTm8gYXV0aG9yaXphdGlvbi46enl4NwEAQFYCCrr7//8Kj/v//xLAYEDCSljPSjWO+///I3z7///CSljPSjV/+///I5P7//8hMUvY"));
 
     #endregion
 
@@ -24,6 +24,9 @@ public abstract class Nep17ContractTemplate : Neo.SmartContract.Testing.SmartCon
     [DisplayName("SetOwner")]
     public event Neo.SmartContract.Testing.TestingStandards.IOwnable.delSetOwner? OnSetOwner;
 
+    [DisplayName("Transfer")]
+    public event Neo.SmartContract.Testing.TestingStandards.INep17Standard.delTransfer? OnTransfer;
+
     #endregion
 
     #region Properties
@@ -31,7 +34,37 @@ public abstract class Nep17ContractTemplate : Neo.SmartContract.Testing.SmartCon
     /// <summary>
     /// Safe property
     /// </summary>
+    public abstract BigInteger? Decimals { [DisplayName("decimals")] get; }
+
+    /// <summary>
+    /// Safe property
+    /// </summary>
     public abstract UInt160? Owner { [DisplayName("getOwner")] get; [DisplayName("setOwner")] set; }
+
+    /// <summary>
+    /// Safe property
+    /// </summary>
+    public abstract string? Symbol { [DisplayName("symbol")] get; }
+
+    /// <summary>
+    /// Safe property
+    /// </summary>
+    public abstract BigInteger? TotalSupply { [DisplayName("totalSupply")] get; }
+
+    /// <summary>
+    /// Safe property
+    /// </summary>
+    public abstract bool? Verify { [DisplayName("verify")] get; }
+
+    #endregion
+
+    #region Safe methods
+
+    /// <summary>
+    /// Safe method
+    /// </summary>
+    [DisplayName("balanceOf")]
+    public abstract BigInteger? BalanceOf(UInt160? owner);
 
     #endregion
 
@@ -40,14 +73,26 @@ public abstract class Nep17ContractTemplate : Neo.SmartContract.Testing.SmartCon
     /// <summary>
     /// Unsafe method
     /// </summary>
-    [DisplayName("destroy")]
-    public abstract void Destroy();
+    [DisplayName("burn")]
+    public abstract void Burn(UInt160? account, BigInteger? amount);
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    [DisplayName("mint")]
+    public abstract void Mint(UInt160? to, BigInteger? amount);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("myMethod")]
     public abstract string? MyMethod();
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    [DisplayName("transfer")]
+    public abstract bool? Transfer(UInt160? from, UInt160? to, BigInteger? amount, object? data = null);
 
     /// <summary>
     /// Unsafe method
