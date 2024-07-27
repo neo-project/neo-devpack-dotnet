bab5016ecbbe0ba8f2c50e94aa744b762652eb88
Jim8y <jimmy@r3e.network>
2024-07-27 18:22:05 +0800

    fix type

--- tests/Neo.Compiler.CSharp.UnitTests/TestingArtifacts/Contract_ErrorNullable.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/TestingArtifacts/Contract_ErrorNullable.cs
@@ -10,7 +10,7 @@ public abstract class Contract_ErrorNullable : Neo.SmartContract.Testing.SmartCo
 {
     #region Compiled data
 
-    public static readonly Neo.SmartContract.Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_ErrorNullable"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""testBigInteger"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":0,""safe"":false},{""name"":""testInt"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":4,""safe"":false},{""name"":""testUInt"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":8,""safe"":false},{""name"":""testLong"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":12,""safe"":false},{""name"":""testULong"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":16,""safe"":false},{""name"":""testShort"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":20,""safe"":false},{""name"":""testUShort"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":24,""safe"":false},{""name"":""testSByte"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":28,""safe"":false},{""name"":""testByte"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":32,""safe"":false},{""name"":""testChar"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":36,""safe"":false},{""name"":""testBool"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":40,""safe"":false},{""name"":""testUInt160"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":44,""safe"":false},{""name"":""testUInt256"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":48,""safe"":false},{""name"":""testUInt160Array"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":52,""safe"":false},{""name"":""testUInt256Array"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":56,""safe"":false},{""name"":""testByteArray"",""parameters"":[{""name"":""a"",""type"":""ByteArray""}],""returntype"":""Void"",""offset"":60,""safe"":false},{""name"":""testString"",""parameters"":[{""name"":""a"",""type"":""String""}],""returntype"":""Void"",""offset"":64,""safe"":false},{""name"":""testObject"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":68,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""nef"":{""optimization"":""All""}}}");
+    public static readonly Neo.SmartContract.Manifest.ContractManifest Manifest = Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_ErrorNullable"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""testBigInteger"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":0,""safe"":false},{""name"":""testInt"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":4,""safe"":false},{""name"":""testUInt"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":8,""safe"":false},{""name"":""testLong"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":12,""safe"":false},{""name"":""testULong"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":16,""safe"":false},{""name"":""testShort"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":20,""safe"":false},{""name"":""testUShort"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":24,""safe"":false},{""name"":""testSByte"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":28,""safe"":false},{""name"":""testByte"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":32,""safe"":false},{""name"":""testChar"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":36,""safe"":false},{""name"":""testBool"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":40,""safe"":false},{""name"":""testUInt160"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":44,""safe"":false},{""name"":""testUInt256"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":48,""safe"":false},{""name"":""testUInt160Array"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":52,""safe"":false},{""name"":""testUInt256Array"",""parameters"":[{""name"":""a"",""type"":""Array""}],""returntype"":""Void"",""offset"":56,""safe"":false},{""name"":""testByteArray"",""parameters"":[{""name"":""a"",""type"":""ByteArray""}],""returntype"":""Void"",""offset"":60,""safe"":false},{""name"":""testString"",""parameters"":[{""name"":""a"",""type"":""String""}],""returntype"":""Void"",""offset"":64,""safe"":false},{""name"":""testObject"",""parameters"":[{""name"":""a"",""type"":""Any""}],""returntype"":""Void"",""offset"":68,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""nef"":{""optimization"":""All""}}}");
 
     /// <summary>
     /// Optimization: "All"
@@ -25,19 +25,19 @@ public abstract class Contract_ErrorNullable : Neo.SmartContract.Testing.SmartCo
     /// Unsafe method
     /// </summary>
     [DisplayName("testBigInteger")]
-    public abstract void TestBigInteger(IList<object>? a);
+    public abstract void TestBigInteger(object? a = null);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("testBool")]
-    public abstract void TestBool(IList<object>? a);
+    public abstract void TestBool(object? a = null);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("testByte")]
-    public abstract void TestByte(IList<object>? a);
+    public abstract void TestByte(object? a = null);
 
     /// <summary>
     /// Unsafe method
@@ -49,19 +49,19 @@ public abstract class Contract_ErrorNullable : Neo.SmartContract.Testing.SmartCo
     /// Unsafe method
     /// </summary>
     [DisplayName("testChar")]
-    public abstract void TestChar(IList<object>? a);
+    public abstract void TestChar(object? a = null);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("testInt")]
-    public abstract void TestInt(IList<object>? a);
+    public abstract void TestInt(object? a = null);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("testLong")]
-    public abstract void TestLong(IList<object>? a);
+    public abstract void TestLong(object? a = null);
 
     /// <summary>
     /// Unsafe method
@@ -73,13 +73,13 @@ public abstract class Contract_ErrorNullable : Neo.SmartContract.Testing.SmartCo
     /// Unsafe method
     /// </summary>
     [DisplayName("testSByte")]
-    public abstract void TestSByte(IList<object>? a);
+    public abstract void TestSByte(object? a = null);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("testShort")]
-    public abstract void TestShort(IList<object>? a);
+    public abstract void TestShort(object? a = null);
 
     /// <summary>
     /// Unsafe method
@@ -91,7 +91,7 @@ public abstract class Contract_ErrorNullable : Neo.SmartContract.Testing.SmartCo
     /// Unsafe method
     /// </summary>
     [DisplayName("testUInt")]
-    public abstract void TestUInt(IList<object>? a);
+    public abstract void TestUInt(object? a = null);
 
     /// <summary>
     /// Unsafe method
@@ -121,13 +121,13 @@ public abstract class Contract_ErrorNullable : Neo.SmartContract.Testing.SmartCo
     /// Unsafe method
     /// </summary>
     [DisplayName("testULong")]
-    public abstract void TestULong(IList<object>? a);
+    public abstract void TestULong(object? a = null);
 
     /// <summary>
     /// Unsafe method
     /// </summary>
     [DisplayName("testUShort")]
-    public abstract void TestUShort(IList<object>? a);
+    public abstract void TestUShort(object? a = null);
 
     #endregion
 
