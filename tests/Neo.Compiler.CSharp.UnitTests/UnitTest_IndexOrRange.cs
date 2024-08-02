59b06563d36dae3dff9774076d5077c84e91819b
Jim8y <jimmy@r3e.network>
2024-08-02 18:34:32 +0800

    update test

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_IndexOrRange.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_IndexOrRange.cs
@@ -20,25 +20,24 @@ namespace Neo.Compiler.CSharp.UnitTests
 
             // TODO: check logs
             Assert.AreEqual(18, logs.Count);
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
-            Assert.AreEqual("", logs.Dequeue());
+            Assert.AreEqual("10", logs.Dequeue());
+            Assert.AreEqual("3", logs.Dequeue());
+            Assert.AreEqual("8", logs.Dequeue());
+            Assert.AreEqual("2", logs.Dequeue());
+            Assert.AreEqual("2", logs.Dequeue());
+            Assert.AreEqual("7", logs.Dequeue());
+            Assert.AreEqual("3", logs.Dequeue());
+            Assert.AreEqual("2", logs.Dequeue());
+            Assert.AreEqual("1", logs.Dequeue());
+            Assert.AreEqual("123456789", logs.Dequeue());
+            Assert.AreEqual("123", logs.Dequeue());
+            Assert.AreEqual("3456789", logs.Dequeue());
+            Assert.AreEqual("45", logs.Dequeue());
+            Assert.AreEqual("89", logs.Dequeue());
+            Assert.AreEqual("123456", logs.Dequeue());
+            Assert.AreEqual("45", logs.Dequeue());
+            Assert.AreEqual("67", logs.Dequeue());
+            Assert.AreEqual("1", logs.Dequeue());
         }
     }
 }
