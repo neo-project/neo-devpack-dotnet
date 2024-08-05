9f79c7d9f2064a3c3e84a2cac972cb20fb185c37
Shargon <shargon@gmail.com>
2024-08-05 16:29:22 -0700

    [TestEngine] Clean TestCleanup (#1119) * Clean TestCleanup * Rename * clean line * use name * Restore check count * Restore todo * Fix NEP17 * Clean helper

--- tests/Neo.Compiler.CSharp.UnitTests/DebugAndTestBase.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/DebugAndTestBase.cs
@@ -1,5 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Json;
+using Neo.Optimizer;
+using Neo.SmartContract;
+using Neo.SmartContract.Manifest;
 using Neo.SmartContract.Testing;
 using Neo.SmartContract.Testing.TestingStandards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Neo.Compiler.CSharp.UnitTests;
 
 public class DebugAndTestBase<T> : TestBase<T>
@@ -7,6 +16,72 @@ public class DebugAndTestBase<T> : TestBase<T>
 {
     static DebugAndTestBase()
     {
-        TestCleanup.TestInitialize(typeof(T));
+        var context = TestCleanup.TestInitialize(typeof(T));
+        TestSingleContractBasicBlockStartEnd(context!);
+    }
+
+    public static void TestSingleContractBasicBlockStartEnd(CompilationContext result)
+    {
+        try
+        {
+            try
+            {
+                TestSingleContractBasicBlockStartEnd(result.CreateExecutable(), result.CreateManifest(), result.CreateDebugInformation());
+            }
+            catch
+            {
+                Console.WriteLine($"Omited: {result.ContractName}");
+            }
+        }
+        catch
+        {
+            Console.WriteLine($"Error compiling: {result.ContractName}");
+            return;
+        }
+    }
+
+    public static void TestSingleContractBasicBlockStartEnd(NefFile nef, ContractManifest manifest, JToken debugInfo)
+    {
+        var basicBlocks = new ContractInBasicBlocks(nef, manifest, debugInfo);
+
+        // TODO: support CALLA and do not return
+
+        List<VM.Instruction> instructions = basicBlocks.GetScriptInstructions().ToList();
+        (_, _, Dictionary<VM.Instruction, HashSet<VM.Instruction>> jumpTargets) =
+            Neo.Optimizer.JumpTarget.FindAllJumpAndTrySourceToTargets(instructions);
+
+        Dictionary<VM.Instruction, VM.Instruction> nextAddrTable = new();
+        VM.Instruction? prev = null;
+        foreach (VM.Instruction i in instructions)
+        {
+            if (prev != null)
+                nextAddrTable[prev] = i;
+            prev = i;
+        }
+
+        foreach (BasicBlock basicBlock in basicBlocks.basicBlocks)
+        {
+            // Basic block ends with allowed OpCodes only, or the next instruction is a jump target
+            Assert.IsTrue(OpCodeTypes.allowedBasicBlockEnds.Contains(basicBlock.instructions.Last().OpCode) || jumpTargets.ContainsKey(nextAddrTable[basicBlock.instructions.Last()]));
+            // Instructions except the first are not jump targets
+            foreach (VM.Instruction i in basicBlock.instructions.Skip(1))
+                Assert.IsFalse(jumpTargets.ContainsKey(i));
+            // Other instructions in the basic block are not those in allowedBasicBlockEnds
+            foreach (VM.Instruction i in basicBlock.instructions.Take(basicBlock.instructions.Count - 1))
+                Assert.IsFalse(OpCodeTypes.allowedBasicBlockEnds.Contains(i.OpCode));
+        }
+
+        // Each jump target starts a new basic block
+        foreach (VM.Instruction target in jumpTargets.Keys)
+            Assert.IsTrue(basicBlocks.basicBlocksByStartInstruction.ContainsKey(target));
+
+        // Each instruction is included in only 1 basic block
+        HashSet<VM.Instruction> includedInstructions = new();
+        foreach (BasicBlock basicBlock in basicBlocks.basicBlocks)
+            foreach (VM.Instruction instruction in basicBlock.instructions)
+            {
+                Assert.IsFalse(includedInstructions.Contains(instruction));
+                includedInstructions.Add(instruction);
+            }
     }
 }
