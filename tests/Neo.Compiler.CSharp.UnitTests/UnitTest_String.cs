d67f446ba02601489470c6fa8087c02bd4188f1c
Jim8y <jimmy@r3e.network>
2024-08-07 14:30:11 +0800

    add more string methods

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_String.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_String.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.SmartContract.Testing;
 using System.Collections.Generic;
@@ -80,6 +81,39 @@ namespace Neo.Compiler.CSharp.UnitTests
 
             Assert.IsFalse(Contract.TestIsNullOrEmpty("hello world"));
             Assert.AreEqual(1047870, Engine.FeeConsumed.Value);
+        }
+
+        [TestMethod]
+        public void Test_TestEndWith()
+        {
+            Assert.IsTrue(Contract.TestEndWith("hello world"));
+            Assert.AreEqual(1357710, Engine.FeeConsumed.Value);
+
+            Assert.IsFalse(Contract.TestEndWith("hel"));
+            Assert.AreEqual(1049310, Engine.FeeConsumed.Value);
+
+            Assert.IsFalse(Contract.TestEndWith("hello"));
+            Assert.AreEqual(1049310, Engine.FeeConsumed.Value);
+        }
+
+        [TestMethod]
+        public void Test_TestContains()
+        {
+            Assert.IsTrue(Contract.TestContains("hello world"));
+            Assert.AreEqual(2032920, Engine.FeeConsumed.Value);
+
+            Assert.IsFalse(Contract.TestContains("hello"));
+            Assert.AreEqual(2032920, Engine.FeeConsumed.Value);
+        }
+
+        [TestMethod]
+        public void Test_TestIndexOf()
+        {
+            Assert.AreEqual(6, Contract.TestIndexOf("hello world"));
+            Assert.AreEqual(2032530, Engine.FeeConsumed.Value);
+
+            Assert.AreEqual(-1, Contract.TestIndexOf("hello"));
+            Assert.AreEqual(2032530, Engine.FeeConsumed.Value);
         }
     }
 }
