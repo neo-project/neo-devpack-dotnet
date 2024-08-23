d1d5dc05e4ab1ea0a1341f28af351ba9ba53ab6c
Jimmy <jimmy@r3e.network>
2024-08-24 01:44:51 +0800

    [Compiler Fix] fix push integer (#1140)
    * fix push integer
    
    * remove unrelated string method
    
    * remove
    
    * reverse debug change
    
    * Apply suggestions from code review
    
    * fix -1 issue
    
    ---------
    
    Co-authored-by: Shargon <shargon@gmail.com>

---
+++ tests/Neo.Compiler.CSharp.UnitTests/TestingArtifacts/Contract_Stack.cs
@@ -0,0 +1,49 @@
+using Neo.Cryptography.ECC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Neo.SmartContract.Testing;
+
+public abstract class Contract_Stack : Neo.SmartContract.Testing.SmartContract, IContractInfo
+{
+    #region Compiled data
+
+    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_Stack"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""test_Push_Integer"",""parameters"":[{""name"":""value"",""type"":""Integer""}],""returntype"":""Integer"",""offset"":0,""safe"":false},{""name"":""test_Push_Integer_Internal"",""parameters"":[],""returntype"":""Array"",""offset"":5,""safe"":false},{""name"":""test_External"",""parameters"":[],""returntype"":""Array"",""offset"":106,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""nef"":{""optimization"":""All""}}}");
+
+    /// <summary>
+    /// Optimization: "All"
+    /// </summary>
+    public static Neo.SmartContract.NefFile Nef => Neo.IO.Helper.AsSerializable<Neo.SmartContract.NefFile>(Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIFXAAF4QMVKEM9KAf8Az0oAgM9KAH/PSgEAgM9KAf9/z0oC//8AAM9KA/////8AAAAAz0oCAAAAgM9KAv///3/PSgT//////////wAAAAAAAAAAz0oDAAAAAAAAAIDPSgP/////////f89AxUoPz0oCwL3w/89KAwDwWisX////z0CuZFC1"));
+
+    #endregion
+
+    #region Unsafe methods
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    [DisplayName("test_External")]
+    public abstract IList<object>? Test_External();
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    [DisplayName("test_Push_Integer")]
+    public abstract BigInteger? Test_Push_Integer(BigInteger? value);
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    [DisplayName("test_Push_Integer_Internal")]
+    public abstract IList<object>? Test_Push_Integer_Internal();
+
+    #endregion
+
+    #region Constructor for internal use only
+
+    protected Contract_Stack(Neo.SmartContract.Testing.SmartContractInitialize initialize) : base(initialize) { }
+
+    #endregion
+}
---
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Stack.cs
@@ -0,0 +1,100 @@
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.SmartContract.Testing;
+
+namespace Neo.Compiler.CSharp.UnitTests;
+
+[TestClass]
+public class UnitTest_Stack : DebugAndTestBase<Contract_Stack>
+{
+    [TestMethod]
+    public void Test_Push()
+    {
+        // Original tests
+        TestPushValue(sbyte.MinValue);
+        TestPushValue(sbyte.MaxValue);
+        TestPushValue(byte.MaxValue);
+        TestPushValue(byte.MinValue);
+        TestPushValue(short.MaxValue);
+        TestPushValue(short.MinValue);
+        TestPushValue(ushort.MaxValue);
+        TestPushValue(uint.MaxValue);
+        TestPushValue(int.MaxValue);
+        TestPushValue(int.MinValue);
+        TestPushValue(ulong.MaxValue);
+        TestPushValue(ulong.MinValue);
+        TestPushValue(long.MaxValue);
+        TestPushValue(long.MinValue);
+
+        // Edge cases
+        TestPushValue(-1);
+        TestPushValue(0);
+        TestPushValue(16);
+        TestPushValue(17);
+        TestPushValue(-128);
+        TestPushValue(127);
+        TestPushValue(-129);
+        TestPushValue(128);
+        TestPushValue(-32768);
+        TestPushValue(32767);
+        TestPushValue(-32769);
+        TestPushValue(32768);
+        TestPushValue(-2147483648);
+        TestPushValue(2147483647);
+        TestPushValue(BigInteger.Parse("-2147483649"));
+        TestPushValue(BigInteger.Parse("2147483648"));
+        TestPushValue(BigInteger.Parse("9223372036854775807"));
+        TestPushValue(BigInteger.Parse("-9223372036854775808"));
+        TestPushValue(BigInteger.Parse("9223372036854775808"));
+        TestPushValue(BigInteger.Parse("-9223372036854775809"));
+        TestPushValue(BigInteger.Pow(2, 127) - 1);
+        TestPushValue(-BigInteger.Pow(2, 127));
+        TestPushValue(BigInteger.Pow(2, 127));
+        TestPushValue(-BigInteger.Pow(2, 127) - 1);
+        TestPushValue(BigInteger.Pow(2, 255) - 1);
+        TestPushValue(-BigInteger.Pow(2, 255));
+        TestPushValue(255);
+        TestPushValue(256);
+        TestPushValue(-127);
+        TestPushValue(-128);
+        TestPushValue(-129);
+        TestPushValue(251);
+        TestPushValue(257);
+        TestPushValue(65535);
+        TestPushValue(4294967295);
+    }
+
+    private void TestPushValue(BigInteger value)
+    {
+        var result = Contract.Test_Push_Integer(value);
+        Assert.AreEqual(value, result, $"Failed for value: {value}");
+    }
+
+    [TestMethod]
+    public void Test_Push_Internal()
+    {
+        var result = Contract.Test_Push_Integer_Internal();
+        Assert.AreEqual(new BigInteger(byte.MinValue), result[0]);
+        Assert.AreEqual(new BigInteger(byte.MaxValue), result[1]);
+        Assert.AreEqual(new BigInteger(sbyte.MinValue), result[2]);
+        Assert.AreEqual(new BigInteger(sbyte.MaxValue), result[3]);
+        Assert.AreEqual(new BigInteger(short.MinValue), result[4]);
+        Assert.AreEqual(new BigInteger(short.MaxValue), result[5]);
+        Assert.AreEqual(new BigInteger(ushort.MaxValue), result[6]);
+        Assert.AreEqual(new BigInteger(uint.MaxValue), result[7]);
+        Assert.AreEqual(new BigInteger(int.MinValue), result[8]);
+        Assert.AreEqual(new BigInteger(int.MaxValue), result[9]);
+        Assert.AreEqual(new BigInteger(ulong.MaxValue), result[10]);
+        Assert.AreEqual(new BigInteger(long.MinValue), result[11]);
+        Assert.AreEqual(new BigInteger(long.MaxValue), result[12]);
+    }
+
+    [TestMethod]
+    public void Test_External()
+    {
+        var result = Contract.Test_External();
+        Assert.AreEqual(BigInteger.MinusOne, result[0]);
+        Assert.AreEqual(new BigInteger(-1000000), result[1]); // Previous error value 15777216
+        Assert.AreEqual(new BigInteger(-1000000000000), result[2]); // previous error value 280474976710656
+    }
+}
