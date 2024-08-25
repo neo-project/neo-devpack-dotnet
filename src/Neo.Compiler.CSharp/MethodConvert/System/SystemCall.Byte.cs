e03504afe08b775a18ef02257d3739dd0da2ad69
Jimmy <jimmy@r3e.network>
2024-08-25 12:14:56 +0800

    [Compiler Add] add pop count (#1154) * add pop count * Update src/Neo.Compiler.CSharp/MethodConvert/StackHelpers.OpCodes.cs --------- Co-authored-by: Shargon <shargon@gmail.com>

--- src/Neo.Compiler.CSharp/MethodConvert/System/SystemCall.Byte.cs
+++ src/Neo.Compiler.CSharp/MethodConvert/System/SystemCall.Byte.cs
@@ -181,4 +181,38 @@ internal partial class MethodConvert
         methodConvert.Push((BigInteger.One << bitWidth) - 1);  // Push (2^bitWidth - 1) as bitmask
         methodConvert.AddInstruction(OpCode.AND);    // Ensure final result is bitWidth-bit
     }
+
+    private static void HandleBytePopCount(MethodConvert methodConvert, SemanticModel model, IMethodSymbol symbol, ExpressionSyntax? instanceExpression, IReadOnlyList<SyntaxNode>? arguments)
+    {
+        if (instanceExpression is not null)
+            methodConvert.ConvertExpression(model, instanceExpression);
+        if (arguments is not null)
+            methodConvert.PrepareArgumentsForMethod(model, symbol, arguments);
+        // Determine bit width of int
+        var bitWidth = sizeof(byte) * 8;
+
+        // Mask to ensure the value is treated as a 8-bit unsigned integer
+        methodConvert.Push((BigInteger.One << bitWidth) - 1); // 0xFF
+        methodConvert.And(); // value = value & 0xFF
+                             // Initialize count to 0
+        methodConvert.Push(0); // value count
+        methodConvert.Swap(); // count value
+        // Loop to count the number of 1 bits
+        JumpTarget loopStart = new();
+        JumpTarget endLoop = new();
+        loopStart.Instruction = methodConvert.Dup(); // count value value
+        methodConvert.Push0(); // count value value 0
+        methodConvert.Jump(OpCode.JMPEQ, endLoop); // count value
+        methodConvert.Dup(); // count value value
+        methodConvert.Push1(); // count value value 1
+        methodConvert.And(); // count value (value & 1)
+        methodConvert.Rot(); // value (value & 1) count
+        methodConvert.Add(); // value count += (value & 1)
+        methodConvert.Swap(); // count value
+        methodConvert.Push1(); // count value 1
+        methodConvert.ShR(); // count value >>= 1
+        methodConvert.Jump(OpCode.JMP, loopStart);
+
+        endLoop.Instruction = methodConvert.Drop(); // Drop the remaining value
+    }
 }
