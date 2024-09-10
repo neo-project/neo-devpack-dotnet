8becc29cbc5659123951c489fd1193e835024956
Shargon <shargon@gmail.com>
2024-09-10 06:45:00 -0700

    Add IsValidAndNotZero (#1164)

--- tests/Neo.SmartContract.Framework.UnitTests/UIntTest.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/UIntTest.cs
@@ -11,12 +11,20 @@ namespace Neo.SmartContract.Framework.UnitTests
         {
             Assert.IsTrue(Contract.IsZeroUInt256(UInt256.Zero));
             AssertGasConsumed(1047510);
+            Assert.IsFalse(Contract.IsValidAndNotZeroUInt256(UInt256.Zero));
+            AssertGasConsumed(1065900);
             Assert.IsTrue(Contract.IsZeroUInt160(UInt160.Zero));
             AssertGasConsumed(1047510);
+            Assert.IsFalse(Contract.IsValidAndNotZeroUInt160(UInt160.Zero));
+            AssertGasConsumed(1065900);
             Assert.IsFalse(Contract.IsZeroUInt256(UInt256.Parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01")));
             AssertGasConsumed(1047510);
+            Assert.IsTrue(Contract.IsValidAndNotZeroUInt256(UInt256.Parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff01")));
+            AssertGasConsumed(1065900);
             Assert.IsFalse(Contract.IsZeroUInt160(UInt160.Parse("01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4")));
             AssertGasConsumed(1047510);
+            Assert.IsTrue(Contract.IsValidAndNotZeroUInt160(UInt160.Parse("01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4")));
+            AssertGasConsumed(1065900);
             Assert.AreEqual("Nas9CRigvY94DyqA59HiBZNrgWHRsgrUgt", Contract.ToAddress(UInt160.Parse("01ff00ff00ff00ff00ff00ff00ff00ff00ff00a4")));
             AssertGasConsumed(4592490);
         }
