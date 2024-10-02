9eba16fb7640beadf2c0e25681cb580c3940cbed
Hecate2 <2474101468@qq.com>
2024-10-02 22:32:39 +0800

    test basic block nextBlock in if return

---
+++ tests/Neo.Compiler.CSharp.UnitTests/Optimizer/BasicBlockTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Optimizer;
+using Neo.SmartContract.Testing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo.Compiler.CSharp.UnitTests.Optimizer
+{
+    [TestClass]
+    public class BasicBlockTests
+    {
+        [TestMethod]
+        public void Test_BasicBlockIfReturn()
+        {
+            //public static BigInteger Factorial(BigInteger a)
+            //{
+            //    ExecutionEngine.Assert(a >= 0, "Minus number not supported");
+            //    if (a >= 2) return a * Factorial(a - 1);
+            //    return 1;
+            //}
+            ContractInBasicBlocks contract = new(Contract_Recursion.Nef, Contract_Recursion.Manifest);
+            List<BasicBlock> blocks = contract.sortedBasicBlocks;
+            Assert.AreEqual(blocks[0].nextBlock, blocks[1]);
+            Assert.AreEqual(blocks[0].jumpTargetBlocks.Count, 1);
+            Assert.AreEqual(blocks[0].jumpTargetBlocks.First(), blocks[3]);
+            Assert.AreEqual(blocks[0].instructions.Last().OpCode, VM.OpCode.JMPIFNOT);
+            Assert.AreEqual(blocks[1].instructions.Last().OpCode, VM.OpCode.CALL);
+            Assert.AreEqual(blocks[2].nextBlock, null);
+            Assert.AreEqual(blocks[2].instructions.Last().OpCode, VM.OpCode.RET);
+        }
+    }
+}
