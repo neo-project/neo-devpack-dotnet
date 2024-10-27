33ae8d4319644275a947bc020bdbe9787f0a6e7a
Jim8y <jimmy@r3e.network>
2024-10-27 15:05:34 +0800

    update last gas

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_UIntTypes.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_UIntTypes.cs
@@ -25,7 +25,7 @@ namespace Neo.Compiler.CSharp.UnitTests
             Assert.IsFalse(Contract.ValidateAddress(InvalidUInt160.InvalidType));
             AssertGasConsumed(1047450);
             Assert.IsFalse(Contract.ValidateAddress(InvalidUInt160.Null));
-            AssertGasConsumed(1047330);
+            AssertGasConsumed(1047450);
             Assert.IsFalse(Contract.ValidateAddress(InvalidUInt160.InvalidType));
             AssertGasConsumed(1047930);
             Assert.IsFalse(Contract.ValidateAddress(InvalidUInt160.InvalidLength));
