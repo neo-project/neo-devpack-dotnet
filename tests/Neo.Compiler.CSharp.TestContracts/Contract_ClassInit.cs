828d772630da219b3ef4f3aeba1a886478f71d69
Hecate2 <2474101468@qq.com>
2024-11-01 10:30:49 +0800

    TryOptimizedObjectCreation (#1207) * TryOptimizedObjectCreation * handle struct * TestGasConsume=true * test initialization with default --------- Co-authored-by: Jimmy <jimmy@r3e.network>

--- tests/Neo.Compiler.CSharp.TestContracts/Contract_ClassInit.cs
+++ tests/Neo.Compiler.CSharp.TestContracts/Contract_ClassInit.cs
@@ -12,11 +12,37 @@ namespace Neo.Compiler.CSharp.TestContracts
         public BigInteger B;
     }
 
+    public class ClassWithDifferentTypes
+    {
+        public bool b;
+        public char c;
+        public int i;
+        public string? s;
+        public IntInit ii;
+        public ClassWithDifferentTypes? cl;
+    }
+
     public class Contract_ClassInit : SmartContract.Framework.SmartContract
     {
         public static IntInit testInitInt()
         {
             return new IntInit();
+        }
+
+        public static ClassWithDifferentTypes TestInitializationExpression()
+        {
+            ClassWithDifferentTypes newCl = new() { s = "s", ii = new(), cl = new() };
+            foreach (ClassWithDifferentTypes cl in new ClassWithDifferentTypes[] { newCl, newCl.cl })
+            {
+                ExecutionEngine.Assert(cl.b == default);
+                ExecutionEngine.Assert(cl.c == default);
+                ExecutionEngine.Assert(cl.i == default);
+            }
+            ExecutionEngine.Assert(newCl.s == "s");
+            ExecutionEngine.Assert(newCl.cl.s == null);
+            ExecutionEngine.Assert(newCl.ii.A == default);
+            ExecutionEngine.Assert(newCl.ii.B == default);
+            return newCl;
         }
     }
 }
