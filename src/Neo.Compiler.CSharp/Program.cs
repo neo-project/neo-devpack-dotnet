2c995da1cc743e5e4f5c927ae7f77919e16b24a1
Hecate2 <2474101468@qq.com>
2024-11-08 11:31:13 +0800

    refactor basic block and re-entrancy analyzer

--- src/Neo.Compiler.CSharp/Program.cs
+++ src/Neo.Compiler.CSharp/Program.cs
@@ -128,7 +128,7 @@ namespace Neo.Compiler
                         File.WriteAllBytes(Path.Combine(directory, filename + ".optimized.nefdbgnfo"), DumpNef.ZipDebugInfo(debugInfo.ToByteArray(true), filename + ".optimized.debug.json"));
                     Console.WriteLine($"Optimization finished.");
                     if (options.SecurityAnalysis)
-                        ReEntrancyAnalyzer.AnalyzeSingleContractReEntrancy(nef, manifest, debugInfo).GetWarningInfo(print: true);
+                        SecurityAnalyzer.SecurityAnalyzer.AnalyzeWithPrint(nef, manifest, debugInfo);
                     return;
                 }
                 else if (extension != ".cs")
@@ -357,7 +357,7 @@ namespace Neo.Compiler
                     Console.WriteLine("Performing security analysis...");
                     try
                     {
-                        ReEntrancyAnalyzer.AnalyzeSingleContractReEntrancy(nef, manifest, debugInfo).GetWarningInfo(print: true);
+                        SecurityAnalyzer.SecurityAnalyzer.AnalyzeWithPrint(nef, manifest, debugInfo);
                     }
                     catch (Exception e) { Console.WriteLine(e); }
                     Console.WriteLine("Finished security analysis.");
