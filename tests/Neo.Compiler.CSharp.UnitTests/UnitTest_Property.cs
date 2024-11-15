c5fe5a4476539c55818bf87f6a10e05262d12a8d
Jim8y <jimmy@r3e.network>
2024-11-15 15:09:17 +0800

    add ut

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Property.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_Property.cs
@@ -17,14 +17,125 @@ namespace Neo.Compiler.CSharp.UnitTests
         public void TestStaticPropertyInc()
         {
             var res = Contract.TestStaticPropertyInc();
-            Assert.AreEqual(3, res);
+            Assert.AreEqual(6, res);
         }
 
         [TestMethod]
         public void TestPropertyInc()
         {
             var res = Contract.TestPropertyInc();
-            Assert.AreEqual(3, res);
+            Assert.AreEqual(6, res);
+        }
+
+        [TestMethod]
+        public void TestStaticPropertyDec()
+        {
+            var res = Contract.TestStaticPropertyDec();
+            Assert.AreEqual(-6, res);
+        }
+
+        [TestMethod]
+        public void TestPropertyDec()
+        {
+            var res = Contract.TestPropertyDec();
+            Assert.AreEqual(-6, res);
+        }
+
+        [TestMethod]
+        public void TestStaticPropertyMul()
+        {
+            var res = Contract.TestStaticPropertyMul();
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestPropertyMul()
+        {
+            var res = Contract.TestPropertyMul();
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestConcatPublicProperties()
+        {
+            var res = Contract.ConcatPublicProperties();
+            Assert.AreEqual("TestInitial", res);
+        }
+
+        [TestMethod]
+        public void TestToggleProtectedProperty()
+        {
+            var res = Contract.ToggleProtectedProperty();
+            Assert.IsTrue(res);
+            res = Contract.ToggleProtectedProperty();
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void TestGetComputedValue()
+        {
+            var res = Contract.GetComputedValue();
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestReset()
+        {
+            Contract.TestPropertyInc();
+            Contract.TestStaticPropertyInc();
+            Contract.SetPublicProperty("Changed");
+            Contract.SetPublicStaticProperty("Changed");
+            Contract.ToggleProtectedProperty();
+
+            Contract.Reset();
+
+            Assert.AreEqual("Test", Contract.PublicProperty());
+            Assert.AreEqual("Initial", Contract.PublicStaticProperty());
+            Assert.AreEqual(0, Contract.TestPropertyInc());
+            Assert.AreEqual(6, Contract.TestStaticPropertyInc());
+            Assert.IsFalse(Contract.ToggleProtectedProperty());
+        }
+
+        [TestMethod]
+        public void TestUninitializedPropertyInc()
+        {
+            var res = Contract.UninitializedPropertyInc();
+            Assert.AreEqual(6, res);
+        }
+
+        [TestMethod]
+        public void TestUninitializedPropertyDec()
+        {
+            var res = Contract.UninitializedPropertyDec();
+            Assert.AreEqual(-6, res);
+        }
+
+        [TestMethod]
+        public void TestUninitializedPropertyMul()
+        {
+            var res = Contract.UninitializedPropertyMul();
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestUninitializedStaticPropertyInc()
+        {
+            var res = Contract.UninitializedStaticPropertyInc();
+            Assert.AreEqual(6, res);
+        }
+
+        [TestMethod]
+        public void TestUninitializedStaticPropertyDec()
+        {
+            var res = Contract.UninitializedStaticPropertyDec();
+            Assert.AreEqual(-6, res);
+        }
+
+        [TestMethod]
+        public void TestUninitializedStaticPropertyMul()
+        {
+            var res = Contract.UninitializedStaticPropertyMul();
+            Assert.AreEqual(0, res);
         }
     }
 }
