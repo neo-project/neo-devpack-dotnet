27fafd3e3ca11ab71c4d81b4e2291eba4fd432d3
Hecate2 <2474101468@qq.com>
2024-12-30 12:42:53 +0800

    CheckWitnessAnalyzer (#1270) * CheckWitnessAnalyzer * UnitTest_CheckWitness --------- Co-authored-by: Jimmy <jimmy@r3e.network>

--- src/Neo.Compiler.CSharp/SecurityAnalyzer/SecurityAnalyzer.cs
+++ src/Neo.Compiler.CSharp/SecurityAnalyzer/SecurityAnalyzer.cs
@@ -11,6 +11,7 @@ namespace Neo.Compiler.SecurityAnalyzer
         {
             ReEntrancyAnalyzer.AnalyzeSingleContractReEntrancy(nef, manifest, debugInfo).GetWarningInfo(print: true);
             WriteInTryAnalzyer.AnalyzeWriteInTry(nef, manifest, debugInfo).GetWarningInfo(print: true);
+            CheckWitnessAnalyzer.AnalyzeCheckWitness(nef, manifest, debugInfo).GetWarningInfo(print: true);
             if (!UpdateAnalzyer.AnalyzeUpdate(nef, manifest, debugInfo))
                 Console.WriteLine("[SEC] This contract cannot be updated, or maybe you used abstract code styles to update it.");
         }
