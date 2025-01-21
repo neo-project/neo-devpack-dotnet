0431391ad8a7d0acc4d8280f36085448c5d63e4f
nan01ab <yjcc201374@outlook.com>
2025-01-21 14:52:43 +0800

    [`Fix`]: `checked(-x)` if x is int or long (#1281)
    * fix: check(-x) if x is int or long
    
    * fix: add more tests
    
    ---------
    
    Co-authored-by: Jimmy <jimmy@r3e.network>

--- tests/Neo.Compiler.CSharp.TestContracts/Contract_Overflow.cs
+++ tests/Neo.Compiler.CSharp.TestContracts/Contract_Overflow.cs
@@ -6,5 +6,20 @@ namespace Neo.Compiler.CSharp.TestContracts
         public static int MulInt(int a, int b) => a * b;
         public static uint AddUInt(uint a, uint b) => a + b;
         public static uint MulUInt(uint a, uint b) => a * b;
+
+        public static int NegateIntChecked(int a) => checked(-a);
+        public static int NegateInt(int a) => -a;
+
+        public static long NegateLongChecked(long a) => checked(-a);
+        public static long NegateLong(long a) => -a;
+
+        public static int NegateShortChecked(short a) => checked(-a);
+        public static int NegateShort(short a) => -a;
+
+        public static int NegateAddInt(int a, int b) => -(a + b);
+        public static int NegateAddIntChecked(int a, int b) => checked(-(a + b));
+
+        public static long NegateAddLong(long a, long b) => -(a + b);
+        public static long NegateAddLongChecked(long a, long b) => checked(-(a + b));
     }
 }
