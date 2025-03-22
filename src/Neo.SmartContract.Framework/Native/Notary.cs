2e676c9c1684a43d3d2603eae9faca130ba90c08
Anna Shaleva <shaleva.ann@nspcc.ru>
2025-03-22 22:40:38 +0300

    Neo.SmartContract.Framework: add native Notary contract API (#1300)
    * Neo.SmartContract.Framework: add native Notary contract API
    
    Ref. https://github.com/neo-project/neo/pull/3178.
    
    Ported from https://github.com/neo-project/neo-devpack-dotnet/pull/556
    with all necessary updates.
    
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>
    
    * Update src/Neo.SmartContract.Framework/Native/Notary.cs
    
    Co-authored-by: Will <201105916+Wi1l-B0t@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Anna Shaleva <shaleva.ann@nspcc.ru>
    Co-authored-by: Jimmy <jimmy@r3e.network>
    Co-authored-by: Will <201105916+Wi1l-B0t@users.noreply.github.com>
    Co-authored-by: Shargon <shargon@gmail.com>

---
+++ src/Neo.SmartContract.Framework/Native/Notary.cs
@@ -0,0 +1,33 @@
+// Copyright (C) 2015-2025 The Neo Project.
+//
+// Ledger.cs file belongs to the neo project and is free
+// software distributed under the MIT software license, see the
+// accompanying file LICENSE in the main directory of the
+// repository or http://www.opensource.org/licenses/mit-license.php
+// for more details.
+//
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
+#pragma warning disable CS0626
+
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Services;
+using System.Numerics;
+
+namespace Neo.SmartContract.Framework.Native
+{
+    [Contract("0xc1e14f19c3e60d0b9244d06dd7ba9b113135ec3b")]
+    public class Notary
+    {
+        [ContractHash]
+        public static extern UInt160 Hash { get; }
+        public static extern bool LockDepositUntil(UInt160 account, uint till);
+        public static extern bool Withdraw(UInt160 from, UInt160 to);
+        public static extern BigInteger BalanceOf(UInt160 account);
+        public static extern uint ExpirationOf(UInt160 account);
+        public static extern bool Verify(byte[] sig);
+        public static extern uint GetMaxNotValidBeforeDelta();
+        public static extern void SetMaxNotValidBeforeDelta(uint value);
+    }
+}
