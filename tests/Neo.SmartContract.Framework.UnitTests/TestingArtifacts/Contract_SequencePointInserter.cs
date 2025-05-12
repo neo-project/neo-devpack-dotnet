8fe84333efbce744157db9af2dd7abe004c3410e
Fernando Diaz Toledano <shargon@gmail.com>
2025-05-12 13:24:08 +0200

    Update artifacts

--- tests/Neo.SmartContract.Framework.UnitTests/TestingArtifacts/Contract_SequencePointInserter.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestingArtifacts/Contract_SequencePointInserter.cs
@@ -11,7 +11,7 @@ public abstract class Contract_SequencePointInserter(Neo.SmartContract.Testing.S
 {
     #region Compiled data
 
-    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_SequencePointInserter"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""test"",""parameters"":[{""name"":""a"",""type"":""Integer""}],""returntype"":""Integer"",""offset"":0,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Version"":""3.7.4"",""nef"":{""optimization"":""All""}}}");
+    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_SequencePointInserter"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""test"",""parameters"":[{""name"":""a"",""type"":""Integer""}],""returntype"":""Integer"",""offset"":0,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Version"":""3.8.1"",""nef"":{""optimization"":""All""}}}");
 
     /// <summary>
     /// Optimization: "All"
