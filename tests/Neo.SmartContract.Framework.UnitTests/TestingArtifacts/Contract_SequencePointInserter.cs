7e1b19e39f0f640e1094943b69fbc4f450893efa
Jimmy <jimmy@r3e.network>
2025-05-25 23:08:16 +0800

    Update test artifacts with correct manifest data
    - Contract_SequencePointInserter.cs: Updated manifest with proper permissions and extra fields
    - Contract_Storage.cs: Updated manifest with proper permissions and extra fields
    
    These artifacts were regenerated after fixing the CompilationEngine target framework
    to net9.0, which now properly compiles test contracts with all expected manifest
    attributes including Author, Description, and Sourcecode.
    
    This completes the fix for PR #1316 CI compatibility issues with
    Neo.SmartContract.Framework 3.8.1.

--- tests/Neo.SmartContract.Framework.UnitTests/TestingArtifacts/Contract_SequencePointInserter.cs
+++ tests/Neo.SmartContract.Framework.UnitTests/TestingArtifacts/Contract_SequencePointInserter.cs
@@ -11,7 +11,7 @@ public abstract class Contract_SequencePointInserter(Neo.SmartContract.Testing.S
 {
     #region Compiled data
 
-    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_SequencePointInserter"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""test"",""parameters"":[{""name"":""a"",""type"":""Integer""}],""returntype"":""Integer"",""offset"":0,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Version"":""3.8.1"",""nef"":{""optimization"":""All""}}}");
+    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_SequencePointInserter"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""test"",""parameters"":[{""name"":""a"",""type"":""Integer""}],""returntype"":""Integer"",""offset"":0,""safe"":false}],""events"":[]},""permissions"":[{""contract"":""*"",""methods"":""*""}],""trusts"":[],""extra"":{""Author"":""core-dev"",""Version"":""0.0.1"",""Description"":""Neo Framework Test Contract"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/tests/Neo.SmartContract.Framework.TestContracts"",""nef"":{""optimization"":""All""}}}");
 
     /// <summary>
     /// Optimization: "All"
