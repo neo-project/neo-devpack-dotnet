a57ff397fc27e89c55d4b1cf507cf362b816b93c
Fernando Diaz Toledano <shargon@gmail.com>
2025-06-01 22:40:06 +0200

    Clean big

--- src/Neo.Compiler.CSharp/MethodConvert/System/SystemCall.Long.cs
+++ src/Neo.Compiler.CSharp/MethodConvert/System/SystemCall.Long.cs
@@ -40,10 +40,10 @@ internal partial class MethodConvert
         JumpTarget endTarget = new();
         methodConvert.CallContractMethod(NativeContract.StdLib.Hash, "atoi", 1, true);
         methodConvert.Dup();                                        // Duplicate result for range check
-        methodConvert.Within(long.MinValue, new BigInteger(long.MaxValue)); // Check if value is within long range
-        methodConvert.Jump(OpCode.JMPIF, endTarget);               // Jump if within range
-        methodConvert.Throw();                                     // Throw if out of range
-        endTarget.Instruction = methodConvert.Nop();               // End target
+        methodConvert.Within(long.MinValue, long.MaxValue);         // Check if value is within long range
+        methodConvert.Jump(OpCode.JMPIF, endTarget);                // Jump if within range
+        methodConvert.Throw();                                      // Throw if out of range
+        endTarget.Instruction = methodConvert.Nop();                // End target
     }
 
     /// <summary>
@@ -134,10 +134,10 @@ internal partial class MethodConvert
         if (arguments is not null)
             methodConvert.PrepareArgumentsForMethod(model, symbol, arguments);
         methodConvert.Dup();                                        // Duplicate value for range check
-        methodConvert.Within(long.MinValue, new BigInteger(long.MaxValue)); // Check if value is within long range
-        methodConvert.Jump(OpCode.JMPIF, endTarget);               // Jump if within range
-        methodConvert.Throw();                                     // Throw if out of range
-        endTarget.Instruction = methodConvert.Nop();               // End target
+        methodConvert.Within(long.MinValue, long.MaxValue);         // Check if value is within long range
+        methodConvert.Jump(OpCode.JMPIF, endTarget);                // Jump if within range
+        methodConvert.Throw();                                      // Throw if out of range
+        endTarget.Instruction = methodConvert.Nop();                // End target
     }
 
     /// <summary>
