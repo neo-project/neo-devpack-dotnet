2d521d3d382e448b0a9b39f9160f992517acd20f
Wi1l-B0t <201105916+Wi1l-B0t@users.noreply.github.com>
2025-12-13 14:52:25 +0800

    Add: RecoverSecp256K1 and VerifyWithEd25519 for Framework.Native.CryptLib

--- src/Neo.SmartContract.Framework/Native/CryptoLib.cs
+++ src/Neo.SmartContract.Framework/Native/CryptoLib.cs
@@ -22,6 +22,19 @@ namespace Neo.SmartContract.Framework.Native
         [ContractHash]
         public static extern UInt160 Hash { get; }
 
+        /// <summary>
+        /// Recovers the public key from a secp256k1 signature in bytes format.
+        /// Available from HF_Echidna.
+        /// </summary>
+        /// <param name="messageHash">The hash of the message that was signed. It cannot be null.</param>
+        /// <param name="signature">
+        /// The signature in raw bytes format if it's length is 64.
+        /// Otherwise, it's the 65-byte signature in format: r[32] + s[32] + v[1]. 64-bytes for eip-2098, where v must be 27 or 28.
+        /// It cannot be null.
+        /// </param>
+        /// <returns>The recovered public key in compressed format, or null if recovery fails.</returns>
+        public static extern ByteString RecoverSecp256K1(ByteString messageHash, ByteString signature);
+
         public static extern ByteString Sha256(ByteString value);
 
         public static extern ByteString Ripemd160(ByteString value);
@@ -34,5 +47,15 @@ namespace Neo.SmartContract.Framework.Native
         public static extern bool VerifyWithECDsa(ByteString message, ECPoint pubkey, ByteString signature, NamedCurve curve);
 
         public static extern bool VerifyWithECDsa(ByteString message, ECPoint pubkey, ByteString signature, NamedCurveHash curveHash);
+
+        /// <summary>
+        /// Verifies that a digital signature is appropriate for the provided key and message using the Ed25519 algorithm.
+        /// Available from HF_Echidna.
+        /// </summary>
+        /// <param name="message">The signed message. It cannot be null.</param>
+        /// <param name="pubkey">The 32-bytes length Ed25519 public key to be used. It cannot be null.</param>
+        /// <param name="signature">The 64-bytes length signature to be verified. It cannot be null.</param>
+        /// <returns>true if the signature is valid; otherwise, false.</returns>
+        public static extern bool VerifyWithEd25519(ByteString message, ByteString pubkey, ByteString signature);
     }
 }
