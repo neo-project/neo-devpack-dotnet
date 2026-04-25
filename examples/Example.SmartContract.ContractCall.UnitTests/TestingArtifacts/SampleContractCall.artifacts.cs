94d3bcc13caa59ad54a9237408c4866e7992340c
Jimmy <jimmy@r3e.network>
2026-04-25 14:04:42 +0800

    Match NEP-17 payment data ABI in the contract call example
    NEP-17 payment data is an arbitrary stack item, but the example contract
    accepted only BigInteger. Use object data and keep the sample guard for
    the integer value the test expects.
    
    Constraint: Preserve the example test behavior while correcting the manifest ABI
    Confidence: high
    Scope-risk: narrow
    Tested: dotnet test examples/Example.SmartContract.ContractCall.UnitTests/Example.SmartContract.ContractCall.UnitTests.csproj -v minimal

--- examples/Example.SmartContract.ContractCall.UnitTests/TestingArtifacts/SampleContractCall.artifacts.cs
+++ examples/Example.SmartContract.ContractCall.UnitTests/TestingArtifacts/SampleContractCall.artifacts.cs
@@ -5,18 +5,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Numerics;
 
+#pragma warning disable CS0067
+
 namespace Neo.SmartContract.Testing;
 
 public abstract class SampleContractCall(Neo.SmartContract.Testing.SmartContractInitialize initialize) : Neo.SmartContract.Testing.SmartContract(initialize), IContractInfo
 {
     #region Compiled data
 
-    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""SampleContractCall"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""onNEP17Payment"",""parameters"":[{""name"":""from"",""type"":""Hash160""},{""name"":""amount"",""type"":""Integer""},{""name"":""data"",""type"":""Integer""}],""returntype"":""Void"",""offset"":0,""safe"":false},{""name"":""_initialize"",""parameters"":[],""returntype"":""Void"",""offset"":91,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Author"":""core-dev"",""Version"":""0.0.1"",""Description"":""A sample contract to demonstrate how to call a contract"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/examples/"",""nef"":{""optimization"":""Basic""}}}");
+    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""SampleContractCall"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""onNEP17Payment"",""parameters"":[{""name"":""from"",""type"":""Hash160""},{""name"":""amount"",""type"":""Integer""},{""name"":""data"",""type"":""Any""}],""returntype"":""Void"",""offset"":0,""safe"":false},{""name"":""_initialize"",""parameters"":[],""returntype"":""Void"",""offset"":107,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Author"":""core-dev"",""Version"":""0.0.1"",""Description"":""A sample contract to demonstrate how to call a contract"",""Sourcecode"":""https://github.com/neo-project/neo-devpack-dotnet/tree/master/examples/"",""nef"":{""optimization"":""Basic""}}}");
 
     /// <summary>
     /// Optimization: "Basic"
     /// </summary>
-    public static Neo.SmartContract.NefFile Nef => Convert.FromBase64String(@"TkVGM05lby5Db21waWxlci5DU2hhcnAgMy45LjArNDIzNzFmNWY0ZTBiZTI4N2ExZmYyOGYzNThhYjI0NmY1YjQuLi4AAAAAAHVXAwN6AHuzqiYEIj5B2/6odHBBOVNuPHFoEcAVDAliYWxhbmNlT2ZpQWJ9W1JyamloE8AfDAtkdW1teU1ldGhvZFhBYn1bUkVAQdv+qHRAQTlTbjxAQWJ9W1JAVgEMFF6QEGiCvFczbXZ7FdXtLpwFPqgTYEB9MYTh").AsSerializable<Neo.SmartContract.NefFile>();
+    public static Neo.SmartContract.NefFile Nef => Convert.FromBase64String(@"TkVGM05lby5Db21waWxlci5DU2hhcnAgMy45LjErMzY2NWY4MzEyYmJlZWQxZGY5NjRjNTM2M2QxNjZjOWQ2NGYuLi4AAAAAAIVXBAN6cGjZIUomBGhxqiYFCCIHaQB7s6omBCI+Qdv+qHRxQTlTbjxyaRHAFQwJYmFsYW5jZU9makFifVtSc2tqaRPAHwwLZHVtbXlNZXRob2RYQWJ9W1JFQEHb/qh0QEE5U248QEFifVtSQFYBDBRekBBogrxXM212exXV7S6cBT6oE2BA2uovsA==").AsSerializable<Neo.SmartContract.NefFile>();
 
     #endregion
 
@@ -26,7 +28,7 @@ public abstract class SampleContractCall(Neo.SmartContract.Testing.SmartContract
     /// Unsafe method
     /// </summary>
     [DisplayName("onNEP17Payment")]
-    public abstract void OnNEP17Payment(UInt160? from, BigInteger? amount, BigInteger? data);
+    public abstract void OnNEP17Payment(UInt160? from, BigInteger? amount, object? data = null);
 
     #endregion
 }
