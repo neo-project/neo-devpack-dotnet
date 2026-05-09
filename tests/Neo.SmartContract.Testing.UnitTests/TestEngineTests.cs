5974dbe19844e8f73e3e317332ee47d5900840af
Jimmy <jimmy@r3e.network>
2026-05-09 11:28:20 +0800

    Make testing syscall registration idempotent

--- tests/Neo.SmartContract.Testing.UnitTests/TestEngineTests.cs
+++ tests/Neo.SmartContract.Testing.UnitTests/TestEngineTests.cs
@@ -22,6 +22,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Numerics;
+using System.Reflection;
 
 namespace Neo.SmartContract.Testing.UnitTests
 {
@@ -96,6 +97,17 @@ namespace Neo.SmartContract.Testing.UnitTests
 
             engine.OnGetCallingScriptHash = (current, expected) => UInt160.Parse("0x0000000000000000000000000000000000000001");
             Assert.AreEqual("0x0000000000000000000000000000000000000001", engine.Execute(script).ConvertTo(typeof(UInt160))!.ToString());
+        }
+
+        [TestMethod]
+        public void TestingSyscallRegistrationIsIdempotent()
+        {
+            var engineType = typeof(TestEngine).Assembly.GetType("Neo.SmartContract.Testing.TestingApplicationEngine")!;
+            var registerMethod = engineType.GetMethod("RegisterTestingSyscall", BindingFlags.Static | BindingFlags.NonPublic);
+
+            Assert.IsNotNull(registerMethod);
+            registerMethod.Invoke(null, null);
+            registerMethod.Invoke(null, null);
         }
 
         [TestMethod]
