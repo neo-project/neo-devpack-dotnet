50a1ce24d979a92faaf5025d6da0933ae60c6e76
Jim8y <jimmy@r3e.network>
2026-07-11 11:53:59 +0800

    Deprecate EntryPointsByCallA

--- src/Neo.Compiler.CSharp/ControlFlow/EntryPoint.cs
+++ src/Neo.Compiler.CSharp/ControlFlow/EntryPoint.cs
@@ -13,6 +13,7 @@ using Neo.Optimizer;
 using Neo.SmartContract;
 using Neo.SmartContract.Manifest;
 using Neo.VM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using VmInstruction = Neo.VM.Instruction;
@@ -60,6 +61,7 @@ namespace Neo.Compiler.ControlFlow
         /// </summary>
         /// <param name="nef">The NEF file.</param>
         /// <returns>A dictionary containing entry points.</returns>
+        [Obsolete("Use AllEntryPoints to include method and PUSHA entry points.")]
         public static Dictionary<int, EntryType> EntryPointsByCallA(NefFile nef)
         {
             Script script = nef.Script;
