174898bcc34a90db52693fbba33b15bbb2e05e7a
Jim8y <jimmy@r3e.network>
2026-07-14 06:43:42 +0800

    Validate supported enum overloads

--- src/Neo.SmartContract.Analyzer/EnumMethodsUsageAnalyzer.cs
+++ src/Neo.SmartContract.Analyzer/EnumMethodsUsageAnalyzer.cs
@@ -14,7 +14,6 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
-using System.Linq;
 
 namespace Neo.SmartContract.Analyzer
 {
@@ -22,11 +21,6 @@ namespace Neo.SmartContract.Analyzer
     public class EnumMethodsUsageAnalyzer : DiagnosticAnalyzer
     {
         public const string DiagnosticId = "NC4025";
-
-        private readonly string[] _unsupportedEnumMethods = {
-            "Format",
-            "GetUnderlyingType"
-        };
 
         private static readonly DiagnosticDescriptor Rule = new(
             DiagnosticId,
@@ -50,16 +44,136 @@ namespace Neo.SmartContract.Analyzer
         {
             if (context.Node is not InvocationExpressionSyntax invocationExpression) return;
 
-            var methodSymbol = context.SemanticModel.GetSymbolInfo(invocationExpression).Symbol as IMethodSymbol;
-
-            if (methodSymbol is not { ContainingType.SpecialType: SpecialType.System_Enum } ||
-                !_unsupportedEnumMethods.Contains(methodSymbol.Name)) return;
+            if (context.SemanticModel.GetSymbolInfo(invocationExpression).Symbol is not IMethodSymbol methodSymbol ||
+                methodSymbol.ContainingType.SpecialType != SpecialType.System_Enum ||
+                IsSupportedEnumMethod(methodSymbol))
+                return;
 
             var diagnostic = Diagnostic.Create(Rule,
                 invocationExpression.GetLocation(),
                 methodSymbol.Name);
 
             context.ReportDiagnostic(diagnostic);
+        }
+
+        private static bool IsSupportedEnumMethod(IMethodSymbol method)
+        {
+            if (!method.IsStatic)
+            {
+                return method.Name switch
+                {
+                    "ToString" => method.Parameters.Length == 0,
+                    "HasFlag" => IsSpecialParameter(method, 0, SpecialType.System_Enum) &&
+                                 method.Parameters.Length == 1,
+                    _ => false
+                };
+            }
+
+            if (method.IsGenericMethod)
+                return IsSupportedGenericMethod(method);
+
+            return method.Name switch
+            {
+                "Parse" => IsParseSignature(method),
+                "TryParse" => IsTryParseSignature(method),
+                "GetNames" or "GetValues" => method.Parameters.Length == 1 && IsSystemTypeParameter(method, 0),
+                "IsDefined" => method.Parameters.Length == 2 &&
+                               IsSystemTypeParameter(method, 0) &&
+                               (IsSpecialParameter(method, 1, SpecialType.System_Object) ||
+                                IsSpecialParameter(method, 1, SpecialType.System_String)),
+                "GetName" => method.Parameters.Length == 2 &&
+                             IsSystemTypeParameter(method, 0) &&
+                             IsSpecialParameter(method, 1, SpecialType.System_Object),
+                _ => false
+            };
+        }
+
+        private static bool IsSupportedGenericMethod(IMethodSymbol method)
+        {
+            if (method.TypeArguments.Length != 1)
+                return false;
+
+            var enumType = method.TypeArguments[0];
+            return method.Name switch
+            {
+                "Parse" => IsGenericParseSignature(method),
+                "TryParse" => IsGenericTryParseSignature(method, enumType),
+                "GetNames" or "GetValues" => method.Parameters.Length == 0,
+                "GetName" => method.Parameters.Length == 1 &&
+                             IsParameter(method, 0, enumType),
+                _ => false
+            };
+        }
+
+        private static bool IsParseSignature(IMethodSymbol method)
+        {
+            return method.Parameters.Length is 2 or 3 &&
+                   IsSystemTypeParameter(method, 0) &&
+                   IsSpecialParameter(method, 1, SpecialType.System_String) &&
+                   (method.Parameters.Length == 2 ||
+                    IsSpecialParameter(method, 2, SpecialType.System_Boolean));
+        }
+
+        private static bool IsTryParseSignature(IMethodSymbol method)
+        {
+            return method.Parameters.Length is 3 or 4 &&
+                   IsSystemTypeParameter(method, 0) &&
+                   IsSpecialParameter(method, 1, SpecialType.System_String) &&
+                   (method.Parameters.Length == 3 ||
+                    IsSpecialParameter(method, 2, SpecialType.System_Boolean)) &&
+                   IsSpecialParameter(
+                       method,
+                       method.Parameters.Length - 1,
+                       SpecialType.System_Object,
+                       RefKind.Out);
+        }
+
+        private static bool IsGenericParseSignature(IMethodSymbol method)
+        {
+            return method.Parameters.Length is 1 or 2 &&
+                   IsSpecialParameter(method, 0, SpecialType.System_String) &&
+                   (method.Parameters.Length == 1 ||
+                    IsSpecialParameter(method, 1, SpecialType.System_Boolean));
+        }
+
+        private static bool IsGenericTryParseSignature(IMethodSymbol method, ITypeSymbol enumType)
+        {
+            return method.Parameters.Length is 2 or 3 &&
+                   IsSpecialParameter(method, 0, SpecialType.System_String) &&
+                   (method.Parameters.Length == 2 ||
+                    IsSpecialParameter(method, 1, SpecialType.System_Boolean)) &&
+                   IsParameter(method, method.Parameters.Length - 1, enumType, RefKind.Out);
+        }
+
+        private static bool IsSystemTypeParameter(IMethodSymbol method, int index)
+        {
+            if (index >= method.Parameters.Length || method.Parameters[index].RefKind != RefKind.None)
+                return false;
+
+            return method.Parameters[index].Type is INamedTypeSymbol { Name: "Type" } type &&
+                   type.ContainingNamespace.ToDisplayString() == "System";
+        }
+
+        private static bool IsSpecialParameter(
+            IMethodSymbol method,
+            int index,
+            SpecialType type,
+            RefKind refKind = RefKind.None)
+        {
+            return index < method.Parameters.Length &&
+                   method.Parameters[index].RefKind == refKind &&
+                   method.Parameters[index].Type.SpecialType == type;
+        }
+
+        private static bool IsParameter(
+            IMethodSymbol method,
+            int index,
+            ITypeSymbol type,
+            RefKind refKind = RefKind.None)
+        {
+            return index < method.Parameters.Length &&
+                   method.Parameters[index].RefKind == refKind &&
+                   SymbolEqualityComparer.Default.Equals(method.Parameters[index].Type, type);
         }
     }
 }
