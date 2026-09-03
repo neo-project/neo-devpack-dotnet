93dc7961fbe5b34b3fa00da51c5bfe1cc6202228
Wi1l-B0t <201105916+Wi1l-B0t@users.noreply.github.com>
2026-09-04 00:16:41 +0800

    Fix:  BigInteger.ToByteArray should return [0x00] if zero

--- tests/Neo.Compiler.CSharp.UnitTests/TestingArtifacts/Contract_TypeConvert.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/TestingArtifacts/Contract_TypeConvert.cs
@@ -13,12 +13,12 @@ public abstract class Contract_TypeConvert(Neo.SmartContract.Testing.SmartContra
 {
     #region Compiled data
 
-    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_TypeConvert"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""testType"",""parameters"":[],""returntype"":""Any"",""offset"":0,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Version"":""3.10.1"",""nef"":{""optimization"":""All""}}}");
+    public static Neo.SmartContract.Manifest.ContractManifest Manifest => Neo.SmartContract.Manifest.ContractManifest.Parse(@"{""name"":""Contract_TypeConvert"",""groups"":[],""features"":{},""supportedstandards"":[],""abi"":{""methods"":[{""name"":""testType"",""parameters"":[],""returntype"":""Any"",""offset"":0,""safe"":false},{""name"":""intToBytes"",""parameters"":[{""name"":""value"",""type"":""Integer""}],""returntype"":""ByteArray"",""offset"":98,""safe"":false}],""events"":[]},""permissions"":[],""trusts"":[],""extra"":{""Version"":""3.10.1"",""nef"":{""optimization"":""All""}}}");
 
     /// <summary>
     /// Optimization: "All"
     /// </summary>
-    public static Neo.SmartContract.NefFile Nef => Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFJXCQAQcGjbMHEScmrbMHMMAQPbMHRs2yF1EIh2btshdwcYxAB3CG8IEGjQbwgRadBvCBJq0G8IE2vQbwgUbNBvCBVt0G8IFm7QbwgXbwfQbwhAp9uR9g==").AsSerializable<Neo.SmartContract.NefFile>();
+    public static Neo.SmartContract.NefFile Nef => Convert.FromBase64String(@"TkVGM1Rlc3RpbmdFbmdpbmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHFXCQAQcGhKJAZFDAEASsqNcRJyakokBkUMAQBKyo1zDAED2zB0bNshdRCIdm7bIXcHGMQAdwhvCBBo0G8IEWnQbwgSatBvCBNr0G8IFGzQbwgVbdBvCBZu0G8IF28H0G8IQFcAAXhKJAZFDAEASsqNQFcgCMI=").AsSerializable<Neo.SmartContract.NefFile>();
 
     #endregion
 
@@ -28,17 +28,48 @@ public abstract class Contract_TypeConvert(Neo.SmartContract.Testing.SmartContra
     /// Unsafe method
     /// </summary>
     /// <remarks>
-    /// Script: VwkAEHBo2zBxEnJq2zBzDAED2zB0bNshdRCIdm7bIXcHGMQAdwhvCBBo0G8IEWnQbwgSatBvCBNr0G8IFGzQbwgVbdBvCBZu0G8IF28H0G8IQA==
+    /// Script: VwABeEokBkUMAQBKyo1A
+    /// INITSLOT 0001 [64 datoshi]
+    /// LDARG0 [2 datoshi]
+    /// DUP [2 datoshi]
+    /// JMPIF 06 [2 datoshi]
+    /// DROP [2 datoshi]
+    /// PUSHDATA1 00 [8 datoshi]
+    /// DUP [2 datoshi]
+    /// SIZE [4 datoshi]
+    /// LEFT [2048 datoshi]
+    /// RET [0 datoshi]
+    /// </remarks>
+    [DisplayName("intToBytes")]
+    public abstract byte[]? IntToBytes(BigInteger? value);
+
+    /// <summary>
+    /// Unsafe method
+    /// </summary>
+    /// <remarks>
+    /// Script: VwkAEHBoSiQGRQwBAErKjXEScmpKJAZFDAEASsqNcwwBA9swdGzbIXUQiHZu2yF3BxjEAHcIbwgQaNBvCBFp0G8IEmrQbwgTa9BvCBRs0G8IFW3QbwgWbtBvCBdvB9BvCEA=
     /// INITSLOT 0900 [64 datoshi]
     /// PUSH0 [1 datoshi]
     /// STLOC0 [2 datoshi]
     /// LDLOC0 [2 datoshi]
-    /// CONVERT 30 'Buffer' [8192 datoshi]
+    /// DUP [2 datoshi]
+    /// JMPIF 06 [2 datoshi]
+    /// DROP [2 datoshi]
+    /// PUSHDATA1 00 [8 datoshi]
+    /// DUP [2 datoshi]
+    /// SIZE [4 datoshi]
+    /// LEFT [2048 datoshi]
     /// STLOC1 [2 datoshi]
     /// PUSH2 [1 datoshi]
     /// STLOC2 [2 datoshi]
     /// LDLOC2 [2 datoshi]
-    /// CONVERT 30 'Buffer' [8192 datoshi]
+    /// DUP [2 datoshi]
+    /// JMPIF 06 [2 datoshi]
+    /// DROP [2 datoshi]
+    /// PUSHDATA1 00 [8 datoshi]
+    /// DUP [2 datoshi]
+    /// SIZE [4 datoshi]
+    /// LEFT [2048 datoshi]
     /// STLOC3 [2 datoshi]
     /// PUSHDATA1 03 [8 datoshi]
     /// CONVERT 30 'Buffer' [8192 datoshi]
