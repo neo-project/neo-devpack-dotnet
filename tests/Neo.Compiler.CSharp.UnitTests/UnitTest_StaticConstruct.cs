e5bdccd4eff8f194a3dc92d7127539d389f16a16
Jim8y <jimmy@r3e.network>
2026-09-05 13:35:52 +0800

    Preserve static initializers when constant folding is invalidated

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_StaticConstruct.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_StaticConstruct.cs
@@ -21,7 +21,7 @@ namespace Neo.Compiler.CSharp.UnitTests
         public void Test_StaticConsturct()
         {
             var var1 = Contract.TestStatic();
-            AssertGasConsumed(987090);
+            AssertGasConsumed(987180);
             // static byte[] callscript = ExecutionEngine.EntryScriptHash;
             // ...
             // return callscript
