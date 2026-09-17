1e391b7b25918cac26fe578bc9037687ed71c219
Jimmy <jimmy@r3e.network>
2026-09-17 18:51:44 +0800

    Cover nullable and struct default value conversion paths

--- tests/Neo.Compiler.CSharp.UnitTests/UnitTest_DefaultNullable.cs
+++ tests/Neo.Compiler.CSharp.UnitTests/UnitTest_DefaultNullable.cs
@@ -9,8 +9,15 @@
 // Redistribution and use in source and binary forms with or without
 // modifications are permitted.
 
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.SmartContract.Testing;
+using Neo.VM;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Neo.Compiler.CSharp.UnitTests
 {
@@ -37,6 +44,57 @@ namespace Neo.Compiler.CSharp.UnitTests
             Assert.IsNull(Contract.TestReturnDefaultExpression());
             Assert.IsTrue(Contract.TestComparisonExpression(null));
             Assert.IsFalse(Contract.TestComparisonExpression(42));
+        }
+
+        [TestMethod]
+        public void TestDefaultExpressionWithoutTypeReportsDiagnostic()
+        {
+            var (model, expression, converter) = CreateDefaultConverter("System");
+            Assert.IsNull(model.GetTypeInfo(expression.Type).Type);
+
+            var method = typeof(MethodConvert).GetMethod("ConvertDefaultExpression", BindingFlags.Instance | BindingFlags.NonPublic)!;
+            var invocation = Assert.ThrowsException<TargetInvocationException>(() => method.Invoke(converter, [model, expression]));
+            Assert.IsInstanceOfType<CompilationException>(invocation.InnerException);
+            var exception = (CompilationException)invocation.InnerException!;
+            StringAssert.Contains(exception.Message, "Cannot determine type for default expression. Ensure the expression has a valid type.");
+            Assert.AreEqual(0, converter.Instructions.Count);
+        }
+
+        [TestMethod]
+        [DataRow("int?", OpCode.PUSHNULL)]
+        [DataRow("Value", OpCode.NEWSTRUCT0)]
+        [DataRow("T", OpCode.NEWSTRUCT0)]
+        public void TestDefaultValueTypeDistinguishesNullableAndStruct(string typeName, OpCode expected)
+        {
+            var (model, expression, converter) = CreateDefaultConverter(typeName);
+            Assert.IsFalse(model.GetDiagnostics().Any(p => p.Severity == DiagnosticSeverity.Error));
+            var type = model.GetTypeInfo(expression.Type).Type!;
+            Assert.IsTrue(type.IsValueType);
+            Assert.AreEqual(typeName != "T", type is INamedTypeSymbol);
+
+            var method = typeof(MethodConvert).GetMethod("ConvertDefaultExpression", BindingFlags.Instance | BindingFlags.NonPublic)!;
+            method.Invoke(converter, [model, expression]);
+
+            CollectionAssert.AreEqual(new[] { expected }, converter.Instructions.Select(p => p.OpCode).ToArray());
+        }
+
+        private static (SemanticModel Model, DefaultExpressionSyntax Expression, MethodConvert Converter) CreateDefaultConverter(string typeName)
+        {
+            var tree = CSharpSyntaxTree.ParseText($$"""
+                public struct Value { }
+                public class Contract
+                {
+                    public static {{typeName}} Main<T>() where T : struct => default({{typeName}});
+                }
+                """);
+            var compilation = CSharpCompilation.Create("DefaultExpressions", [tree],
+                [MetadataReference.CreateFromFile(typeof(object).Assembly.Location)],
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+            var model = compilation.GetSemanticModel(tree);
+            var expression = tree.GetRoot().DescendantNodes().OfType<DefaultExpressionSyntax>().Single();
+            var symbol = (IMethodSymbol)model.GetDeclaredSymbol(expression.FirstAncestorOrSelf<MethodDeclarationSyntax>()!)!;
+            var context = new CompilationContext(new CompilationEngine(new CompilationOptions()), symbol.ContainingType);
+            return (model, expression, new MethodConvert(context, symbol));
         }
 
         [TestMethod]
