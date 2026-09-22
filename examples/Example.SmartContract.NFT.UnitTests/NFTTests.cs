50da00d1ab86af83b20363342efa97708b81b3dd
Jim8y <jimmy@r3e.network>
2026-09-23 07:09:19 +0800

    Test duplicate NFT claims and remove redundant test dependencies

--- examples/Example.SmartContract.NFT.UnitTests/NFTTests.cs
+++ examples/Example.SmartContract.NFT.UnitTests/NFTTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.SmartContract;
 using Neo.SmartContract.Testing;
+using Neo.SmartContract.Testing.Exceptions;
 using Neo.SmartContract.Testing.TestingStandards;
 using System.Text;
 
@@ -17,6 +18,18 @@ namespace Example.SmartContract.NFT.UnitTests
 
             var tokenId = Encoding.UTF8.GetBytes("7772");
             Assert.AreEqual(Engine.Transaction.Sender, Contract.OwnerOf(tokenId));
+            Assert.IsNotNull(Contract.Properties(tokenId));
+        }
+
+        [TestMethod]
+        public void TestClaimCannotMintTheSameTokenTwice()
+        {
+            Contract.Claim(7772);
+            var tokenId = Encoding.UTF8.GetBytes("7772");
+            var originalOwner = Contract.OwnerOf(tokenId);
+
+            Assert.ThrowsException<TestException>(() => Contract.Claim(7772));
+            Assert.AreEqual(originalOwner, Contract.OwnerOf(tokenId));
             Assert.IsNotNull(Contract.Properties(tokenId));
         }
     }
